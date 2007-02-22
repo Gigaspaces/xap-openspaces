8caaa1b1fae45bac8e96a9b3c432bc5c902f2d69
kimchy <kimchy@localhost>
2007-02-22 10:02:23 +0000

    extracted config string to a constant
    svn path=/trunk/openspaces/; revision=12800
    
    
    Former-commit-id: 0bf7b65d7c6ac3a2b6579ead77a49637229ab2b8

--- src/main/src/org/openspaces/pu/container/support/ConfigLocationParser.java
+++ src/main/src/org/openspaces/pu/container/support/ConfigLocationParser.java
@@ -9,11 +9,13 @@ import java.io.IOException;
  */
 public abstract class ConfigLocationParser {
 
+    public static final String CONFIG_PARAMETER = "config";
+
     public static void parse(ApplicationContextProcessingUnitContainerProvider containerProvider,
                              CommandLineParser.Parameter[] params) throws IOException {
         // parse the config location parameters
         for (int i = 0; i < params.length; i++) {
-            if (params[i].getName().equalsIgnoreCase("config")) {
+            if (params[i].getName().equalsIgnoreCase(CONFIG_PARAMETER)) {
                 for (int j = 0; j < params[i].getArguments().length; j++) {
                     containerProvider.addConfigLocation(params[i].getArguments()[j]);
                 }
