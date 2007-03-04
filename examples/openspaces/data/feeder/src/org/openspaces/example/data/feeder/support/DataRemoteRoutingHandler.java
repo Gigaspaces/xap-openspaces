e0d7aa8977c0465f9bf26e3847966bac18b6f618
kimchy <kimchy@localhost>
2007-03-04 10:20:06 +0000

    more javadoc
    svn path=/trunk/openspaces/; revision=12951
    
    
    Former-commit-id: 670586fc0fcdd56a97a70bf9e4448dcbd1a45671

--- examples/openspaces/data/feeder/src/org/openspaces/example/data/feeder/support/DataRemoteRoutingHandler.java
+++ examples/openspaces/data/feeder/src/org/openspaces/example/data/feeder/support/DataRemoteRoutingHandler.java
@@ -10,7 +10,9 @@ import org.openspaces.remoting.SpaceRemoteInvocation;
 public class DataRemoteRoutingHandler implements RemoteRoutingHandler {
 
     public void setRemoteInvocationRouting(SpaceRemoteInvocation remoteInvocation) {
-        Data data = (Data) remoteInvocation.getArguments()[0];
-        remoteInvocation.setRouting(data.getType().intValue());
+        if (remoteInvocation.getMethodName().equals("processData")) {
+            Data data = (Data) remoteInvocation.getArguments()[0];
+            remoteInvocation.setRouting(data.getType().intValue());
+        }
     }
 }
