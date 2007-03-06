c19a610fcf839da1b125a25d5969fe8027693059
kimchy <kimchy@localhost>
2007-03-06 08:49:23 +0000

    added explicit setting of different url properties on the space factory bean. This will simplify the configuration of a space within a spring context.
    svn path=/trunk/openspaces/; revision=12982
    
    
    Former-commit-id: 1566a642181954c0657961713d4ea35fdd3d213a

--- src/main/src/org/openspaces/core/space/UrlSpaceFactoryBean.java
+++ src/main/src/org/openspaces/core/space/UrlSpaceFactoryBean.java
@@ -5,6 +5,7 @@ import com.j_spaces.core.client.FinderException;
 import com.j_spaces.core.client.SpaceFinder;
 import com.j_spaces.core.client.SpaceURL;
 import com.j_spaces.core.client.SpaceURLParser;
+import com.j_spaces.core.client.view.View;
 import org.openspaces.core.GigaSpaceException;
 import org.openspaces.core.cluster.ClusterInfo;
 import org.openspaces.core.cluster.ClusterInfoAware;
@@ -34,11 +35,35 @@ import java.util.Properties;
  */
 public class UrlSpaceFactoryBean extends AbstractSpaceFactoryBean implements BeanLevelMergedPropertiesAware, ClusterInfoAware {
 
+    private static final String LOCAL_CACHE_UPDATE_MODE_PUSH = "push";
+
+    private static final String LOCAL_CACHE_UPDATE_MODE_PULL = "pull";
+
     private String url;
 
     private Map parameters;
 
     private Properties urlProperties;
+
+    private String schema;
+
+    private String groups;
+
+    private Integer lookupTimeout;
+
+    private boolean versioned = true;
+
+    private boolean noWriteLease = false;
+
+    private boolean mirror = false;
+
+    private boolean useLocalCache = false;
+
+    private int localCacheUpdateMode = SpaceURL.UPDATE_MODE_PULL;
+
+    private View[] localViews;
+
+    private boolean fifo = false;
 
     private Properties beanLevelProperties;
 
@@ -107,6 +132,137 @@ public class UrlSpaceFactoryBean extends AbstractSpaceFactoryBean implements Bea
     }
 
     /**
+     * <p>The space instance is created using a space schema file which can be used as a template configuration
+     * file for creating a space. The user specifies one of the pre-configured schema names (to create a space
+     * instance from its template) or a custom one using this property.
+     *
+     * <p>If a schema name is not defined, a default schema name called <code>default</code> will be used.
+     */
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    /**
+     * Indicates that all take/write operations be conducted in FIFO mode. Default is <code>false</code>.
+     */
+    public void setFifo(boolean fifo) {
+        this.fifo = fifo;
+    }
+
+    /**
+     * The Jini Lookup Service group to find container or space using multicast (jini protocol). Groups
+     * are comma separated list.
+     */
+    public void setGroups(String groups) {
+        this.groups = groups;
+    }
+
+
+    /**
+     * The max timeout in <b>milliseconds</b> to find a Container or Space using multicast (jini protocol).
+     * Defaults to <code>6000</code> (i.e. 6 seconds).
+     */
+    public void setLookupTimeout(Integer lookupTimeout) {
+        this.lookupTimeout = lookupTimeout;
+    }
+
+    /**
+     * When <code>false</code>, optimistic lock is disabled. Default is <code>true</code>.
+     */
+    public void setVersioned(boolean versioned) {
+        this.versioned = versioned;
+    }
+
+    /**
+     * If <code>true</code> - Lease object would not return from the write/writeMultiple operations.
+     * Defaults to <code>false</code>.
+     */
+    public void setNoWriteLease(boolean noWriteLease) {
+        this.noWriteLease = noWriteLease;
+    }
+
+    /**
+     * When setting this URL property to <code>true</code> it will allow the space to connect to the Mirror
+     * service to push its data and operations for asynchronous persistency. Defaults to <code>false</code>.
+     */
+    public void setMirror(boolean mirror) {
+        this.mirror = mirror;
+    }
+
+    /**
+     * <p>In some cases, the memory capacity of an individual application is not capable of holding all the
+     * information in the local application memory. When this happens, the desired solution will be to hold
+     * only a portion of the information in the application's memory and the rest in a separate process(s).
+     * This mode is also known as two-level cache. In this mode, the cache is divided into two components,
+     * local cache and master cache. The local cache always resides in the physical address space of the
+     * application and the master cache runs in a different process. The master cache is used to share data
+     * among the different embedded local caches running within other application instances.
+     *
+     * <p>In this mode, when a read/get operation is called, a matching object is first looked up in the local
+     * embedded cache. If the object is not found in the local cache, it will be searched for in the master
+     * cache. If it is not found in the master cache, it will reload the data from the data source. Updates
+     * on the central cache will be propagated into all local embedded cache instances in either pull or
+     * push mode, using unicast or multicast protocol.
+     *
+     * @see #setLocalCacheUpdateMode(int)
+     * @see #setLocalCacheUpdateModeName(String)
+     */
+    public void setUseLocalCache(boolean useLocalCache) {
+        this.useLocalCache = useLocalCache;
+    }
+
+    /**
+     * <p>If set to {@link com.j_spaces.core.client.SpaceURL#UPDATE_MODE_PULL} (<code>1</code>) each update triggers
+     * an invalidation event at every cache instance. The invalidate event marks the object in the local cache
+     * instances as invalid. Therefore, an attempt to read this object triggers a reload process in the master
+     * space. This configuration is useful in cases where objects are updated frequently, but the updated value
+     * is required by the application less frequently.
+     *
+     * <p>If set to {@link com.j_spaces.core.client.SpaceURL#UPDATE_MODE_PUSH} (<code>2</code>) the master pushes
+     * the updates to the local cache, which holds a reference to the same updated object.
+     *
+     * <p>This flag only applies when setting {@link #setUseLocalCache(boolean)} to <code>true</code>.
+     *
+     * @see #setLocalCacheUpdateModeName(String)
+     */
+    public void setLocalCacheUpdateMode(int localCacheUpdateMode) {
+        this.localCacheUpdateMode = localCacheUpdateMode;
+    }
+
+    /**
+     * Allows to set the local cahce update mode using a descriptive name instead of integer constants using
+     * {@link #setLocalCacheUpdateMode(int)}. Accepts either <code>push</code> or <code>pull</code>.
+     *
+     * @see #setLocalCacheUpdateMode(int)
+     */
+    public void setLocalCacheUpdateModeName(String localCacheUpdateModeName) {
+        if (LOCAL_CACHE_UPDATE_MODE_PULL.equalsIgnoreCase(localCacheUpdateModeName)) {
+            setLocalCacheUpdateMode(SpaceURL.UPDATE_MODE_PULL);
+        } else if (LOCAL_CACHE_UPDATE_MODE_PUSH.equalsIgnoreCase(localCacheUpdateModeName)) {
+            setLocalCacheUpdateMode(SpaceURL.UPDATE_MODE_PUSH);
+        } else {
+            throw new IllegalArgumentException("Wrong localCacheUpdateModeName [" + localCacheUpdateModeName + "], " +
+                    "shoudl be either '" + LOCAL_CACHE_UPDATE_MODE_PULL + "' or '" + LOCAL_CACHE_UPDATE_MODE_PUSH + "'");
+        }
+    }
+
+    /**
+     * <p>The space local view proxy maintains a subset of the master space's data, allowing the client to
+     * read distributed data without any remote operations.
+     *
+     * <p>Data is streamed into the client view in an implicit manner Ð as opposed to local cache data, that is
+     * loaded into the client on-demand, and later updated or evicted.
+     *
+     * <p>The local view is defined via a filter Ð regular template or SQLQuery Ð as opposed to the local cache
+     * that caches data at the client side only after the data has been read
+     *
+     * <p>If local views are set this factory will automatically set <code>useLocalCache</code> for the space.
+     */
+    public void setLocalViews(View[] localViews) {
+        this.localViews = localViews;
+    }
+
+    /**
      * <p>Externally mananed override properties using open spaces extended config support. Should
      * not be set directly but allowed for different Spring context container to set it.
      */
@@ -155,6 +311,35 @@ public class UrlSpaceFactoryBean extends AbstractSpaceFactoryBean implements Bea
             }
         }
 
+        if (schema != null) {
+            props.put(spaceUrlProperty(SpaceURL.SCHEMA_NAME), schema);
+        }
+
+        props.put(spaceUrlProperty(SpaceURL.FIFO_MODE), Boolean.toString(fifo));
+
+        if (groups != null) {
+            props.put(spaceUrlProperty(SpaceURL.GROUPS), groups);
+        }
+
+        if (lookupTimeout != null) {
+            props.put(spaceUrlProperty(SpaceURL.TIMEOUT), lookupTimeout.toString());
+        }
+
+        props.put(spaceUrlProperty(SpaceURL.VERSIONED), Boolean.toString(versioned));
+        props.put(spaceUrlProperty(SpaceURL.NO_WRITE_LEASE), Boolean.toString(noWriteLease));
+        props.put(spaceUrlProperty(SpaceURL.MIRROR), Boolean.toString(mirror));
+
+        // handle local views and local caches
+        if (localViews != null && localViews.length != 0) {
+            props.put(spaceUrlProperty(SpaceURL.VIEWS), localViews);
+            useLocalCache = true;
+        }
+
+        if (useLocalCache) {
+            props.put(spaceUrlProperty(SpaceURL.USE_LOCAL_CACHE), Boolean.toString(useLocalCache).toLowerCase());
+            props.put(spaceUrlProperty(SpaceURL.LOCAL_CACHE_UPDATE_MODE), Integer.toString(localCacheUpdateMode));
+        }
+
         // copy over the external config overrides
         if (beanLevelProperties != null) {
             for (Iterator it = beanLevelProperties.entrySet().iterator(); it.hasNext();) {
@@ -170,16 +355,16 @@ public class UrlSpaceFactoryBean extends AbstractSpaceFactoryBean implements Bea
                 if (clusterInfo.getNumberOfBackups() != null && clusterInfo.getNumberOfBackups().intValue() != 0) {
                     totalMembers += "," + clusterInfo.getNumberOfBackups();
                 }
-                props.setProperty(SpaceURL.PROPERTIES_SPACE_URL_ARG + "." + SpaceURL.CLUSTER_TOTAL_MEMBERS, totalMembers);
+                props.setProperty(spaceUrlProperty(SpaceURL.CLUSTER_TOTAL_MEMBERS), totalMembers);
             }
             if (clusterInfo.getInstanceId() != null) {
-                props.setProperty(SpaceURL.PROPERTIES_SPACE_URL_ARG + "." + SpaceURL.CLUSTER_MEMBER_ID, clusterInfo.getInstanceId().toString());
+                props.setProperty(spaceUrlProperty(SpaceURL.CLUSTER_MEMBER_ID), clusterInfo.getInstanceId().toString());
             }
             if (clusterInfo.getBackupId() != null) {
-                props.setProperty(SpaceURL.PROPERTIES_SPACE_URL_ARG + "." + SpaceURL.CLUSTER_BACKUP_ID, clusterInfo.getBackupId().toString());
+                props.setProperty(spaceUrlProperty(SpaceURL.CLUSTER_BACKUP_ID), clusterInfo.getBackupId().toString());
             }
             if (clusterInfo.getSchema() != null) {
-                props.setProperty(SpaceURL.PROPERTIES_SPACE_URL_ARG + "." + SpaceURL.CLUSTER_SCHEMA, clusterInfo.getSchema());
+                props.setProperty(spaceUrlProperty(SpaceURL.CLUSTER_SCHEMA), clusterInfo.getSchema());
             }
         }
 
@@ -188,5 +373,9 @@ public class UrlSpaceFactoryBean extends AbstractSpaceFactoryBean implements Bea
         } catch (MalformedURLException e) {
             throw new CannotCreateSpaceException("Failed to parse url [" + url + "]", e);
         }
+    }
+
+    private String spaceUrlProperty(String propertyName) {
+        return SpaceURL.PROPERTIES_SPACE_URL_ARG + "." + propertyName;
     }
 }
