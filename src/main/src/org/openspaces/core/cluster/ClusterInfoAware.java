68c456ca30e6bacc1669234a1b13f6c0975995c7
kimchy <kimchy@localhost>
2007-03-13 20:26:43 +0000

    more javadoc and formatting
    svn path=/trunk/openspaces/; revision=13169
    
    
    Former-commit-id: 93d4a1ac640e8b61d3c76d281c069c665436c89f

--- src/main/src/org/openspaces/core/cluster/ClusterInfoAware.java
+++ src/main/src/org/openspaces/core/cluster/ClusterInfoAware.java
@@ -1,29 +1,31 @@
 package org.openspaces.core.cluster;
 
 /**
- * <p>Allows for beans implementing this interface to be injected with
- * {@link org.openspaces.core.cluster.ClusterInfo ClusterInfo}.
- *
- * <p>Note, the cluster information is obtained externally from the applicaiton context which
- * means that this feature need to be supported by specific containers (and is not supported
- * by plain Spring application context). This means that beans that implementations of
- * {@link org.openspaces.core.cluster.ClusterInfoAware ClusterInfoAware} should take into account
- * the fact that the cluster info provided might be null.
- *
+ * Allows for beans implementing this interface to be injected with {@link ClusterInfo}.
+ * 
+ * <p>
+ * Note, the cluster information is obtained externally from the applicaiton context which means
+ * that this feature need to be supported by specific containers (and is not supported by plain
+ * Spring application context). This means that beans that implementations of
+ * {@link ClusterInfoAware} should take into account the fact that the cluster info provided might
+ * be null.
+ * 
  * @author kimchy
  */
 public interface ClusterInfoAware {
 
     /**
-     * <p>Sets the cluster information.
-     *
-     * <p>Note, the cluster information is obtained externally from the applicaiton
-     * context which means that this feature need to be supported by specific containers
-     * (and is not supported by plain Spring application context). This means that beans
-     * that implement {@link org.openspaces.core.cluster.ClusterInfoAware ClusterInfoAware}
+     * <p>
+     * Sets the cluster information.
+     * 
+     * <p>
+     * Note, the cluster information is obtained externally from the applicaiton context which means
+     * that this feature need to be supported by specific containers (and is not supported by plain
+     * Spring application context). This means that beans that implement {@link ClusterInfoAware}
      * should take into account the fact that the cluster info provided might be null.
-     *
-     * @param clusterInfo The cluster infromation to be injected
+     * 
+     * @param clusterInfo
+     *            The cluster infromation to be injected
      */
     void setClusterInfo(ClusterInfo clusterInfo);
 }
--- src/main/src/org/openspaces/core/cluster/ClusterInfoBeanPostProcessor.java
+++ src/main/src/org/openspaces/core/cluster/ClusterInfoBeanPostProcessor.java
@@ -4,16 +4,20 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 /**
- * A Spring {@link org.springframework.beans.factory.config.BeanPostProcessor BeanPostProcessor}
- * that takes a {@link org.openspaces.core.cluster.ClusterInfo ClusterInfo} and injects it to all
- * the beans that implements {@link org.openspaces.core.cluster.ClusterInfoAware ClusterInfoAware} interface.
- *
+ * A Spring {@link BeanPostProcessor} that takes a {@link ClusterInfo} and injects it to all the
+ * beans that implements {@link ClusterInfoAware} interface.
+ * 
  * @author kimchy
  */
 public class ClusterInfoBeanPostProcessor implements BeanPostProcessor {
 
     private ClusterInfo clusterInfo;
 
+    /**
+     * Constructs a new cluster info bean post processor based on the provided cluster info.
+     * 
+     * @param clusterInfo
+     */
     public ClusterInfoBeanPostProcessor(ClusterInfo clusterInfo) {
         this.clusterInfo = clusterInfo;
     }
