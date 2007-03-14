d39e98b48a0914db30e5b3ca6f888c8bc70e1641
kimchy <kimchy@localhost>
2007-03-14 07:36:06 +0000

    more javadoc and formatting
    svn path=/trunk/openspaces/; revision=13173
    
    
    Former-commit-id: 65fd81d7d0d2d63010212fd72df6c3c5ed2654c0

--- src/main/src/org/openspaces/core/exception/DefaultExceptionTranslator.java
+++ src/main/src/org/openspaces/core/exception/DefaultExceptionTranslator.java
@@ -4,13 +4,20 @@ import org.openspaces.core.GigaSpaceException;
 import org.openspaces.core.SpaceOperationException;
 
 /**
- * The default exception translator, currently translating all exceptions
- * into a {@link org.openspaces.core.SpaceOperationException}.
- *
+ * The default exception translator, currently translating all exceptions into a
+ * {@link SpaceOperationException}.
+ * 
  * @author kimchy
+ * @see org.openspaces.core.GigaSpace
+ * @see org.openspaces.core.DefaultGigaSpace
+ * @see org.openspaces.core.GigaSpaceFactoryBean
  */
 public class DefaultExceptionTranslator implements ExceptionTranslator {
 
+    /**
+     * Translateds general JavaSpaces and Jini exceptions into a GigaSpace exception. Currently
+     * translates all exceptions into a {@link SpaceOperationException}.
+     */
     public GigaSpaceException translate(Exception e) {
         return new SpaceOperationException(e.getMessage(), e);
     }
--- src/main/src/org/openspaces/core/exception/ExceptionTranslator.java
+++ src/main/src/org/openspaces/core/exception/ExceptionTranslator.java
@@ -3,18 +3,18 @@ package org.openspaces.core.exception;
 import org.openspaces.core.GigaSpaceException;
 
 /**
- * Translates a low level exception into a {@link org.openspaces.core.GigaSpaceException}
- * runtime exception.
- *
+ * Translates a low level JavaSpaces/Jini exception into a {@link GigaSpaceException} runtime
+ * exception.
+ * 
  * @author kimchy
  */
 public interface ExceptionTranslator {
 
     /**
-     * Translates a low leval exception into a {@link org.openspaces.core.GigaSpaceException}
-     * rutime exception.
-     *
-     * @param e The low level exception to translate
+     * Translates a low leval exception into a {@link GigaSpaceException} rutime exception.
+     * 
+     * @param e
+     *            The low level exception to translate
      * @return The translated exception
      */
     GigaSpaceException translate(Exception e);
