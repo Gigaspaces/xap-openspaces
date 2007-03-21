46eba0a86c6ce454a041fbaa96c4b18d50491432
ming <ming@localhost>
2007-03-21 16:58:05 +0000

    refactored to work inside the gs cli
    svn path=/trunk/openspaces/; revision=13375
    
    
    Former-commit-id: a916e0e844e0483ff9e42a74b8dba7bd283a7e25

--- src/main/src/org/openspaces/pu/container/servicegrid/deploy/Deploy.java
+++ src/main/src/org/openspaces/pu/container/servicegrid/deploy/Deploy.java
@@ -41,6 +41,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -53,132 +55,10 @@ public class Deploy {
 // ------------------------------ FIELDS ------------------------------
 
     private static final Logger LOGGER = Logger.getLogger(Deploy.class.getName());
+    private DeployAdmin deployAdmin;
+    private String[] groups;
 
 // -------------------------- STATIC METHODS --------------------------
-
-    static OperationalString loadDeployment(
-            String puString, String codeserver, String jiniGroup, SLA sla, File[] jars, String puPath, String puName, File[] sharedJars)
-            throws Exception {
-        URL opstringURL = Deploy.class.getResource("/org/openspaces/pu/container/servicegrid/puservicebean.xml");
-        System.out.println("opstringURL = " + opstringURL);
-        OperationalString opString = null;
-
-        //load the servicebean opstring
-        OpStringLoader opStringLoader = new OpStringLoader();
-        opStringLoader.setDefaultGroups(new String[]{jiniGroup});
-        opStringLoader.setCodebaseOverride(codeserver);
-        opString = opStringLoader.parseOperationalString(opstringURL)[0];
-        ((OpString) opString).setName(puName);
-
-        //this opstring should only have one servicebean
-        ServiceElement[] serviceElements = opString.getServices();
-        ServiceElement element = serviceElements[0];
-
-        //put the entire pu spring xml as parameter to servicebean
-        element.getServiceBeanConfig().addInitParameter("pu", puString);
-
-        //sla
-        Policy policy = sla.getPolicy();
-        if (policy != null) {
-            String type;
-            if (policy instanceof ScaleUpPolicy) {
-                type = "scaling";
-            } else if (policy instanceof RelocationPolicy) {
-                type = "relocation";
-            } else {
-                throw new IllegalArgumentException("Unknown SLA Policy:" + policy);
-            }
-
-            String max = String.valueOf(sla.getNumberOfInstances());
-            //todo: make sure max is greater then num of instances
-            if (policy instanceof ScaleUpPolicy) {
-                max = String.valueOf(((ScaleUpPolicy) policy).getScaleUpTo());
-            }
-            //todo:300 is hard coded for now
-            String[] configParms = getSLAConfigArgs(type, max, "3000", "3000");
-            org.jini.rio.core.SLA slaElement = new org.jini.rio.core.SLA(
-                    policy.getMonitor(),
-                    new double[]{policy.getLow(), policy.getHigh()},
-                    configParms,
-                    null);
-            element.getServiceLevelAgreements().addServiceSLA(slaElement);
-        }
-
-        //requirements
-        List hosts = new ArrayList();
-        if (sla.getRequirements() != null) {
-            for (int i = 0; i < sla.getRequirements().size(); i++) {
-                Object requirement = sla.getRequirements().get(i);
-                if (requirement instanceof RangeRequirement) {
-                    RangeRequirement rangeRequirement = (RangeRequirement) requirement;
-                    ThresholdValues thresholdValues = new ThresholdValues(rangeRequirement.getLow(), rangeRequirement.getHigh());
-                    element.getServiceLevelAgreements().addSystemThreshold(rangeRequirement.getWatch(), thresholdValues);
-                } else if (requirement instanceof Host) {
-                    hosts.add(((Host) requirement).getHost());
-                } else if (requirement instanceof Generic) {
-                    Generic generic = (Generic) requirement;
-                    ServiceLevelAgreements.SystemRequirement systemRequirement = new ServiceLevelAgreements.SystemRequirement(
-                            generic.getName(),
-                            null,
-                            generic.getAttributes()
-                    );
-                    element.getServiceLevelAgreements().addSystemRequirement(systemRequirement);
-                }
-            }
-        }
-        //put hosts as cluster
-        if (hosts.size() > 0) {
-            element.setCluster((String[]) hosts.toArray(new String[hosts.size()]));
-        }
-
-        if (sla.getMaxInstancesPerVM() > 0) {
-            element.setMaxPerMachine(sla.getMaxInstancesPerVM());
-        }
-
-        //put jars
-        ClassBundle classBundle = element.getComponentBundle();
-        classBundle.addJAR(puPath + "/");
-        for (int i = 0; i < jars.length; i++) {
-            File jar = jars[i];
-            String path = jar.getPath();
-            classBundle.addJAR(path);
-            System.out.println("added jar:" + path);
-        }
-
-        //shared-lib as sharedComponents
-        String[] sharedJarPaths = new String[sharedJars.length];
-        for (int i = 0; i < sharedJars.length; i++) {
-            File sharedJar = sharedJars[i];
-            String path = sharedJar.getPath();
-            sharedJarPaths[i] = path;
-        }
-        Map jarsMap = new HashMap();
-        jarsMap.put("hack", sharedJarPaths);
-        classBundle.addSharedComponents(jarsMap);
-
-        //this is the MOST IMPORTANT part
-        boolean hasBackups = sla.getNumberOfBackups() > 0;
-        if (hasBackups) {
-            //the extra one is the primary
-            element.setPlanned(sla.getNumberOfBackups() + 1);
-            String name = element.getName();
-            opString.removeService(element);
-            for (int i = 1; i <= sla.getNumberOfInstances(); i++) {
-                ServiceElement clone = deepCopy(element);
-                clone.getServiceBeanConfig().setName(name + "." + i);
-                clone.getServiceBeanConfig().addInitParameter("clusterGroup", String.valueOf(i));
-                opString.addService(clone);
-            }
-        } else {
-            element.setPlanned(sla.getNumberOfInstances());
-            element.getServiceBeanConfig().addInitParameter("clusterGroup", String.valueOf(1));
-        }
-
-
-        System.out.println(element);
-
-        return (opString);
-    }
 
     private static ServiceElement deepCopy(ServiceElement element) throws IOException, ClassNotFoundException {
         //write
@@ -282,6 +162,232 @@ public class Deploy {
         IntegratedProcessingUnitContainer integratedContainer = (IntegratedProcessingUnitContainer) factory.createContainer();
     }
 
+    private static void printUsage() {
+        System.out.println("Usage: Deploy [PU Name]");
+    }
+
+// --------------------- GETTER / SETTER METHODS ---------------------
+
+    public DeployAdmin getDeployAdmin() {
+        if (deployAdmin == null) {
+            //find gsm and codebase
+            GSM gsm = ProvisionerFinder.find(null, 5000, getGroups());
+            if (gsm == null) {
+                throw new RuntimeException("GSM Not Found!");
+            }
+            try {
+                deployAdmin = (DeployAdmin) gsm.getAdmin();
+            } catch (RemoteException e) {
+                LOGGER.log(Level.WARNING, e.toString(), e);
+            }
+        }
+
+        return deployAdmin;
+    }
+
+    public void setDeployAdmin(DeployAdmin deployAdmin) {
+        this.deployAdmin = deployAdmin;
+    }
+
+    public String[] getGroups() {
+        if (groups == null) {
+            String groupsProperty = System.getProperty("com.gs.jini_lus.groups");
+            System.out.println("JINI Groups = " + groupsProperty);
+            if (groupsProperty != null) {
+                StringTokenizer tokenizer = new StringTokenizer(groupsProperty);
+                int count = tokenizer.countTokens();
+                groups = new String[count];
+                for (int i = 0; i < count; i++) {
+                    groups[i] = tokenizer.nextToken();
+                }
+            }
+        }
+        return groups;
+    }
+
+    public void setGroups(String[] groups) {
+        this.groups = groups;
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    OperationalString loadDeployment(
+            String puString, String codeserver, SLA sla, File[] jars, String puPath, String puName, File[] sharedJars)
+            throws Exception {
+        URL opstringURL = Deploy.class.getResource("/org/openspaces/pu/container/servicegrid/puservicebean.xml");
+        //System.out.println("opstringURL = " + opstringURL);
+        OperationalString opString = null;
+
+        //load the servicebean opstring
+        OpStringLoader opStringLoader = new OpStringLoader();
+        opStringLoader.setDefaultGroups(getGroups());
+        opStringLoader.setCodebaseOverride(codeserver);
+        opString = opStringLoader.parseOperationalString(opstringURL)[0];
+        ((OpString) opString).setName(puName);
+
+        //this opstring should only have one servicebean
+        ServiceElement[] serviceElements = opString.getServices();
+        ServiceElement element = serviceElements[0];
+
+        //put the entire pu spring xml as parameter to servicebean
+        element.getServiceBeanConfig().addInitParameter("pu", puString);
+
+        //sla
+        Policy policy = sla.getPolicy();
+        if (policy != null) {
+            String type;
+            if (policy instanceof ScaleUpPolicy) {
+                type = "scaling";
+            } else if (policy instanceof RelocationPolicy) {
+                type = "relocation";
+            } else {
+                throw new IllegalArgumentException("Unknown SLA Policy:" + policy);
+            }
+
+            String max = String.valueOf(sla.getNumberOfInstances());
+            //todo: make sure max is greater then num of instances
+            if (policy instanceof ScaleUpPolicy) {
+                max = String.valueOf(((ScaleUpPolicy) policy).getScaleUpTo());
+            }
+            //todo:300 is hard coded for now
+            String[] configParms = getSLAConfigArgs(type, max, "3000", "3000");
+            org.jini.rio.core.SLA slaElement = new org.jini.rio.core.SLA(
+                    policy.getMonitor(),
+                    new double[]{policy.getLow(), policy.getHigh()},
+                    configParms,
+                    null);
+            element.getServiceLevelAgreements().addServiceSLA(slaElement);
+        }
+
+        //requirements
+        List hosts = new ArrayList();
+        if (sla.getRequirements() != null) {
+            for (int i = 0; i < sla.getRequirements().size(); i++) {
+                Object requirement = sla.getRequirements().get(i);
+                if (requirement instanceof RangeRequirement) {
+                    RangeRequirement rangeRequirement = (RangeRequirement) requirement;
+                    ThresholdValues thresholdValues = new ThresholdValues(rangeRequirement.getLow(), rangeRequirement.getHigh());
+                    element.getServiceLevelAgreements().addSystemThreshold(rangeRequirement.getWatch(), thresholdValues);
+                } else if (requirement instanceof Host) {
+                    hosts.add(((Host) requirement).getHost());
+                } else if (requirement instanceof Generic) {
+                    Generic generic = (Generic) requirement;
+                    ServiceLevelAgreements.SystemRequirement systemRequirement = new ServiceLevelAgreements.SystemRequirement(
+                            generic.getName(),
+                            null,
+                            generic.getAttributes()
+                    );
+                    element.getServiceLevelAgreements().addSystemRequirement(systemRequirement);
+                }
+            }
+        }
+        //put hosts as cluster
+        if (hosts.size() > 0) {
+            element.setCluster((String[]) hosts.toArray(new String[hosts.size()]));
+        }
+
+        if (sla.getMaxInstancesPerVM() > 0) {
+            element.setMaxPerMachine(sla.getMaxInstancesPerVM());
+        }
+
+        //put jars
+        ClassBundle classBundle = element.getComponentBundle();
+        classBundle.addJAR(puPath + "/");
+        for (int i = 0; i < jars.length; i++) {
+            File jar = jars[i];
+            String path = jar.getPath();
+            classBundle.addJAR(path);
+//            System.out.println("added jar:" + path);
+        }
+
+        //shared-lib as sharedComponents
+        String[] sharedJarPaths = new String[sharedJars.length];
+        for (int i = 0; i < sharedJars.length; i++) {
+            File sharedJar = sharedJars[i];
+            String path = sharedJar.getPath();
+            sharedJarPaths[i] = path;
+        }
+        Map jarsMap = new HashMap();
+        jarsMap.put("hack", sharedJarPaths);
+        classBundle.addSharedComponents(jarsMap);
+
+        //this is the MOST IMPORTANT part
+        boolean hasBackups = sla.getNumberOfBackups() > 0;
+        if (hasBackups) {
+            //the extra one is the primary
+            element.setPlanned(sla.getNumberOfBackups() + 1);
+            String name = element.getName();
+            opString.removeService(element);
+            for (int i = 1; i <= sla.getNumberOfInstances(); i++) {
+                ServiceElement clone = deepCopy(element);
+                clone.getServiceBeanConfig().setName(name + "." + i);
+                clone.getServiceBeanConfig().addInitParameter("clusterGroup", String.valueOf(i));
+                opString.addService(clone);
+            }
+        } else {
+            element.setPlanned(sla.getNumberOfInstances());
+            element.getServiceBeanConfig().addInitParameter("clusterGroup", String.valueOf(1));
+        }
+
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, element.toString());
+        }
+
+        return (opString);
+    }
+
+    public void deploy(String[] args) throws Exception {
+        //get codebase from service
+        String codeserver = getCodebase(getDeployAdmin());
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "codeserver = " + codeserver);
+        }
+
+        //get pu xml
+        String puPath = args[0];
+        int index = puPath.lastIndexOf('/');
+        index = index == -1 ? 0 : index;
+        String puName = puPath.substring(index);
+
+        //list remote files, only works with webster
+        URL root = new URL(codeserver);
+        HTTPFileSystemView view = new HTTPFileSystemView(root);
+        File puHome = view.createFileObject(puPath);
+
+        //get list of all jars
+        File lib = view.createFileObject(puHome, "lib");
+        File[] jars = view.getFiles(lib, false);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "jars = " + Arrays.asList(jars));
+        }
+
+        //get list of all shared
+        File shared = view.createFileObject(puHome, "shared-lib");
+        File[] sharedJars = view.getFiles(shared, false);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "sharedJars = " + Arrays.asList(sharedJars));
+        }
+
+        //read pu xml
+        String puString = readPUFile(root, puPath);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "puString = " + puString);
+        }
+
+        //get sla from pu string
+        SLA sla = SLAUtil.loadSLA(puString);
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.log(Level.FINE, "sla = " + sla);
+        }
+
+        //deploy to sg
+        OperationalString opString = loadDeployment(puString, codeserver, sla, jars, puPath, puName, sharedJars);
+        Map result = deployAdmin.deploy(opString);
+//        System.out.println("result = " + result);
+
+//        deployLocal(sla, puString);
+    }
+
 // --------------------------- main() method ---------------------------
 
     public static void main(String[] args) throws Exception {
@@ -291,10 +397,7 @@ public class Deploy {
             printUsage();
             System.exit(-1);
         }
-        String puPath = args[0];
 
-        String jiniGroup = System.getProperty("com.gs.jini_lus.groups");
-        System.out.println("jiniGroup = " + jiniGroup);
 
         System.setSecurityManager(new RMISecurityManager() {
             public void checkPermission(java.security.Permission perm) {
@@ -304,45 +407,7 @@ public class Deploy {
             }
         });
 
-        //find gsm and codebase
-        GSM gsm = ProvisionerFinder.find(null, 5000, new String[]{jiniGroup});
-        DeployAdmin deployAdmin = (DeployAdmin) gsm.getAdmin();
-        String codeserver = getCodebase(deployAdmin);
-        System.out.println("codeserver = " + codeserver);
-
-        //get pu xml
-        int index = puPath.lastIndexOf('/');
-        index = index == -1 ? 0 : index;
-        String puName = puPath.substring(index);
-        System.out.println("puName = " + puName);
-
-        URL root = new URL(codeserver);
-        HTTPFileSystemView view = new HTTPFileSystemView(root);
-        File puHome = view.createFileObject(puPath);
-        //get list of all jars
-        File lib = view.createFileObject(puHome, "lib");
-        File[] jars = view.getFiles(lib, false);
-        System.out.println("jars = " + Arrays.asList(jars));
-        //get list of all shared
-        File shared = view.createFileObject(puHome, "shared-lib");
-        File[] sharedJars = view.getFiles(shared, false);
-        System.out.println("sharedJars = " + Arrays.asList(sharedJars));
-
-        //read pu xml
-        String puString = readPUFile(root, puPath);
-        System.out.println("puString = " + puString);
-        SLA sla = SLAUtil.loadSLA(puString);
-        System.out.println("sla = " + sla);
-
-        //deploy to sg
-        OperationalString opString = loadDeployment(puString, codeserver, jiniGroup, sla, jars, puPath, puName, sharedJars);
-        Map result = deployAdmin.deploy(opString);
-//        System.out.println("result = " + result);
-
-//        deployLocal(sla, puString);
-    }
-
-    private static void printUsage() {
-        System.out.println("Usage: Deploy [PU Name]");
+        Deploy deployer = new Deploy();
+        deployer.deploy(args);
     }
 }
