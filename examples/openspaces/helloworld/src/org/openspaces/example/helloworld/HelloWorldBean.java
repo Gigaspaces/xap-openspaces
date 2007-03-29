b1cbcdc53eaa43b04e26cf7385670d81bd1b79ac
kimchy <kimchy@localhost>
2007-03-29 08:05:02 +0000

    intial docs for hello world example
    svn path=/trunk/openspaces/; revision=13530
    
    
    Former-commit-id: 189a59c88e4a35ae8915df0bf9ae6c682004abd5

--- examples/openspaces/helloworld/src/org/openspaces/example/helloworld/HelloWorldBean.java
+++ examples/openspaces/helloworld/src/org/openspaces/example/helloworld/HelloWorldBean.java
@@ -7,6 +7,19 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
 /**
+ * <p>A simple bean that is run within a Spring context that acts
+ * as the processing unit context as well (when executed within a
+ * processing unit container).
+ *
+ * <p>Gets injected with a {@link org.openspaces.core.GigaSpace} interface
+ * which provides a simpler programming model on top of JavaSpaces core interface.
+ *
+ * <p>The bean writes and takes a {@link org.openspaces.example.helloworld.Message}
+ * object from the space using the injected {@link org.openspaces.core.GigaSpace}
+ * when the bean starts up (using Spings {@link org.springframework.beans.factory.InitializingBean}
+ * callback). This means that spring context is started or the processing unit is
+ * deployed, this code will be executed.
+ *
  * @author kimchy
  */
 public class HelloWorldBean implements InitializingBean {
--- examples/openspaces/helloworld/src/org/openspaces/example/helloworld/Message.java
+++ examples/openspaces/helloworld/src/org/openspaces/example/helloworld/Message.java
@@ -1,6 +1,12 @@
 package org.openspaces.example.helloworld;
 
 /**
+ * A simple message object that is written to the space. Note, this
+ * message uses GigaSpaces support for POJO entries. With GigaSpaces
+ * support for POJO entries there is no need even to mark the class
+ * using annotations or xml though further customization is allowed
+ * when using it. 
+ *
  * @author kimchy
  */
 public class Message {
