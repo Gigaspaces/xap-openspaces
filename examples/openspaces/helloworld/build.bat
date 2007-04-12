9e6e27d1680f62357079d5f792647a597f364b8a
kimchy <kimchy@localhost>
2007-04-12 13:34:40 +0000

    GS-1705 HelloWorld example in OpenSpaces build fails
    svn path=/trunk/openspaces/; revision=13883
    
    
    Former-commit-id: 94ee8fa8f732a15c85a636ccb565f1d46cd226b3

--- examples/openspaces/helloworld/build.bat
+++ examples/openspaces/helloworld/build.bat
@@ -2,4 +2,4 @@
 
 @call %~dp0\..\..\..\bin\setenv.bat
 
-@%JAVACMD% %LOOKUP_GROUPS_PROP% -classpath %ANT_JARS% org.apache.tools.ant.Main %1
+@%JAVACMD% %LOOKUP_GROUPS_PROP% -classpath %ANT_JARS%;"%JAVA_HOME%/lib/tools.jar" org.apache.tools.ant.Main %1
