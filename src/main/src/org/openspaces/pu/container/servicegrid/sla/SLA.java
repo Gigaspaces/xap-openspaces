b829dbf5a88e028488e0b8d3a61b99f6c40817f2
kimchy <kimchy@localhost>
2007-04-19 10:21:10 +0000

    javadoc for SLA
    svn path=/trunk/openspaces/; revision=14122
    
    
    Former-commit-id: c776d9daf04fc476fcf4c41a1dbeacd11f1a49ef

--- src/main/src/org/openspaces/pu/container/servicegrid/sla/SLA.java
+++ src/main/src/org/openspaces/pu/container/servicegrid/sla/SLA.java
@@ -7,6 +7,43 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
+ * A set of definitions controlling the nature of a processing unit deployment
+ * on the Service Grid.
+ *
+ * <p>The SLA allows to control a clustered deployment which maps to
+ * {@link org.openspaces.core.cluster.ClusterInfo ClusterInfo}. The
+ * {@link #setClusterSchema(String) clusterSchema}, {@link #setNumberOfInstances(int) numberOfInstances},
+ * and {@link #setNumberOfBackups(int) numberOfBackups} control the cluster
+ * topology. Note that it is up to the user to defined the correct values
+ * according to the chosen cluster schema. For example, when using <code>partitioned</code>
+ * schema, the number of backups should not be set (or set to 0).
+ *
+ * <p>The SLA is translated into one or more service bean definitions, each can have
+ * one or more instances. When having one or more backups, a service bean defintiion
+ * will be created for each primary and its backups group. So, in case of 2 instnaces
+ * with 1 backup, two service bean definition will be created, one for the first instance
+ * and its backup, and the second for the second instance and its backup.
+ *
+ * <p>Max instances per VM can also be set which using {@link #setMaxInstancesPerVM(int)}.
+ * It controls how many instnaces of a specific deployment will be created within a
+ * grid container. This is very benefitial when using primary with backup where we would
+ * not want the primary to run in the same VM as the backup.
+ *
+ * <p>A list of {@link org.openspaces.pu.container.servicegrid.sla.monitor.Monitor monitor}s
+ * can be added to the SLA. The monitors can be used to monitor different <b>runtime</b> aspects
+ * of the processing unit. They can then be used to affect certain {@link #setPolicy(Policy) Policy}
+ * that is set.
+ *
+ * <p>A list of {@link org.openspaces.pu.container.servicegrid.sla.requirement.Requirement Requirement}s
+ * can also be added to the SLA. The requirements control if a certain processing unit instance will be
+ * <b>deployed</b> to a grid container. It will only be deployed if the grid container meets the specified
+ * requirements.
+ *
+ * <p>A {@link org.openspaces.pu.container.servicegrid.sla.Policy Policy} can be set on the SLA to control
+ * the actions the grid container should take in case the policy associated monitor value breach the
+ * policy thresholds.
+ *
+ * @author kimchy
  */
 public class SLA implements Serializable {
 
@@ -24,58 +61,136 @@ public class SLA implements Serializable {
 
     private int maxInstancesPerVM;
 
+    /**
+     * Returns the cluster schema the processing unit will use. Usually maps to the space
+     * cluster schema. Can have <code>null</code> value which means that it was not set.
+     *
+     * @see org.openspaces.core.cluster.ClusterInfo#getSchema()
+     */
     public String getClusterSchema() {
         return clusterSchema;
     }
 
+    /**
+     * Sets the cluster schema the processing unit will use. Usually maps to the space
+     * cluster schema. Can have <code>null</code> value which means that it was not set.
+     *
+     * @see org.openspaces.core.cluster.ClusterInfo#setSchema(String)
+     */
     public void setClusterSchema(String clusterSchema) {
         this.clusterSchema = clusterSchema;
     }
 
-    public int getMaxInstancesPerVM() {
-        return maxInstancesPerVM;
-    }
-
-    public void setMaxInstancesPerVM(int maxInstancesPerVM) {
-        this.maxInstancesPerVM = maxInstancesPerVM;
-    }
-
-    public int getNumberOfBackups() {
-        return numberOfBackups;
-    }
-
-    public void setNumberOfBackups(int numberOfBackups) {
-        this.numberOfBackups = numberOfBackups;
-    }
-
+    /**
+     * Returns the number of primary instances that are running within the cluster. Note, this are
+     * the number of primary instances. Each instance might also have one or more backups that are
+     * set by {@link #setNumberOfBackups(int) numberOfBackups}. Defaults to <code>1</code>.
+     *
+     * @see org.openspaces.core.cluster.ClusterInfo#getNumberOfInstances()
+     */
     public int getNumberOfInstances() {
         return numberOfInstances;
     }
 
+    /**
+     * Sets the number of primary instances that are running within the cluster. Note, this are the
+     * number of primary instances. Each instance might also have one or more backups that are set
+     * by {@link #setNumberOfBackups(int) numberOfBackups}. . Defaults to <code>1</code>.
+     */
     public void setNumberOfInstances(int numberOfInstances) {
         this.numberOfInstances = numberOfInstances;
     }
 
+    /**
+     * Returns the number of backups that each primary instance will have in a cluster. Can have
+     * <code>0</code> value which means that it was not set and should not be taken into
+     * account. Defaults to <code>0</code>.
+     */
+    public int getNumberOfBackups() {
+        return numberOfBackups;
+    }
+
+    /**
+     * Sets the number of backups that each primary instance will have in a cluster. Can have
+     * <code>0</code> value which means that it was not set and should not be taken into
+     * account. Defaults to <code>0</code>.
+     */
+    public void setNumberOfBackups(int numberOfBackups) {
+        this.numberOfBackups = numberOfBackups;
+    }
+
+    /**
+     * Returns the maximum number of instances of the processing unit allowed to run within
+     * the same grid container.
+     *
+     * <p>Note, when using numebr of backups higher than 0, this value only applies to a
+     * primary with its backups group.
+     */
+    public int getMaxInstancesPerVM() {
+        return maxInstancesPerVM;
+    }
+
+    /**
+     * Sets the maximum number of instances of the processing unit allowed to run within
+     * the same grid container.
+     *
+     * <p>Note, when using numebr of backups higher than 0, this value only applies to a
+     * primary with its backups group.
+     */
+    public void setMaxInstancesPerVM(int maxInstancesPerVM) {
+        this.maxInstancesPerVM = maxInstancesPerVM;
+    }
+
+    /**
+     * Returns the policy associated with the SLA controlling the runtime policy on the
+     * action needed to be taken when the monitor associated with the policy breaks the
+     * policy threasholds.
+     */
     public Policy getPolicy() {
         return policy;
     }
 
+    /**
+     * Sets the policy associated with the SLA controlling the runtime policy on the
+     * action needed to be taken when the monitor associated with the policy breaks the
+     * policy threasholds.
+     */
     public void setPolicy(Policy policy) {
         this.policy = policy;
     }
 
+    /**
+     * Returns a list of requirements that control if a certain processing unit instance will be
+     * <b>deployed</b> to a grid container. It will only be deployed if the grid container meets the specified
+     * requirements.
+     */
     public List<Requirement> getRequirements() {
         return requirements;
     }
 
+    /**
+     * Sets a list of requirements that control if a certain processing unit instance will be
+     * <b>deployed</b> to a grid container. It will only be deployed if the grid container meets the specified
+     * requirements.
+     */
     public void setRequirements(List<Requirement> requirements) {
         this.requirements = requirements;
     }
 
+    /**
+     * Returns a list of monitors can be used to monitor different <b>runtime</b> aspects
+     * of the processing unit. They can then be used to affect certain {@link #setPolicy(Policy) Policy}
+     * that is set.
+     */
     public List<Monitor> getMonitors() {
         return monitors;
     }
 
+    /**
+     * Sets a list of monitors can be used to monitor different <b>runtime</b> aspects
+     * of the processing unit. They can then be used to affect certain {@link #setPolicy(Policy) Policy}
+     * that is set.
+     */
     public void setMonitors(List<Monitor> monitors) {
         this.monitors = monitors;
     }
