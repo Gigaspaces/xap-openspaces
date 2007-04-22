1f9cb555707013c5c2a43f97d847ca1eac066611
kimchy <kimchy@localhost>
2007-04-22 07:08:24 +0000

    javadoc for policies
    svn path=/trunk/openspaces/; revision=14140
    
    
    Former-commit-id: 4b2164f6225a49771d1ea2782319e7bfbc9e668c

--- src/main/src/org/openspaces/pu/container/servicegrid/sla/ScaleUpPolicy.java
+++ src/main/src/org/openspaces/pu/container/servicegrid/sla/ScaleUpPolicy.java
@@ -1,15 +1,27 @@
 package org.openspaces.pu.container.servicegrid.sla;
 
 /**
+ * Scale up policy will cause a processing unit instance to be created
+ * when the policy associated monitor breaches its threashold values.
+ *
+ * @author kimchy
  */
 public class ScaleUpPolicy extends AbstractPolicy {
 
     private int scaleUpTo;
 
+    /**
+     * The maximum number of processing instances this scale up policy will scale
+     * up to. Should be higher than {@link #getHigh() high} value.
+     */
     public int getScaleUpTo() {
         return scaleUpTo;
     }
 
+    /**
+     * The maximum number of processing instances this scale up policy will scale
+     * up to. Should be higher than {@link #getHigh() high} value.
+     */
     public void setScaleUpTo(int scaleUpTo) {
         this.scaleUpTo = scaleUpTo;
     }
