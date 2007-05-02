a9d271840e132424d62d4f20e87c0b39b374a829
kimchy <kimchy@localhost>
2007-05-02 15:48:03 +0000

    more requirements tests
    svn path=/trunk/openspaces/; revision=14301
    
    
    Former-commit-id: a5ccb26a46346314cbd6bac23ff59d9462054014

--- src/main/test/org/openspaces/itest/pu/sla/requirements/RequirementsSlaSchameTests.java
+++ src/main/test/org/openspaces/itest/pu/sla/requirements/RequirementsSlaSchameTests.java
@@ -1,7 +1,9 @@
 package org.openspaces.itest.pu.sla.requirements;
 
 import org.openspaces.pu.sla.SLA;
+import org.openspaces.pu.sla.requirement.CpuRequirement;
 import org.openspaces.pu.sla.requirement.HostRequirement;
+import org.openspaces.pu.sla.requirement.MemoryRequirement;
 import org.openspaces.pu.sla.requirement.SystemRequirement;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
@@ -22,11 +24,15 @@ public class RequirementsSlaSchameTests extends AbstractDependencyInjectionSprin
         SLA sla = (SLA) getApplicationContext().getBean("SLA");
         assertNotNull(sla);
         assertNotNull(sla.getRequirements());
-        assertEquals(2, sla.getRequirements().size());
+        assertEquals(4, sla.getRequirements().size());
         HostRequirement host = (HostRequirement) sla.getRequirements().get(0);
         assertEquals("test", host.getHost());
         SystemRequirement system = (SystemRequirement) sla.getRequirements().get(1);
         assertEquals("test2", system.getName());
         assertNotNull(system.getAttributes());
+        CpuRequirement cpuRequirement = (CpuRequirement) sla.getRequirements().get(2);
+        assertEquals(.9, cpuRequirement.getHigh());
+        MemoryRequirement memoryRequirement = (MemoryRequirement) sla.getRequirements().get(3);
+        assertEquals(.8, memoryRequirement.getHigh());
     }
 }
