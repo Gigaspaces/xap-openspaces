767e6a5b9dae2edf9cd34a5af1974986dd338da9
kimchy <kimchy@localhost>
2007-05-09 07:14:19 +0000

    GS-1930 Polling Event Container: When no message is received under a transaction, the transaction should be aborted and not committed
    svn path=/trunk/openspaces/; revision=14389
    
    
    Former-commit-id: f460f032590ef6e1a3cf1f5f74726d2898c4a20c

--- src/main/src/org/openspaces/events/polling/AbstractPollingEventListenerContainer.java
+++ src/main/src/org/openspaces/events/polling/AbstractPollingEventListenerContainer.java
@@ -15,35 +15,35 @@ import org.springframework.util.Assert;
 /**
  * Base class for listener container implementations which are based on polling. Provides support
  * for listener handling based on Space take operations.
- * 
+ *
  * <p>
  * This listener container variant is built for repeated polling attempts, each invoking the
  * {@link #receiveAndExecute} method. The receive timeout for each attempt can be configured through
  * the {@link #setReceiveTimeout "receiveTimeout"} property.
- * 
+ *
  * <p>
  * The container allows to set the template object used for the operations. Note, this can be a Pojo
  * based template, or one of GigaSpace's query classes such as
  * {@link com.j_spaces.core.client.SQLQuery}.
- * 
+ *
  * <p>
  * A pluggable recieve operation handler can be provided by setting
  * {@link #setReceiveOperationHandler(org.openspaces.events.polling.receive.ReceiveOperationHandler)}.
  * The default handler used it
  * {@link org.openspaces.events.polling.receive.SingleTakeReceiveOperationHandler}.
- * 
+ *
  * <p>
  * Event reception and listener execution can automatically be wrapped in transactions through
  * passing a Spring {@link org.springframework.transaction.PlatformTransactionManager} into the
  * {@link #setTransactionManager transactionManager} property. This will usually be a
  * {@link org.openspaces.core.transaction.manager.LocalJiniTransactionManager LocalJiniTransactionManager}.
- * 
+ *
  * <p>
  * This base class does not assume any specific mechanism for asynchronous execution of polling
  * invokers. Check out {@link SimplePollingEventListenerContainer} for a concrete implementation
  * which is based on Spring's {@link org.springframework.core.task.TaskExecutor} abstraction,
  * including dynamic scaling of concurrent consumers and automatic self recovery.
- * 
+ *
  * <p>
  * The {@link #setTemplate(Object)} parameter is required in order to perform matching on which
  * events to receive. If the {@link #setEventListener(org.openspaces.events.SpaceDataEventListener)}
@@ -51,7 +51,7 @@ import org.springframework.util.Assert;
  * the event listener will be used to get the template. This feature helps when event listeners
  * directly can only work with a certain template and removes the requirement of configuring the
  * template as well.
- * 
+ *
  * <p>
  * An advance feature allows for pluggable
  * {@link #setTriggerOperationHandler(org.openspaces.events.polling.trigger.TriggerOperationHandler) triggerOperationHandler}
@@ -59,7 +59,7 @@ import org.springframework.util.Assert;
  * perform a trigger receive outside of a transaction scope, and if it returned a value, perform the
  * take within a transaction. A useful implementation of it is
  * {@link org.openspaces.events.polling.trigger.ReadTriggerOperationHandler}.
- * 
+ *
  * @author kimchy
  */
 public abstract class AbstractPollingEventListenerContainer extends AbstractEventListenerContainer {
@@ -72,7 +72,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
     private Object template;
 
     private boolean performSnapshot = true;
-    
+
     private PlatformTransactionManager transactionManager;
 
     private DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
@@ -88,7 +88,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
 
     /**
      * Sets the specified template to be used with the polling space operation.
-     * 
+     *
      * @see org.openspaces.core.GigaSpace#take(Object,long)
      */
     public void setTemplate(Object template) {
@@ -108,7 +108,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
      * If set to <code>true</code> will perform snaphot operation on the provided template
      * before invoking the receive operation. Defaults to <code>true</code>.
      *
-     * @see org.openspaces.core.GigaSpace#snapshot(Object) 
+     * @see org.openspaces.core.GigaSpace#snapshot(Object)
      */
     public void setPerformSnapshot(boolean performSnapshot) {
         this.performSnapshot = performSnapshot;
@@ -117,7 +117,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
     /**
      * Specify the Spring {@link org.springframework.transaction.PlatformTransactionManager} to use
      * for transactional wrapping of event reception plus listener execution.
-     * 
+     *
      * <p>Default is none, not performing any transactional wrapping.
      */
     public void setTransactionManager(PlatformTransactionManager transactionManager) {
@@ -135,7 +135,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
     /**
      * Specify the transaction name to use for transactional wrapping. Default is the bean name of
      * this listener container, if any.
-     * 
+     *
      * @see org.springframework.transaction.TransactionDefinition#getName()
      */
     public void setTransactionName(String transactionName) {
@@ -145,7 +145,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
     /**
      * Specify the transaction timeout to use for transactional wrapping, in <b>seconds</b>.
      * Default is none, using the transaction manager's default timeout.
-     * 
+     *
      * @see org.springframework.transaction.TransactionDefinition#getTimeout()
      * @see #setReceiveTimeout
      */
@@ -155,7 +155,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
 
     /**
      * Specify the transaciton isolation to use for transactional wrapping.
-     * 
+     *
      * @see org.springframework.transaction.support.DefaultTransactionDefinition#setIsolationLevel(int)
      */
     public void setTransactionIsolationLevel(int transactionIsolationLevel) {
@@ -164,7 +164,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
 
     /**
      * Specify the transaciton isolation to use for transactional wrapping.
-     * 
+     *
      * @see org.springframework.transaction.support.DefaultTransactionDefinition#setIsolationLevelName(String)
      */
     public void setTransactionIsolationLevelName(String transactionIsolationLevelName) {
@@ -174,10 +174,10 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
     /**
      * Set the timeout to use for receive calls, in <b>milliseconds</b>. The default is 60000 ms,
      * that is, 1 minute.
-     * 
+     *
      * <p><b>NOTE:</b> This value needs to be smaller than the transaction timeout used by the
      * transaction manager (in the appropriate unit, of course).
-     * 
+     *
      * @see org.openspaces.core.GigaSpace#take(Object,long)
      */
     public void setReceiveTimeout(long receiveTimeout) {
@@ -246,7 +246,7 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
     /**
      * Execute the listener for a message received from the given consumer, wrapping the entire
      * operation in an external transaction if demanded.
-     * 
+     *
      * @see #doReceiveAndExecute
      */
     protected boolean receiveAndExecute() throws DataAccessException, TransactionException {
@@ -278,7 +278,12 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
                 rollbackOnException(status, err);
                 throw err;
             }
-            this.transactionManager.commit(status);
+            // if no message is received, rollback the transaction (for better performace).
+            if (!messageReceived) {
+                this.transactionManager.rollback(status);
+            } else {
+                this.transactionManager.commit(status);
+            }
             return messageReceived;
         } else {
             // Execute receive outside of transaction.
@@ -340,11 +345,9 @@ public abstract class AbstractPollingEventListenerContainer extends AbstractEven
 
     /**
      * Perform a rollback, handling rollback exceptions properly.
-     * 
-     * @param status
-     *            object representing the transaction
-     * @param ex
-     *            the thrown application exception or error
+     *
+     * @param status object representing the transaction
+     * @param ex     the thrown application exception or error
      */
     private void rollbackOnException(TransactionStatus status, Throwable ex) {
         logger.trace(message("Initiating transaction rollback on application exception"), ex);
