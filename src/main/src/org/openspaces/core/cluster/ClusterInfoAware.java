c7e25b0870adc7d51e92ddf6de1af5eb3cc540a0
kimchy <kimchy@localhost>
2007-05-13 07:26:42 +0000

    fixed javadoc
    svn path=/trunk/openspaces/; revision=14437
    
    
    Former-commit-id: 01f5251c2d750cc8b222bdd7e3c955b05f34afa3

--- src/main/src/org/openspaces/core/cluster/ClusterInfoAware.java
+++ src/main/src/org/openspaces/core/cluster/ClusterInfoAware.java
@@ -3,8 +3,7 @@ package org.openspaces.core.cluster;
 /**
  * Allows for beans implementing this interface to be injected with {@link ClusterInfo}.
  * 
- * <p>
- * Note, the cluster information is obtained externally from the applicaiton context which means
+ * <p>Note, the cluster information is obtained externally from the applicaiton context which means
  * that this feature need to be supported by specific containers (and is not supported by plain
  * Spring application context). This means that beans that implementations of
  * {@link ClusterInfoAware} should take into account the fact that the cluster info provided might
@@ -15,11 +14,9 @@ package org.openspaces.core.cluster;
 public interface ClusterInfoAware {
 
     /**
-     * <p>
      * Sets the cluster information.
      * 
-     * <p>
-     * Note, the cluster information is obtained externally from the applicaiton context which means
+     * <p>Note, the cluster information is obtained externally from the applicaiton context which means
      * that this feature need to be supported by specific containers (and is not supported by plain
      * Spring application context). This means that beans that implement {@link ClusterInfoAware}
      * should take into account the fact that the cluster info provided might be null.
