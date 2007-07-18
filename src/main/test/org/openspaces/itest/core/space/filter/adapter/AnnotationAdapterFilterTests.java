a1c5daee4c1d2ac27504cdc881366044469bd52e
kimchy <kimchy@localhost>
2007-07-18 08:21:23 +0000

    added test verification for GigaSpacesLateContext in filter
    svn path=/trunk/openspaces/; revision=16177
    
    
    Former-commit-id: 9943c1b3369316a9ab9feef1def565da3fa8f723

--- src/main/test/org/openspaces/itest/core/space/filter/adapter/AnnotationAdapterFilterTests.java
+++ src/main/test/org/openspaces/itest/core/space/filter/adapter/AnnotationAdapterFilterTests.java
@@ -24,4 +24,8 @@ public class AnnotationAdapterFilterTests extends AbstractAdapterFilterTests {
     protected String[] getConfigLocations() {
         return new String[]{"/org/openspaces/itest/core/space/filter/adapter/adapter-annotation-filter.xml"};
     }
+
+    public void testGigaSpacesLateContext() {
+        assertNotNull(simpleFilter.gigaSpace);
+    }
 }
