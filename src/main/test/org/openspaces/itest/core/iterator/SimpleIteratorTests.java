4f29f868ae3edfeb57164cf51edd458f09cefcb3
kimchy <kimchy@localhost>
2007-09-05 18:57:31 +0000

    more schema documentation
    svn path=/trunk/openspaces/; revision=17849
    
    
    Former-commit-id: 601da287813bd015a880fb470dfe9e44342598d0

---
+++ src/main/test/org/openspaces/itest/core/iterator/SimpleIteratorTests.java
@@ -0,0 +1,36 @@
+package org.openspaces.itest.core.iterator;
+
+import com.j_spaces.core.client.GSIterator;
+import junit.framework.TestCase;
+import net.jini.core.entry.Entry;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.GigaSpaceConfigurer;
+import org.openspaces.core.space.UrlSpaceConfigurer;
+
+/**
+ * @author kimchy
+ */
+public class SimpleIteratorTests extends TestCase {
+
+    public void testSimpleIterator() {
+        UrlSpaceConfigurer urlSpaceConfigurer = new UrlSpaceConfigurer("/./space");
+        GigaSpace gigaSpace = new GigaSpaceConfigurer(urlSpaceConfigurer.space()).gigaSpace();
+
+        for (int i = 0; i < 50; i++) {
+            TestMessage testMessage = new TestMessage();
+            testMessage.id = i;
+            gigaSpace.write(testMessage);
+        }
+
+        GSIterator it = gigaSpace.iterator().withHistory().addTemplate(new TestMessage()).iterate();
+        int counter = 0;
+        for (Object test : it) {
+            counter++;
+        }
+        assertEquals(50, counter);
+    }
+
+    public static class TestMessage implements Entry {
+        public Integer id;
+    }
+}
