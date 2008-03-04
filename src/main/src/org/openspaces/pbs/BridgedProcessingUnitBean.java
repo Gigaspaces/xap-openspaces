d1917af14e87535026724460b38e8b740c655307
eitany <eitany@localhost>
2008-03-04 13:07:57 +0000

    GS-3966
    Added processing unit .net adapter to openspaces
    
    Reviewer: Shay B
    
    svn path=/trunk/openspaces/; revision=23324
    
    
    Former-commit-id: 43f258b9e72a960b27879ab2befc79d307427503

---
+++ src/main/src/org/openspaces/pbs/BridgedProcessingUnitBean.java
@@ -0,0 +1,75 @@
+package org.openspaces.pbs;
+
+import org.openspaces.core.cluster.ClusterInfo;
+import org.openspaces.core.cluster.ClusterInfoAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import com.gigaspaces.serialization.pbs.openspaces.ProcessingUnitProxy; 
+
+/**
+ * Bridged processing unit implementation
+ * 
+ * @author eitany
+ * @since 6.5
+ */
+public class BridgedProcessingUnitBean implements InitializingBean, DisposableBean, ClusterInfoAware {
+    private ProcessingUnitProxy _proxy;  
+    private String _assemblyFullPath;
+    private String _implementationName;
+    private String[] _dependencies;
+    private ClusterInfo _clusterInfo;
+
+    /**
+     * Injects the bridged processing unit implementation's assembly full path
+     * 
+     * @param assemblyFullPath
+     */
+    public void setAssemblyFullPath(String assemblyFullPath) {
+        _assemblyFullPath = assemblyFullPath;
+    }
+
+    /**
+     * Injects the bridged processing unit implementation's full type name
+     * 
+     * @param implementationName
+     */
+    public void setImplementationName(String implementationName) {
+        _implementationName = implementationName;
+    }
+
+    /**
+     * Injects the bridged processing unit implementation's dependencies
+     * 
+     * @param dependencies
+     */
+    public void setDependencies(String[] dependencies) {
+        _dependencies = dependencies;
+    }
+	/**
+	 * {@inheritDoc}
+	 */
+    public void afterPropertiesSet() throws Exception {  
+        _proxy = new ProcessingUnitProxy(_assemblyFullPath, _implementationName, _dependencies);
+        if (_clusterInfo == null) {
+            _proxy.init();
+        } else {
+            _proxy.init(_clusterInfo.getBackupId(), _clusterInfo.getInstanceId(), _clusterInfo.getNumberOfBackups(), _clusterInfo.getNumberOfInstances(), _clusterInfo.getSchema());
+        }
+        _proxy.start();
+    }
+	/**
+	 * {@inheritDoc}
+	 */
+    public void destroy() throws Exception {
+        _proxy.stop();
+        _proxy.destruct();
+        _proxy = null;
+    }
+	/**
+	 * {@inheritDoc}
+	 */
+    public void setClusterInfo(ClusterInfo clusterInfo) {
+        _clusterInfo = clusterInfo;
+    }
+
+}
