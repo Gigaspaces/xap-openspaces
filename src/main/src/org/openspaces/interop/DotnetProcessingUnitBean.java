462464b209009ed90a8a6f4b06c4fb59bb91e60a
eitany <eitany@localhost>
2008-03-31 13:05:17 +0000

    GS-3966
    Added Custom properties to processing unit
    Renamed BridgedProcessingUnitBean to DotnetProcessingUnitBean
    Renamed containing package to interop instead of pbs
    
    Reviewer: Niv
    
    svn path=/trunk/openspaces/; revision=24356
    
    
    Former-commit-id: c3fd03a4ba81d82d1b91819272bedbb5abe4642b

---
+++ src/main/src/org/openspaces/interop/DotnetProcessingUnitBean.java
@@ -0,0 +1,101 @@
+package org.openspaces.interop;
+
+import java.util.Properties;
+
+import org.jini.rio.boot.ServiceClassLoader;
+import org.openspaces.core.cluster.ClusterInfo;
+import org.openspaces.core.cluster.ClusterInfoAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import com.gigaspaces.serialization.pbs.openspaces.ProcessingUnitProxy; 
+
+/**
+ * Dotnet processing unit bean, used as an adapter that will delegate
+ * the life cycle method invocation to the .Net processing unit implementation of
+ * the .Net GigaSpaces.Core.IProcessingUnit interface 
+ * 
+ * @author eitany
+ * @since 6.5
+ */
+public class DotnetProcessingUnitBean implements InitializingBean, DisposableBean, ClusterInfoAware {
+    private ProcessingUnitProxy proxy;  
+    private String assemblyFullPath;
+    private String implementationClassName;
+    private String[] dependencies;
+    private ClusterInfo clusterInfo;
+    private Properties customProperties;
+
+    /**
+     * Injects the .Net processing unit implementation's assembly full path
+     * 
+     * @param assemblyFullPath
+     */
+    public void setAssemblyFullPath(String assemblyFullPath) {
+        this.assemblyFullPath = assemblyFullPath;
+    }
+
+    /**
+     * Injects the .Net processing unit implementation class name
+     * 
+     * @param implementationName
+     */
+    public void setImplementationClassName(String implementationClassName) {
+        this.implementationClassName = implementationClassName;
+    }
+
+    /**
+     * Injects the .Net processing unit implementation's dependencies
+     * 
+     * @param dependencies
+     */
+    public void setDependencies(String[] dependencies) {
+        this.dependencies = dependencies;
+    }
+    
+    /**
+     * Injects the .Net processing unit custom properties that will be passed
+     * to the init method
+     * 
+     * @param customProperties
+     */
+    public void setCustomProperties(Properties customProperties)
+    {
+        this.customProperties = customProperties;
+    }
+	/**
+	 * {@inheritDoc}
+	 */
+    public void afterPropertiesSet() throws Exception {  
+        
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            if (classLoader instanceof ServiceClassLoader) {
+                Thread.currentThread().setContextClassLoader(classLoader.getParent());
+            }
+            proxy = new ProcessingUnitProxy(assemblyFullPath, implementationClassName, dependencies);
+            if (clusterInfo == null) {
+                proxy.init(customProperties);
+            } else {
+                proxy.init(customProperties, clusterInfo.getBackupId(), clusterInfo.getInstanceId(), clusterInfo.getNumberOfBackups(), clusterInfo.getNumberOfInstances(), clusterInfo.getSchema());
+            }
+            proxy.start();
+        } finally {
+            Thread.currentThread().setContextClassLoader(classLoader);
+        }
+    }
+	/**
+	 * {@inheritDoc}
+	 */
+    public void destroy() throws Exception {
+        proxy.stop();
+        proxy.destruct();
+        proxy = null;
+    }
+	/**
+	 * {@inheritDoc}
+	 */
+    public void setClusterInfo(ClusterInfo clusterInfo) {
+        this.clusterInfo = clusterInfo;
+    }
+
+}
