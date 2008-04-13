12c93b2a647ff4ba71daee69128ebebf6053ad81
shaiw <shaiw@localhost>
2008-04-13 14:06:40 +0000

    GS-2496
    GigaSpaces can be installed on a path that contains a space.
    Made a small change in the Locator and updated all Windows scripts.
    Unix scripts did not need altering.
    
    Reviewed by Gershon.
    
    svn path=/trunk/gigaspaces/; revision=24880
    
    
    Former-commit-id: 96e3a102a54edde7ea7d2232d4a0b88ed08d4781

--- bin/installmavenrep.bat
+++ bin/installmavenrep.bat
@@ -1,18 +1,18 @@
 @echo off
 
+if "%1" == ""  (
 @rem The call to setenv.bat can be commented out if necessary.
-@call %~dp0\setenv.bat
-
-set JSHOMEDIR=%~dp0\..
+@call "%~dp0\setenv.bat"
+)
 
 if "%1" == "version"  (
-%JAVACMD% -cp %JSHOMEDIR%/lib/JSpaces.jar;%JSHOMEDIR%/lib/openspaces/openspaces.jar org.openspaces.maven.support.OutputVersion
+%JAVACMD% -cp "%JSHOMEDIR%/lib/JSpaces.jar";"%JSHOMEDIR%/lib/openspaces/openspaces.jar" org.openspaces.maven.support.OutputVersion
 goto END
 )
 
 FOR /F "usebackq tokens=*" %%i IN (`installmavenrep.bat version`) DO @set VERSION=%%i
 
-%JAVACMD% -cp %JSHOMEDIR%/lib/JSpaces.jar;%JSHOMEDIR%/lib/openspaces/openspaces.jar org.openspaces.maven.support.POMGenerator %TEMP% %VERSION% ../lib/openspaces/maven-openspaces-plugin/
+%JAVACMD% -cp "%JSHOMEDIR%/lib/JSpaces.jar";"%JSHOMEDIR%/lib/openspaces/openspaces.jar" org.openspaces.maven.support.POMGenerator %TEMP% %VERSION% ../lib/openspaces/maven-openspaces-plugin/
 
 echo ""
 echo ""
@@ -21,20 +21,20 @@ echo ""
 echo ""
 
 REM Jini Jars
-@call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-start -Dversion=%VERSION% -DpomFile=%TEMP%/jini-start-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/jini/start.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-lib -Dversion=%VERSION% -DpomFile=%TEMP%/jini-jsk-lib-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/jini/jsk-lib.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-platform -Dversion=%VERSION% -DpomFile=%TEMP%/jini-jsk-platform-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/jini/jsk-platform.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-resources -Dversion=%VERSION% -DpomFile=%TEMP%/jini-jsk-resources-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/jini/jsk-resources.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-reggie -Dversion=%VERSION% -DpomFile=%TEMP%/jini-reggie-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/jini/reggie.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-mahalo -Dversion=%VERSION% -DpomFile=%TEMP%/jini-mahalo-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/jini/mahalo.jar
+@call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-start -Dversion=%VERSION% -DpomFile=%TEMP%/jini-start-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/jini/start.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-lib -Dversion=%VERSION% -DpomFile=%TEMP%/jini-jsk-lib-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/jini/jsk-lib.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-platform -Dversion=%VERSION% -DpomFile=%TEMP%/jini-jsk-platform-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/jini/jsk-platform.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-resources -Dversion=%VERSION% -DpomFile=%TEMP%/jini-jsk-resources-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/jini/jsk-resources.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-reggie -Dversion=%VERSION% -DpomFile=%TEMP%/jini-reggie-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/jini/reggie.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-mahalo -Dversion=%VERSION% -DpomFile=%TEMP%/jini-mahalo-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/jini/mahalo.jar"
 
 REM GigaSpaces Jars
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-boot -Dversion=%VERSION% -DpomFile=%TEMP%/gs-boot-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/ServiceGrid/gs-boot.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-service -Dversion=%VERSION% -DpomFile=%TEMP%/gs-service-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/ServiceGrid/gs-service.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-lib -Dversion=%VERSION% -DpomFile=%TEMP%/gs-lib-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/ServiceGrid/gs-lib.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=JSpaces -Dversion=%VERSION% -DpomFile=%TEMP%/JSpaces-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/JSpaces.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=openspaces -Dversion=%VERSION% -DpomFile=%TEMP%/openspaces-pom.xml -Dpackaging=jar -Dfile=%JSHOMEDIR%/lib/openspaces/openspaces.jar
-call mvn install:install-file -DgroupId=gigaspaces -DartifactId=mule-os -Dversion=%VERSION% -Dpackaging=jar -DpomFile=%TEMP%/mule-os-pom.xml -Dfile=%JSHOMEDIR%/lib/openspaces/mule-os.jar
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-boot -Dversion=%VERSION% -DpomFile=%TEMP%/gs-boot-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/ServiceGrid/gs-boot.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-service -Dversion=%VERSION% -DpomFile=%TEMP%/gs-service-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/ServiceGrid/gs-service.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-lib -Dversion=%VERSION% -DpomFile=%TEMP%/gs-lib-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/ServiceGrid/gs-lib.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=JSpaces -Dversion=%VERSION% -DpomFile=%TEMP%/JSpaces-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/JSpaces.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=openspaces -Dversion=%VERSION% -DpomFile=%TEMP%/openspaces-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/openspaces/openspaces.jar"
+call mvn install:install-file -DgroupId=gigaspaces -DartifactId=mule-os -Dversion=%VERSION% -Dpackaging=jar -DpomFile=%TEMP%/mule-os-pom.xml -Dfile="%JSHOMEDIR%/lib/openspaces/mule-os.jar"
 
 REM Copy licenese file
 copy "%JSHOMEDIR%\gslicense.xml" "%HOMEDRIVE%\%HOMEPATH%\.m2\repository\gigaspaces\gs-boot\%VERSION%"
