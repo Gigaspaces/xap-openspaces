fd5ee400f166c5f515a07c96f74c50ab0b4e4a8d
kimchy <kimchy@localhost>
2008-04-28 10:05:35 +0000

    updated the default maven template with better javadoc and comments
    svn path=/trunk/openspaces/; revision=25766
    
    
    Former-commit-id: e50d1f2d28d3546cc5b113f153a40a939efaae7b

--- maven/maven-openspaces-plugin/src/main/resources/templates/default/common/src/Data.java
+++ maven/maven-openspaces-plugin/src/main/resources/templates/default/common/src/Data.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2006-2007 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ${puGroupId}.common;
 
 import com.gigaspaces.annotation.pojo.SpaceClass;
@@ -27,14 +11,9 @@ import java.io.Serializable;
  * of the object, a type (used to perform routing when working with partitioned space),
  * the raw data and processed data, and a boolean flag indicating if this Data object
  * was processed or not.
- *
- * <p>Note, this object implements Serializable because it is used as a parameter when
- * using OpenSpaces remoting support.
- *
- * @author kimchy
  */
 @SpaceClass
-public class Data implements Serializable {
+public class Data {
 
     private String id;
 
--- maven/maven-openspaces-plugin/src/main/resources/templates/default/feeder/src/Feeder.java
+++ maven/maven-openspaces-plugin/src/main/resources/templates/default/feeder/src/Feeder.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2006-2007 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ${puGroupId}.feeder;
 
 import org.openspaces.core.GigaSpace;
@@ -29,7 +13,8 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 /**
- * A feeder bean started a scheduled task that writes a new Data object to the space.
+ * A feeder bean started a scheduled task that writes a new Data objects to the space 
+ * (in an unprocessed state).
  *
  * <p>The space is injected into this bean using OpenSpaces support for @GigaSpaceContext
  * annotation.
