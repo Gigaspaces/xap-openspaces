8afb6747f99d8e518edac72d344f50c5121f51a9
kimchy <kimchy@localhost>
2008-05-20 07:40:04 +0000

    updated the installmavenrep.sh to install using the correct group
    svn path=/trunk/openspaces/; revision=26939
    
    
    Former-commit-id: b1314c6fdc037fd6aa75a68778e827720a775f4f

--- tools/maven/installmavenrep.sh
+++ tools/maven/installmavenrep.sh
@@ -20,20 +20,20 @@ echo ""
 echo ""
 
 # Jini Jars
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-start -Dversion=$VERSION -DpomFile=$TMPDIR/jini-start-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/start.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-lib -Dversion=$VERSION -DpomFile=$TMPDIR/jini-jsk-lib-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/jsk-lib.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-platform -Dversion=$VERSION -DpomFile=$TMPDIR/jini-jsk-platform-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/jsk-platform.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-jsk-resources -Dversion=$VERSION -DpomFile=$TMPDIR/jini-jsk-resources-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/jsk-resources.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-reggie -Dversion=$VERSION -DpomFile=$TMPDIR/jini-reggie-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/reggie.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=jini-mahalo -Dversion=$VERSION -DpomFile=$TMPDIR/jini-mahalo-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/mahalo.jar
+mvn install:install-file -DgroupId=com.gigaspaces.jini -DartifactId=jini-start -Dversion=$VERSION -DpomFile=$TMPDIR/jini-start-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/start.jar
+mvn install:install-file -DgroupId=com.gigaspaces.jini -DartifactId=jini-jsk-lib -Dversion=$VERSION -DpomFile=$TMPDIR/jini-jsk-lib-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/jsk-lib.jar
+mvn install:install-file -DgroupId=com.gigaspaces.jini -DartifactId=jini-jsk-platform -Dversion=$VERSION -DpomFile=$TMPDIR/jini-jsk-platform-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/jsk-platform.jar
+mvn install:install-file -DgroupId=com.gigaspaces.jini -DartifactId=jini-jsk-resources -Dversion=$VERSION -DpomFile=$TMPDIR/jini-jsk-resources-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/jsk-resources.jar
+mvn install:install-file -DgroupId=com.gigaspaces.jini -DartifactId=jini-reggie -Dversion=$VERSION -DpomFile=$TMPDIR/jini-reggie-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/reggie.jar
+mvn install:install-file -DgroupId=com.gigaspaces.jini -DartifactId=jini-mahalo -Dversion=$VERSION -DpomFile=$TMPDIR/jini-mahalo-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/jini/mahalo.jar
 
 # GigaSpaces Jars
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-boot -Dversion=$VERSION -DpomFile=$TMPDIR/gs-boot-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/ServiceGrid/gs-boot.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-service -Dversion=$VERSION -DpomFile=$TMPDIR/gs-service-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/ServiceGrid/gs-service.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=gs-lib -Dversion=$VERSION -DpomFile=$TMPDIR/gs-lib-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/ServiceGrid/gs-lib.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=JSpaces -Dversion=$VERSION -DpomFile=$TMPDIR/JSpaces-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/JSpaces.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=openspaces -Dversion=$VERSION -DpomFile=$TMPDIR/openspaces-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/openspaces/openspaces.jar
-mvn install:install-file -DgroupId=gigaspaces -DartifactId=mule-os -Dversion=$VERSION -Dpackaging=jar -DpomFile=$TMPDIR/mule-os-pom.xml -Dfile=${JSHOMEDIR}/lib/openspaces/mule-os.jar
+mvn install:install-file -DgroupId=com.gigaspaces.core -DartifactId=gs-boot -Dversion=$VERSION -DpomFile=$TMPDIR/gs-boot-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/ServiceGrid/gs-boot.jar
+mvn install:install-file -DgroupId=com.gigaspaces.core -DartifactId=gs-service -Dversion=$VERSION -DpomFile=$TMPDIR/gs-service-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/ServiceGrid/gs-service.jar
+mvn install:install-file -DgroupId=com.gigaspaces.core -DartifactId=gs-lib -Dversion=$VERSION -DpomFile=$TMPDIR/gs-lib-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/ServiceGrid/gs-lib.jar
+mvn install:install-file -DgroupId=com.gigaspaces.core -DartifactId=JSpaces -Dversion=$VERSION -DpomFile=$TMPDIR/JSpaces-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/JSpaces.jar
+mvn install:install-file -DgroupId=org.openspaces -DartifactId=openspaces -Dversion=$VERSION -DpomFile=$TMPDIR/openspaces-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/openspaces/openspaces.jar
+mvn install:install-file -DgroupId=org.openspaces -DartifactId=mule-os -Dversion=$VERSION -Dpackaging=jar -DpomFile=$TMPDIR/mule-os-pom.xml -Dfile=${JSHOMEDIR}/lib/openspaces/mule-os.jar
 
 # Build and install OpenSpaces Maven Plugin
 mvn -f maven-openspaces-plugin/pom.xml install
