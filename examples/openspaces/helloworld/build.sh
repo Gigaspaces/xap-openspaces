0c6426bd502337799319bac900be42075503f534
kimchy <kimchy@localhost>
2008-06-23 12:43:09 +0000

    GS-3898 Build fails for openspaces/helloworld application.
    svn path=/trunk/openspaces/; revision=28481
    
    
    Former-commit-id: 898ad0c86f8d995d610e803485b8811155da98b4

--- examples/openspaces/helloworld/build.sh
+++ examples/openspaces/helloworld/build.sh
@@ -1,4 +1,4 @@
 JSHOMEDIR=`dirname $0`/../../..; export JSHOMEDIR
 . ${JSHOMEDIR}/bin/setenv.sh
 
-"$JAVACMD" ${LOOKUP_GROUPS_PROP} -classpath "${ANT_JARS}" org.apache.tools.ant.Main $1
+"$JAVACMD" ${LOOKUP_GROUPS_PROP} -classpath "${ANT_JARS}":"${JAVA_HOME}/lib/tools.jar" org.apache.tools.ant.Main $1
