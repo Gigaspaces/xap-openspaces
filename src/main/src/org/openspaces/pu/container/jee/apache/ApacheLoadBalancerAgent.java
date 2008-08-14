8d3bb9b1354851a1c632242a35e7e2296a37c38d
kimchy <kimchy@localhost>
2008-08-14 00:25:19 +0000

    GS-5411
    Add apache-agent that automatically identifies changes in the cluster and modifies apache load balancer
    
    (inital support)
    
    svn path=/trunk/openspaces/; revision=37081
    
    
    Former-commit-id: 3103e0bbd11cb2a395935e0a30245a634bec6f46

--- src/main/src/org/openspaces/pu/container/jee/apache/ApacheLoadBalancerAgent.java
+++ src/main/src/org/openspaces/pu/container/jee/apache/ApacheLoadBalancerAgent.java
@@ -16,38 +16,65 @@
 
 package org.openspaces.pu.container.jee.apache;
 
+import com.gigaspaces.logger.GSLogConfigLoader;
+import com.j_spaces.core.Constants;
 import com.j_spaces.kernel.PlatformVersion;
 import com.j_spaces.kernel.SecurityPolicyLoader;
-import com.j_spaces.core.Constants;
-import com.gigaspaces.logger.GSLogConfigLoader;
-
-import java.util.StringTokenizer;
-import java.util.Arrays;
-
-import org.openspaces.pu.container.support.CommandLineParser;
-import org.jini.rio.boot.BootUtil;
-import org.apache.commons.logging.Log;
-import net.jini.discovery.LookupDiscoveryManager;
-import net.jini.discovery.DiscoveryListener;
+import net.jini.core.lookup.ServiceTemplate;
 import net.jini.discovery.DiscoveryEvent;
-import net.jini.lookup.ServiceDiscoveryManager;
+import net.jini.discovery.DiscoveryListener;
+import net.jini.discovery.LookupDiscoveryManager;
 import net.jini.lookup.LookupCache;
-import net.jini.lookup.ServiceDiscoveryListener;
 import net.jini.lookup.ServiceDiscoveryEvent;
+import net.jini.lookup.ServiceDiscoveryListener;
+import net.jini.lookup.ServiceDiscoveryManager;
+import org.jini.rio.boot.BootUtil;
+import org.openspaces.core.cluster.ClusterInfo;
+import org.openspaces.pu.container.servicegrid.JeePUServiceDetails;
+import org.openspaces.pu.container.servicegrid.PUServiceBean;
+import org.openspaces.pu.container.servicegrid.PUServiceDetails;
+import org.openspaces.pu.container.support.CommandLineParser;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * Experimental support for automatically refreshing apache load balancer.
+ *
+ *
+ * <p>Note, internal API here on how to find processing units is subject to change.
+ *
  * @author kimchy
  */
-public class ApacheLoadBalancerAgent implements DiscoveryListener, ServiceDiscoveryListener {
+public class ApacheLoadBalancerAgent implements DiscoveryListener, ServiceDiscoveryListener, Runnable {
 
     private String[] groups;
 
     private String locators;
 
+    private String apachectlLocation;
+
+    private String configLocation;
+
 
     private LookupDiscoveryManager ldm;
     private ServiceDiscoveryManager sdm;
     private LookupCache cache;
+
+    private Map<String, LoadBalancersInfo> loadBalancersInfoMap = new ConcurrentHashMap<String, LoadBalancersInfo>();
+
+    private volatile boolean running = false;
+
+    private Thread configThread;
+
+    private int updateInterval = 10000;
 
     public String[] getGroups() {
         if (groups == null) {
@@ -84,38 +111,310 @@ public class ApacheLoadBalancerAgent implements DiscoveryListener, ServiceDiscov
         this.locators = locators;
     }
 
+    public String getApachectlLocation() {
+        return apachectlLocation;
+    }
+
+    public void setApachectlLocation(String apachectlLocation) {
+        this.apachectlLocation = apachectlLocation;
+    }
+
+    public String getConfigLocation() {
+        return configLocation;
+    }
+
+    public void setConfigLocation(String configLocation) {
+        this.configLocation = configLocation;
+    }
+
+    public int getUpdateInterval() {
+        return updateInterval;
+    }
+
+    public void setUpdateInterval(int updateInterval) {
+        this.updateInterval = updateInterval;
+    }
+
     public void start() throws Exception {
+        System.out.println("Starting Apache Load Balancer Agent...");
+        System.out.println("");
+        System.out.println("groups " + Arrays.toString(getGroups()) + ", locators [" + locators + "]");
+
+        if (apachectlLocation == null) {
+            throw new IllegalArgumentException("apachectl must be provided");
+        }
+        if (!new File(apachectlLocation).exists()) {
+            throw new IllegalArgumentException("apacheclt Location [" + apachectlLocation + "] does not exists");
+        }
+        if (configLocation == null) {
+            throw new IllegalArgumentException("config directory location must be provided");
+        }
+        new File(configLocation).mkdirs();
+
+        System.out.println("apachectl Location [" + apachectlLocation + "]");
+        System.out.println("config directory [" + configLocation + "]");
+        System.out.println("update config interval [" + updateInterval + "ms]");
+        System.out.println("");
+
+        loadBalancersInfoMap.clear();
+        // list all the files and init with empty load balancers
+        System.out.println("Detecting exising config files...");
+        new File(configLocation).listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                String clusterName = name.substring(0, name.length() - ".conf".length());
+                System.out.println("[" + clusterName + "]: existing config detected");
+                loadBalancersInfoMap.put(clusterName, new LoadBalancersInfo());
+                return false;
+            }
+        });
+        System.out.println("Done detecting exising config files");
+        System.out.println("");
+
         ldm = new LookupDiscoveryManager(getGroups(), BootUtil.toLookupLocators(getLocators()), this);
 
         sdm = new ServiceDiscoveryManager(ldm, null);
-        cache = sdm.createLookupCache(null, null, this);
+        ServiceTemplate template = new ServiceTemplate(null, new Class[]{PUServiceBean.class}, null);
+        cache = sdm.createLookupCache(template, null, this);
 
-        System.out.println("Starting Apache Load Balancer Agent");
-        System.out.println("Groups " + Arrays.toString(getGroups()) + ", Locators [" + locators + "]");
+        running = true;
+        configThread = new Thread(this, "Config Writer Thread");
+        configThread.setDaemon(false);
+        configThread.start();
+
+        System.out.println("");
+        System.out.println("Started Apache Load Balancer Agent successfully");
+        System.out.println("");
     }
 
     public void stop() {
-        System.out.println("Stopping Apached Load Balancer Agent");
+        running = false;
+        System.out.println("Stopping Apached Load Balancer Agent...");
+        cache.terminate();
+        sdm.terminate();
+        ldm.terminate();
+        System.out.println("Stopped Apached Load Balancer Agent");
     }
 
     public void discovered(DiscoveryEvent event) {
-        System.out.println("LUS Discovered [" + event + "]");
+        System.out.println("LUS Discovered " + Arrays.toString(event.getRegistrars()));
     }
 
     public void discarded(DiscoveryEvent event) {
-        System.out.println("LUS Discarded [" + event + "]");
+        System.out.println("LUS Discarded " + Arrays.toString(event.getRegistrars()));
     }
 
-    public void serviceAdded(ServiceDiscoveryEvent event) {
-        System.out.println("Service Added [" + event + "]");
+    public synchronized void serviceAdded(ServiceDiscoveryEvent event) {
+        PUServiceBean service = (PUServiceBean) event.getPostEventServiceItem().service;
+        try {
+            PUServiceDetails[] details = service.listServiceDetails();
+            ClusterInfo clusterInfo = service.getClusterInfo();
+            for (PUServiceDetails detail : details) {
+                if (detail instanceof JeePUServiceDetails) {
+                    JeePUServiceDetails jeeDetails = (JeePUServiceDetails) detail;
+                    LoadBalancersInfo loadBalancersInfo = loadBalancersInfoMap.get(clusterInfo.getName());
+                    if (loadBalancersInfo == null) {
+                        loadBalancersInfo = new LoadBalancersInfo();
+                        loadBalancersInfoMap.put(clusterInfo.getName(), loadBalancersInfo);
+                    }
+                    loadBalancersInfo.putBalancer(new LoadBalancerInfo(clusterInfo, (JeePUServiceDetails) detail));
+                    loadBalancersInfo.setDirty(true);
+
+                    System.out.println("[" + clusterInfo.getName() + "]: Adding [" + clusterInfo.getRunningNumberOffset1() + "] [" + jeeDetails.getHost() + ":" + jeeDetails.getPort() + jeeDetails.getContextPath() + "]");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to add service");
+            e.printStackTrace(System.out);
+        }
     }
 
-    public void serviceRemoved(ServiceDiscoveryEvent event) {
-        System.out.println("Service Removed [" + event + "]");
+    public synchronized void serviceRemoved(ServiceDiscoveryEvent event) {
+        PUServiceBean service = (PUServiceBean) event.getPreEventServiceItem().service;
+        try {
+            PUServiceDetails[] details = service.listServiceDetails();
+            ClusterInfo clusterInfo = service.getClusterInfo();
+            for (PUServiceDetails detail : details) {
+                JeePUServiceDetails jeeDetails = (JeePUServiceDetails) detail;
+                if (detail instanceof JeePUServiceDetails) {
+                    LoadBalancersInfo loadBalancersInfo = loadBalancersInfoMap.get(clusterInfo.getName());
+                    if (loadBalancersInfo == null) {
+                        continue;
+                    }
+                    loadBalancersInfo.removeBalancer(new LoadBalancerInfo(clusterInfo, (JeePUServiceDetails) detail));
+                    loadBalancersInfo.setDirty(true);
+                    System.out.println("[" + clusterInfo.getName() + "]: Removing [" + clusterInfo.getRunningNumberOffset1() + "] [" + jeeDetails.getHost() + ":" + jeeDetails.getPort() + jeeDetails.getContextPath() + "]");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to remove service");
+            e.printStackTrace(System.out);
+        }
     }
 
-    public void serviceChanged(ServiceDiscoveryEvent event) {
+    public synchronized void serviceChanged(ServiceDiscoveryEvent event) {
         // don't care about this one
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                Thread.sleep(updateInterval);
+            } catch (InterruptedException e) {
+                break;
+            }
+            boolean dirty = false;
+            for (Map.Entry<String, LoadBalancersInfo> entry : loadBalancersInfoMap.entrySet()) {
+                LoadBalancerInfo[] infos = null;
+                synchronized (this) {
+                    if (entry.getValue().isDirty()) {
+                        infos = entry.getValue().getBlanacers();
+                        entry.getValue().setDirty(false);
+                    }
+                }
+                if (infos == null) {
+                    continue;
+                }
+                dirty = true;
+                System.out.println("[" + entry.getKey() + "]: Detected as dirty, updating config file...");
+                File confFile = new File(configLocation + "/" + entry.getKey() + ".conf");
+                try {
+                    PrintWriter writer = new PrintWriter(new FileOutputStream(confFile));
+                    writer.println("ProxyPass /" + entry.getKey() + " balancer://" + entry.getKey() + "_cluster/ stickysession=JSESSIONID nofailover=Off");
+                    writer.println("");
+                    writer.println("<Proxy balancer://" + entry.getKey() + "_cluster>");
+                    for (LoadBalancerInfo info : infos) {
+                        JeePUServiceDetails serviceDetails = info.getServiceDetails();
+                        ClusterInfo clusterInfo = info.getClusterInfo();
+                        writer.println("\tBalancerMember http://" + serviceDetails.getHost() + ":" + serviceDetails.getPort() + serviceDetails.getContextPath() + " route=" + clusterInfo.getName() + clusterInfo.getRunningNumberOffset1());
+                    }
+                    writer.println("</Proxy>");
+                    writer.flush();
+                    writer.close();
+                    System.out.println("[" + entry.getKey() + "]: Updated config file");
+                } catch (Exception e) {
+                    System.out.println("Failed to write config file, will try again later");
+                    e.printStackTrace(System.out);
+                    entry.getValue().setDirty(true);
+                }
+            }
+            if (dirty) {
+                String command = apachectlLocation + " graceful";
+                System.out.println("Executing [" + command + "]...");
+                Process process = null;
+                try {
+                    process = Runtime.getRuntime().exec(apachectlLocation + " graceful");
+                    waitFor(process, 60000);
+                    int exitValue = process.exitValue();
+                    System.out.println("Executed [" + command + "], exit code [" + exitValue + "]");
+                } catch (IOException e) {
+                    System.out.println("Failed to run [" + command + "]");
+                    e.printStackTrace(System.out);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (process != null) {
+                        try {
+                            process.getErrorStream().close();
+                        } catch (Exception e) {
+                            // do nothing
+                        }
+                        try {
+                            process.getInputStream().close();
+                        } catch (Exception e) {
+                            // do nothing
+                        }
+                        try {
+                            process.getOutputStream().close();
+                        } catch (Exception e) {
+                            // do mothing
+                        }
+                        try {
+                            process.destroy();
+                        } catch (Exception e) {
+                            // do nothing
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    private boolean waitFor(Process process, long timeout) throws InterruptedException {
+        /* interval constant */
+        final int interval = 200; // 200 milliseconds
+        long timeWaiting = 0;
+
+        while (timeWaiting < timeout) {
+            if (!isProcessAlive(process))
+                return true;
+
+            /* process still alive, wait next interval */
+            Thread.sleep(interval);
+            timeWaiting += interval;
+        }
+
+        /* process hasn't been destroyed */
+        return false;
+    }
+
+    /**
+     * @return <code>true</code> if supplied process is still alive, otherwise <code>false</code>
+     */
+    private boolean isProcessAlive(Process process) {
+        try {
+            process.exitValue();
+            return false;
+        } catch (IllegalThreadStateException e) {
+            return true;
+        }
+    }
+
+    public class LoadBalancersInfo {
+
+        private Map<Integer, LoadBalancerInfo> balancers = new ConcurrentHashMap<Integer, LoadBalancerInfo>();
+
+        private volatile boolean dirty = true;
+
+        public boolean isDirty() {
+            return dirty;
+        }
+
+        public void setDirty(boolean dirty) {
+            this.dirty = dirty;
+        }
+
+        public void putBalancer(LoadBalancerInfo balancerInfo) {
+            balancers.put(balancerInfo.getClusterInfo().getRunningNumberOffset1(), balancerInfo);
+        }
+
+        public void removeBalancer(LoadBalancerInfo balancerInfo) {
+            balancers.remove(balancerInfo.getClusterInfo().getRunningNumberOffset1());
+        }
+
+        public LoadBalancerInfo[] getBlanacers() {
+            return balancers.values().toArray(new LoadBalancerInfo[0]);
+        }
+    }
+
+    public class LoadBalancerInfo {
+
+        private ClusterInfo clusterInfo;
+
+        private JeePUServiceDetails serviceDetails;
+
+        public LoadBalancerInfo(ClusterInfo clusterInfo, JeePUServiceDetails serviceDetails) {
+            this.clusterInfo = clusterInfo;
+            this.serviceDetails = serviceDetails;
+        }
+
+        public ClusterInfo getClusterInfo() {
+            return clusterInfo;
+        }
+
+        public JeePUServiceDetails getServiceDetails() {
+            return serviceDetails;
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -125,7 +424,10 @@ public class ApacheLoadBalancerAgent implements DiscoveryListener, ServiceDiscov
         }
         final ApacheLoadBalancerAgent agent = new ApacheLoadBalancerAgent();
 
-        CommandLineParser.Parameter[] params = CommandLineParser.parse(args, args.length - 1);
+        CommandLineParser.Parameter[] params = CommandLineParser.parse(args, args.length);
+        String apachectlLocation = null;
+        String apcaheLocation = null;
+        String configLocation = null;
         for (CommandLineParser.Parameter param : params) {
             if (param.getName().equalsIgnoreCase("groups")) {
                 agent.setGroups(param.getArguments());
@@ -137,6 +439,33 @@ public class ApacheLoadBalancerAgent implements DiscoveryListener, ServiceDiscov
                 }
                 agent.setLocators(sb.toString());
             }
+            if (param.getName().equalsIgnoreCase("apache")) {
+                apcaheLocation = param.getArguments()[0];
+            }
+            if (param.getName().equalsIgnoreCase("apachectl")) {
+                apachectlLocation = param.getArguments()[0];
+            }
+            if (param.getName().equalsIgnoreCase("conf-dir")) {
+                configLocation = param.getArguments()[0];
+            }
+            if (param.getName().equalsIgnoreCase("update-interval")) {
+                agent.setUpdateInterval(Integer.parseInt(param.getArguments()[0]));
+            }
+        }
+        if (apachectlLocation != null) {
+            agent.setApachectlLocation(apachectlLocation);
+        } else if (apcaheLocation != null) {
+            agent.setApachectlLocation(apcaheLocation + "/bin/apachectl");
+        } else {
+            throw new IllegalArgumentException("Either apache location or apachectl location must be provided");
+        }
+
+        if (configLocation != null) {
+            agent.setConfigLocation(configLocation);
+        } else if (apcaheLocation != null) {
+            agent.setConfigLocation(apcaheLocation + "/conf/gigaspaces");
+        } else {
+            throw new IllegalArgumentException("Either conig director location or apache location must be provied");
         }
 
         try {
---
+++ tools/apache/apache-lb-agent.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# The call to setenv.sh can be commented out if necessary.
+export JSHOMEDIR=`dirname $0`/../../
+. `dirname $0`/../../bin/setenv.sh
+
+bootclasspath="-Xbootclasspath/p:$XML_JARS"
+
+JAVACMD="${JAVA_HOME}/bin/java"
+
+# Check for Cygwin
+cygwin=
+case $OS in
+    Windows*)
+        cygwin=1
+esac
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if [ "$cygwin" = "1" ]; then
+    CPS=";"
+else
+    CPS=":"
+fi
+export CPS
+
+LOOKUP_GROUPS_PROP=-Dcom.gs.jini_lus.groups=${LOOKUPGROUPS}; export LOOKUP_GROUPS_PROP
+
+if [ "${LOOKUPLOCATORS}" = "" ] ; then
+LOOKUPLOCATORS=; export LOOKUPLOCATORS
+fi
+LOOKUP_LOCATORS_PROP="-Dcom.gs.jini_lus.locators=${LOOKUPLOCATORS}"; export LOOKUP_LOCATORS_PROP
+
+COMMAND_LINE="${JAVACMD} ${JAVA_OPTIONS} $bootclasspath ${RMI_OPTIONS} ${LOOKUP_LOCATORS_PROP} ${LOOKUP_GROUPS_PROP} -Djava.security.policy=${POLICY} -Dcom.gs.home=${JSHOMEDIR} -classpath "${PRE_CLASSPATH}${CPS}${COMMON_JARS}${CPS}${SPRING_JARS}${CPS}${EXT_JARS}$CPS${JDBC_JARS}$CPS${JSHOMEDIR}${CPS}${JSHOMEDIR}/lib/JSpaces.jar${CPS}${OPENSPACES_JARS}${CPS}${POST_CLASSPATH}" org.openspaces.pu.container.jee.apache.ApacheLoadBalancerAgent $*"
+
+echo
+echo
+echo Starting apache-lb-agent with line:
+echo ${COMMAND_LINE}
+
+${COMMAND_LINE}
+echo
+echo
