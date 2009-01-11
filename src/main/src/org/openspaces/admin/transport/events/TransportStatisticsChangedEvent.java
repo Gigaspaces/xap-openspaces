e3fb89feef417f227ed697469d33a554cf2d0c44
kimchy <kimchy@localhost>
2009-01-11 14:53:40 +0000

    admin javadoc
    svn path=/trunk/openspaces/; revision=47902
    
    
    Former-commit-id: 9b9fb87e32838db566c6d68ef256480eb7b189fb

--- src/main/src/org/openspaces/admin/transport/events/TransportStatisticsChangedEvent.java
+++ src/main/src/org/openspaces/admin/transport/events/TransportStatisticsChangedEvent.java
@@ -1,9 +1,33 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openspaces.admin.transport.events;
 
 import org.openspaces.admin.transport.Transport;
 import org.openspaces.admin.transport.TransportStatistics;
 
 /**
+ * An event indicating that a transport level statistics has changed.
+ *
+ * <p>Note, monitoring needs to be enabled in order to receive the events.
+ *
+ * @author kimchy
+ * @see TransportStatisticsChangedEventManager
+ * @see TransportStatisticsChangedEventListener
+ *
  * @author kimchy
  */
 public class TransportStatisticsChangedEvent {
@@ -17,10 +41,16 @@ public class TransportStatisticsChangedEvent {
         this.statistics = statistics;
     }
 
+    /**
+     * Retruns the associated transport with the event.
+     */
     public Transport getTransport() {
         return transport;
     }
 
+    /**
+     * Returns the transport statistics sampled.
+     */
     public TransportStatistics getStatistics() {
         return statistics;
     }
--- src/main/src/org/openspaces/admin/transport/events/TransportsStatisticsChangedEvent.java
+++ src/main/src/org/openspaces/admin/transport/events/TransportsStatisticsChangedEvent.java
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openspaces.admin.transport.events;
 
 import org.openspaces.admin.transport.Transports;
 import org.openspaces.admin.transport.TransportsStatistics;
 
 /**
+ * An event indicating that an aggregated transports level statistics has changed.
+ *
+ * <p>Note, monitoring needs to be enabled in order to receive the events.
+ *
  * @author kimchy
  */
 public class TransportsStatisticsChangedEvent {
@@ -17,10 +37,16 @@ public class TransportsStatisticsChangedEvent {
         this.statistics = statistics;
     }
 
+    /**
+     * Returns the tranports associated with this events.
+     */
     public Transports getTransports() {
         return transports;
     }
 
+    /**
+     * Returns the aggregated transports statistics sampled.
+     */
     public TransportsStatistics getStatistics() {
         return statistics;
     }
