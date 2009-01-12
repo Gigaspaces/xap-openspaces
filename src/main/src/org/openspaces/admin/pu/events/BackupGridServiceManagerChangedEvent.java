7813701b10888ddb57656866acb2f880e3f56733
kimchy <kimchy@localhost>
2009-01-12 12:41:35 +0000

    admin javadoc
    svn path=/trunk/openspaces/; revision=48019
    
    
    Former-commit-id: deab63d54efeef08a7b36207a7ca021fbfdadff8

--- src/main/src/org/openspaces/admin/pu/events/BackupGridServiceManagerChangedEvent.java
+++ src/main/src/org/openspaces/admin/pu/events/BackupGridServiceManagerChangedEvent.java
@@ -1,15 +1,44 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openspaces.admin.pu.events;
 
 import org.openspaces.admin.gsm.GridServiceManager;
 import org.openspaces.admin.pu.ProcessingUnit;
 
 /**
+ * An event indicating that a backup GSM of a processing unit was either added or removed.
+ *
  * @author kimchy
+ * @see org.openspaces.admin.pu.ProcessingUnit#getBackupGridServiceManagerChanged()
+ * @see org.openspaces.admin.pu.ProcessingUnits#getBackupGridServiceManagerChanged()
  */
 public class BackupGridServiceManagerChangedEvent {
 
+    /**
+     * The type of the event.
+     */
     public static enum Type {
+        /**
+         * The event indicates that a GSM backup was added.
+         */
         ADDED,
+        /**
+         * The event indicates that a GSM backup was removed.
+         */
         REMOVED
     }
 
@@ -25,14 +54,23 @@ public class BackupGridServiceManagerChangedEvent {
         this.gridServiceManager = gridServiceManager;
     }
 
+    /**
+     * Returns the Processing Unit that the backup GSM event occured on.
+     */
     public ProcessingUnit getProcessingUnit() {
         return processingUnit;
     }
 
+    /**
+     * Returns the type of the event.
+     */
     public Type getType() {
         return type;
     }
 
+    /**
+     * Retruns the backup GSM that was either added or removed for the given Processing Unit.
+     */
     public GridServiceManager getGridServiceManager() {
         return gridServiceManager;
     }
--- src/main/src/org/openspaces/admin/pu/events/ManagingGridServiceManagerChangedEvent.java
+++ src/main/src/org/openspaces/admin/pu/events/ManagingGridServiceManagerChangedEvent.java
@@ -1,10 +1,30 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openspaces.admin.pu.events;
 
 import org.openspaces.admin.gsm.GridServiceManager;
 import org.openspaces.admin.pu.ProcessingUnit;
 
 /**
+ * An event indicating that a managing GSM of a processing unit was changed.
+ *
  * @author kimchy
+ * @see org.openspaces.admin.pu.ProcessingUnit#getManagingGridServiceManagerChanged()
+ * @see org.openspaces.admin.pu.ProcessingUnits#getManagingGridServiceManagerChanged()
  */
 public class ManagingGridServiceManagerChangedEvent {
 
@@ -20,14 +40,23 @@ public class ManagingGridServiceManagerChangedEvent {
         this.previousGridServiceManager = previousGridServiceManager;
     }
 
+    /**
+     * Returns the processing unit the managing GSM was changed for.
+     */
     public ProcessingUnit getProcessingUnit() {
         return processingUnit;
     }
 
+    /**
+     * Returns the new GSM that is associated with the processing unit.
+     */
     public GridServiceManager getNewGridServiceManager() {
         return newGridServiceManager;
     }
 
+    /**
+     * Returns the previous GSM that is associated with the processing unit.
+     */
     public GridServiceManager getPreviousGridServiceManager() {
         return previousGridServiceManager;
     }
