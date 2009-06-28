f48989a1a425007297733a310e16883237aa8dcd
niv <niv@localhost>
2009-06-28 12:24:54 +0000

    Removed unused import.
    svn path=/trunk/openspaces/; revision=56369
    
    
    Former-commit-id: f70cf43fe60e7433b5144aa9592e9b52fa47b413

--- src/main/src/org/openspaces/core/space/UrlSpaceFactoryBean.java
+++ src/main/src/org/openspaces/core/space/UrlSpaceFactoryBean.java
@@ -62,7 +62,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
