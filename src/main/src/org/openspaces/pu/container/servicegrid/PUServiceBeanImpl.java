6a2828bac07deaaaa39a4dee4b2b4170e58653e7
eitany <eitany@localhost>
2009-07-28 13:21:14 +0000

    Remove unused imports
    svn path=/trunk/openspaces/; revision=57709
    
    
    Former-commit-id: 21ad140fe17e08dfaef75126ec06d455f224fe34

--- src/main/src/org/openspaces/pu/container/servicegrid/PUServiceBeanImpl.java
+++ src/main/src/org/openspaces/pu/container/servicegrid/PUServiceBeanImpl.java
@@ -89,7 +89,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
 /**
