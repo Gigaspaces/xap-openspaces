3a18603c227f211da91ef85ef7ec7799f558477b
kimchy <kimchy@localhost>
2009-10-06 10:34:47 +0000

    GS-7159
    Expose and provide log retrieval API for GSC/GSM/GSA/LUS
    
    supprot GSA log extraction, including supprot for multi forward chunk
    
    svn path=/trunk/gigaspaces/; revision=60440
    
    
    Former-commit-id: 1a95d1b0a1576292ff4f7b41fffb1d1924976722

--- src/main/src/org/openspaces/admin/samples/TestDeployer.java
+++ src/main/src/org/openspaces/admin/samples/TestDeployer.java
@@ -1,10 +1,10 @@
 package org.openspaces.admin.samples;
 
+import com.gigaspaces.log.*;
 import org.openspaces.admin.Admin;
 import org.openspaces.admin.AdminFactory;
+import org.openspaces.admin.gsa.GridServiceAgent;
 import org.openspaces.admin.gsc.GridServiceContainer;
-
-import com.gigaspaces.log.*;
 
 /**
  * @author kimchy
@@ -19,6 +19,25 @@ public class TestDeployer {
 
         System.out.println("Getting logs...");
 
+        GridServiceAgent agent = admin.getGridServiceAgents().waitForAtLeastOne();
+        LogEntryMatcher matcher = new ForwardChunkLogEntryMatcher(new NLogEntryMatcher(2));
+        while (true) {
+            LogEntries[] entries = agent.log(LogProcessType.GSC, matcher);
+            boolean notAllNull = false;
+            for (LogEntries logEntries : entries) {
+                if (logEntries == null) {
+                    continue;
+                }
+                notAllNull = true;
+                for (LogEntry log : logEntries.logEntries()) {
+                    System.out.println(logEntries.getProcessType() + "/" + logEntries.getPid() + ": " + log.getText());
+                }
+            }
+            if (!notAllNull) {
+                break;
+            }
+        }
+
 //        LogEntryMatcher matcher = new ReverseLogEntryMatcher(new LastNLogEntryMatcher(2));
 //        while (true) {
 //            final LogEntries logs = container.log(matcher);
@@ -30,16 +49,16 @@ public class TestDeployer {
 //            }
 //        }
 
-        LogEntryMatcher matcher = new ForwardChunkLogEntryMatcher(new NLogEntryMatcher(2));
-        while (true) {
-            final LogEntries logs = container.log(matcher);
-            if (logs == null) {
-                break;
-            }
-            for (LogEntry log : logs.logEntries()) {
-                System.out.print(log.getTextWithLF());
-            }
-        }
+//        LogEntryMatcher matcher = new ForwardChunkLogEntryMatcher(new NLogEntryMatcher(2));
+//        while (true) {
+//            final LogEntries logs = container.log(matcher);
+//            if (logs == null) {
+//                break;
+//            }
+//            for (LogEntry log : logs.logEntries()) {
+//                System.out.print(log.getTextWithLF());
+//            }
+//        }
 
 //        new Thread(new Runnable() {
 //
