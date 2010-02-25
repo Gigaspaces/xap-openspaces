2f2a773d77614568e21296a73bd9372d78cd2b08
niv <niv@localhost>
2010-02-25 07:23:14 +0000

    Removed unused import.
    svn path=/trunk/openspaces/; revision=65497
    
    
    Former-commit-id: c7b7445c1e1f931d3ba3956bcfdf251d49326be9

--- src/main/src/org/openspaces/pu/service/PlainServiceDetails.java
+++ src/main/src/org/openspaces/pu/service/PlainServiceDetails.java
@@ -4,7 +4,6 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
