f13bd6a75ba49c18bc917f64371685adeffd68c3
anna <anna@localhost>
2010-03-03 11:06:06 +0000

    GS-7402 Added javadoc
    svn path=/trunk/gigaspaces/; revision=65734
    
    
    Former-commit-id: ca2d0300d4586b2098bef95de4b24ef6db054105

--- src/main/src/org/openspaces/core/TakeMultipleException.java
+++ src/main/src/org/openspaces/core/TakeMultipleException.java
@@ -12,10 +12,15 @@ import org.openspaces.core.exception.ExceptionTranslator;
  * <li>SQLQueries/Templates.
  * </ul>
  *
+ * At space level - the failure is fail-fast - once an operation on an object failed - exception is returned immediately and no other objects are processed. <br><br>
+ * 
+ * At cluster level - if one of operation target spaces fails, first completion of the operation on other spaces is done. 
+ * Then the aggregated exception is thrown. <br>  
+ * 
  * <p>The exception contains:
  * <ul>
  * <li>An array of exceptions that caused it. One exception per each space that failed.
- * <li>An array of entries that were successfully read or take.
+ * <li>An array of entries that were successfully taken.
  * </ul>
  *
  * <p>
