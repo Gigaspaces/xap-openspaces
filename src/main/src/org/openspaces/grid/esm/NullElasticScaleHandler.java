16a7891c0346b45ab55de0679ae1220bf55a8cc5
moran <moran@localhost>
2010-03-23 09:22:42 +0000

    Add logging
    svn path=/trunk/openspaces/; revision=66576
    
    
    Former-commit-id: 01694b815efa6afca415a6d6e5000b6c702b12f7

--- src/main/src/org/openspaces/grid/esm/NullElasticScaleHandler.java
+++ src/main/src/org/openspaces/grid/esm/NullElasticScaleHandler.java
@@ -1,9 +1,13 @@
 package org.openspaces.grid.esm;
 
+import java.util.logging.Logger;
+
 import org.openspaces.admin.machine.Machine;
 
 public class NullElasticScaleHandler implements ElasticScaleHandler {
 
+    private final static Logger logger = Logger.getLogger("org.openspaces.grid.esm");
+    
     public void init(ElasticScaleConfig config) {
     }
 
@@ -11,9 +15,11 @@ public class NullElasticScaleHandler implements ElasticScaleHandler {
         return true;
     }
     
-    public void scaleOut(ElasticScaleHandlerContext command) {
+    public void scaleOut(ElasticScaleHandlerContext context) {
+        logger.info("Request to scale out - Needs a new machine");
     }
 
     public void scaleIn(Machine machine) {
+        logger.info("Request to scale in - No need for this machine [" + ToStringHelper.machineToString(machine)+"]");
     }
 }
