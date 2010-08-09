86952f976c6eccd4d214da0c5f50fda0fc4f285b
kobi <kobi@localhost>
2010-08-09 13:10:48 +0000

    GS-7895 Add Serial version for backward.
    svn path=/trunk/openspaces/; revision=72293
    
    
    Former-commit-id: 935f9f3fa688fda5e64f0b1429c4e789988575c9

--- src/main/src/org/openspaces/remoting/EventDrivenSpaceRemotingEntry.java
+++ src/main/src/org/openspaces/remoting/EventDrivenSpaceRemotingEntry.java
@@ -35,6 +35,8 @@ import java.util.Arrays;
 public class EventDrivenSpaceRemotingEntry extends MetaDataEntry implements SpaceRemotingInvocation, SpaceRemotingResult,
         Cloneable, Externalizable {
 
+    static final long serialVersionUID = 5723421441254655906L;
+
     static int bitIndexCounter = 0;
     private static final int LOOKUP_NAME_BIT_MASK = 1 << bitIndexCounter++;
     private static final int METHOD_NAME_BIT_MASK = 1 << bitIndexCounter++;
