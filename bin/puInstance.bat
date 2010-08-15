641645925f236665bcd5d73b54e2ae0daa53ee06
guy <guy@localhost>
2010-08-15 11:17:07 +0000

    GS-8099 Remove the redefinitions of all the properties that are set at the setenv. This way a the JAVACMD will be defined in the setnev.
    svn path=/trunk/openspaces/; revision=72567
    
    
    Former-commit-id: 0e0fbecaade3412513460e1eb4ab8ea077a22a62

--- bin/puInstance.bat
+++ bin/puInstance.bat
@@ -7,15 +7,6 @@ rem set booclasspath
 set bootclasspath=-Xbootclasspath/p:%XML_JARS%
 
 @echo Starting a Processing Unit Instance
-@set JSHOMEDIR=%~dp0\..
-
-set LOOKUP_GROUPS_PROP=-Dcom.gs.jini_lus.groups=%LOOKUPGROUPS%
-
-if "%LOOKUPLOCATORS%" == ""  (
-set LOOKUPLOCATORS=
-)
-set LOOKUP_LOCATORS_PROP=-Dcom.gs.jini_lus.locators=%LOOKUPLOCATORS%
-
 set COMMAND_LINE=%JAVACMD% %JAVA_OPTIONS% %bootclasspath% %LOOKUP_LOCATORS_PROP% %LOOKUP_GROUPS_PROP% %GS_LOGGING_CONFIG_FILE_PROP% %RMI_OPTIONS% "-Dcom.gs.home=%JSHOMEDIR%" -Djava.security.policy=%POLICY% -classpath %PRE_CLASSPATH%;%GS_JARS%;%SPRING_JARS%;%EXT_JARS%;%JDBC_JARS%;%POST_CLASSPATH% org.openspaces.pu.container.standalone.StandaloneProcessingUnitContainer %*
 
 set TITLE="Processing Unit Instance ["%*"] started on [%computername%]"
--- bin/puInstance.sh
+++ bin/puInstance.sh
@@ -4,36 +4,7 @@
 . `dirname $0`/setenv.sh
 
 echo Starting a Processing Unit Instance
-if [ "${JSHOMEDIR}" = "" ] ; then 
-  JSHOMEDIR=`dirname $0`/..
-fi
-export JSHOMEDIR
-
-bootclasspath="-Xbootclasspath/p:$XML_JARS"
-
-JAVACMD="${JAVA_HOME}/bin/java"
-
-
-# Check for Cygwin
-cygwin=
-case $OS in 
-    Windows*) 
-        cygwin=1
-esac
-# For Cygwin, ensure paths are in UNIX format before anything is touched
-if [ "$cygwin" = "1" ]; then
-    CPS=";"
-else
-    CPS=":"
-fi
-export CPS
-
-LOOKUP_GROUPS_PROP=-Dcom.gs.jini_lus.groups=${LOOKUPGROUPS}; export LOOKUP_GROUPS_PROP
-
-if [ "${LOOKUPLOCATORS}" = "" ] ; then
-LOOKUPLOCATORS=; export LOOKUPLOCATORS
-fi
-LOOKUP_LOCATORS_PROP="-Dcom.gs.jini_lus.locators=${LOOKUPLOCATORS}"; export LOOKUP_LOCATORS_PROP
+bootclasspath="-Xbootclasspath/p:$(toNative $XML_JARS)"
 
 COMMAND_LINE="${JAVACMD} ${JAVA_OPTIONS} $bootclasspath ${GS_LOGGING_CONFIG_FILE_PROP} ${RMI_OPTIONS} ${LOOKUP_LOCATORS_PROP} ${LOOKUP_GROUPS_PROP} -Djava.security.policy=${POLICY} -Dcom.gs.home=${JSHOMEDIR} -classpath "${PRE_CLASSPATH}${CPS}${GS_JARS}${CPS}${SPRING_JARS}${CPS}${EXT_JARS}$CPS${JDBC_JARS}${CPS}${POST_CLASSPATH}" org.openspaces.pu.container.standalone.StandaloneProcessingUnitContainer $*"
 
