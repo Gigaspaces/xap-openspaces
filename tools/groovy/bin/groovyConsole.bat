08c1710aa6c2d44e3f6f8e2d5edd326fe031e90f
guy <guy@localhost>
2010-10-06 08:43:15 +0000

    GS-8264 Adding Groovy 1.7.5
    svn path=/trunk/openspaces/; revision=75341
    
    
    Former-commit-id: fbb74d78d4c725e90ee98a3b1439a40cad20ae5a

---
+++ tools/groovy/bin/groovyConsole.bat
@@ -0,0 +1,19 @@
+@if "%DEBUG%" == "" @echo off
+
+@rem 
+@rem $Revision: 19763 $ $Date: 2010-04-05 14:33:24 +0200 (Mo, 05. Apr 2010) $
+@rem 
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+:begin
+@rem Determine what directory it is in.
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.\
+
+"%DIRNAME%\startGroovy.bat" "%DIRNAME%" groovy.ui.Console %*
+
+@rem End local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" endlocal
+%COMSPEC% /C exit /B %ERRORLEVEL%
