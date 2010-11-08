85bd2ca2db63a849c5dddf16597da791ec9fc7d3
idan <idan@localhost>
2010-11-08 12:27:58 +0000

    GS-8334 Implemented relationships, basic operations & transactions.
    svn path=/trunk/openspaces/; revision=76954
    
    
    Former-commit-id: 841fd4496dce416bf77380c172f4b56756d592f2

---
+++ src/main/src/org/openspaces/jpa/openjpa/GSConfiguration.java
@@ -0,0 +1,56 @@
+package org.openspaces.jpa.openjpa;
+
+import net.jini.core.transaction.server.TransactionManager;
+
+import org.apache.openjpa.conf.OpenJPAConfigurationImpl;
+import org.openspaces.core.space.UrlSpaceConfigurer;
+
+import com.gigaspaces.client.transaction.ITransactionManagerProvider;
+import com.gigaspaces.client.transaction.ITransactionManagerProvider.TransactionManagerType;
+import com.gigaspaces.client.transaction.TransactionManagerConfiguration;
+import com.gigaspaces.client.transaction.TransactionManagerProviderFactory;
+import com.j_spaces.core.IJSpace;
+
+/**
+ * Holds OpenJPA's configuration properties & GigaSpaces resources.
+ * OpenJPA keeps a single instance of this class.
+ * 
+ * @author idan
+ * @since 8.0
+ * 
+ */
+public class GSConfiguration extends OpenJPAConfigurationImpl {
+
+    private IJSpace _space;
+    private ITransactionManagerProvider _transactionManagerProvider;
+    
+    public GSConfiguration() {
+        super();        
+        // Default transaction timeout
+        setLockTimeout(1000000);
+    }
+
+    public void initialize() {
+        // Set a space proxy using the provided connection url        
+        _space = new UrlSpaceConfigurer(getConnectionURL()).space();
+        
+        // Create a transaction manager
+        TransactionManagerConfiguration configuration = new TransactionManagerConfiguration(TransactionManagerType.DISTRIBUTED);
+        try {
+            _transactionManagerProvider = TransactionManagerProviderFactory.newInstance(_space, configuration);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+    public IJSpace getSpace() {
+        return _space;
+    }
+    
+    public TransactionManager getTransactionManager() {
+        return _transactionManagerProvider.getTransactionManager();
+    }
+    
+    
+    
+}
--- src/main/src/org/openspaces/jpa/openjpa/GSStoreManager.java
+++ src/main/src/org/openspaces/jpa/openjpa/GSStoreManager.java
@@ -1,179 +1,338 @@
 package org.openspaces.jpa.openjpa;
 
 import java.lang.reflect.AnnotatedElement;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-import org.apache.openjpa.jdbc.kernel.ConnectionInfo;
-import org.apache.openjpa.jdbc.kernel.JDBCFetchConfiguration;
-import org.apache.openjpa.jdbc.kernel.JDBCStoreManager;
-import org.apache.openjpa.jdbc.meta.ClassMapping;
-import org.apache.openjpa.jdbc.meta.FieldMapping;
+import net.jini.core.transaction.Transaction;
+import net.jini.core.transaction.TransactionFactory;
+
+import org.apache.openjpa.abstractstore.AbstractStoreManager;
+import org.apache.openjpa.conf.OpenJPAConfiguration;
 import org.apache.openjpa.kernel.FetchConfiguration;
 import org.apache.openjpa.kernel.OpenJPAStateManager;
 import org.apache.openjpa.kernel.PCState;
+import org.apache.openjpa.kernel.StoreQuery;
+import org.apache.openjpa.lib.rop.ResultObjectProvider;
+import org.apache.openjpa.meta.ClassMetaData;
 import org.apache.openjpa.meta.FieldMetaData;
 import org.apache.openjpa.util.ApplicationIds;
-import org.openspaces.core.space.UrlSpaceConfigurer;
 
 import com.j_spaces.core.IJSpace;
 import com.j_spaces.core.client.ExternalEntry;
+import com.j_spaces.core.client.UpdateModifiers;
 
 /**
- * GigaSpaces OpenJPA StoreManager implementation.
- * 
- * The following implementation is the layer resposible for loading/saving objects from GigaSpaces.
- * Currently there's a mix between JDBC & SpaceAPI but eventually only SpaceAPI will be used.
- * 
- * Notes:
- *  Only flush (persist) & initialize (find) methods have been overriden.
- *  JPQL is functional for non relational queries & objects.
+ * A GigaSpaces back-end implementation for OpenJPA.
+ * Responsible for storing and fetching data from GigaSpaces using space API.
  * 
  * @author idan
  * @since 8.0
- * 
+ *
  */
-public class GSStoreManager extends JDBCStoreManager {
-    
-    private static final String GS_JDBC_URL_PREFIX = "jdbc:gigaspaces:url:";
-    private IJSpace _space;
+public class GSStoreManager extends AbstractStoreManager {
+
+    private Transaction _transaction = null;
     
     @Override
-    public Connection getConnection() {
-        // Set a space proxy according to the provided JDBC url
-        String connectionUrl = getConfiguration().getConnectionURL();
-        String spaceUrl = null;
+    protected Collection getUnsupportedOptions() {
+        Collection<String> unsupportedOptions = (Collection<String>)super.getUnsupportedOptions();
+        unsupportedOptions.remove(OpenJPAConfiguration.OPTION_ID_DATASTORE);
+        return unsupportedOptions;
+    }
 
-        if (connectionUrl.startsWith(GS_JDBC_URL_PREFIX))
-            spaceUrl = connectionUrl.substring(GS_JDBC_URL_PREFIX.length());
-        else
-            throw new IllegalArgumentException("Invalid connection url in JDBC configuration.");
-        
-        _space = new UrlSpaceConfigurer(spaceUrl).space();
-        
-        return super.getConnection();
+    @Override
+    public boolean syncVersion(OpenJPAStateManager sm, Object edata) {
+        return true;
+    }
+
+    
+    @Override
+    public void begin() {
+        try {
+            _transaction = (TransactionFactory.create(getConfiguration().getTransactionManager(), getConfiguration().getLockTimeout())).transaction;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }            
+    }
+
+    @Override
+    public void commit() {
+        try {
+            _transaction.commit();
+        } catch (Exception e) {
+           throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void rollback() {
+        try {
+            _transaction.abort();
+        } catch (Exception e) {
+           throw new RuntimeException(e.getMessage(), e);
+        }        
+    }    
+    
+    @Override
+    protected void open() {
+        // Specific gigaspaces initialization (space proxy)
+        getConfiguration().initialize();
+    }
+
+    
+    @Override
+    public StoreQuery newQuery(String language) {        
+    	// Not implemented..
+        //ExpressionParser ep = QueryLanguages.parserForLanguage(language);
+        //return new GSStoreQuery(ep, this);
+    	return null;
+    }
+
+    @Override
+    protected OpenJPAConfiguration newConfiguration() {
+        return new GSConfiguration();
+    }
+
+    public GSConfiguration getConfiguration() {
+        return (GSConfiguration) getContext().getConfiguration();
     }
     
-    
-    @Override
-    protected boolean initializeState(OpenJPAStateManager sm, PCState state,
-            JDBCFetchConfiguration fetch, ConnectionInfo info)
-            throws ClassNotFoundException, SQLException {
+    public boolean exists(OpenJPAStateManager sm, Object edata) {
+        return true;
+    }
 
-        // When using JPQL info.result != null which means that a JDBC query has already been
-        // executed. This is not enough for owned relationships and therefore currently we call
-        // space.read for getting the desired object with its nested objects.
+    public boolean isCached(List<Object> oids, BitSet edata) {
+        return false;
+    }
+
+    @Override
+    public Collection loadAll(Collection sms, PCState state, int load,
+            FetchConfiguration fetch, Object edata) {
+            return null;
+        }
+
+    @Override
+    public boolean initialize(OpenJPAStateManager sm, PCState state,
+            FetchConfiguration fetchConfiguration, Object edata) {
+
+        // TODO: Optimization when edata != null
         
-        if (_space == null)
-            getConnection();
-        
-        ClassMapping cm = (ClassMapping)sm.getMetaData();
-        String className = cm.getDescribedType().getName();
-        
-        Object[] ids = ApplicationIds.toPKValues(sm.getObjectId(), cm);
-        
-        ExternalEntry ee = new ExternalEntry(className, new Object[cm.getFields().length]);
+        final IJSpace space = getConfiguration().getSpace();        
+        final ClassMetaData cm = sm.getMetaData();        
+        final Object[] ids = ApplicationIds.toPKValues(sm.getObjectId(), cm);        
+        final ExternalEntry template = new ExternalEntry(cm.getDescribedType().getName(), new Object[cm.getFields().length]);
 
         for (int i = 0; i < ids.length; i++) {
-            ee.setFieldValue(cm.getPrimaryKeyFields()[i].getDeclaredIndex(), ids[i]);
+            template.setFieldValue(cm.getPrimaryKeyFields()[i].getDeclaredIndex(), ids[i]);
         }
         
         try {                        
-            ExternalEntry res = (ExternalEntry)_space.read(ee, null, 0);            
+            ExternalEntry res = (ExternalEntry)space.read(template, _transaction, 0);            
             if (res == null)
                 return false;            
-            
+
             // TODO: Handle sub-classes etc...
-            sm.initialize(cm.getDescribedType(), state);
+            sm.initialize(cm.getDescribedType(), state);                        
             
             FieldMetaData[] fms = cm.getFields();
             for (int i = 0; i < fms.length; i++) {
                 // Skip primary keys and non-persistent keys
                 if (fms[i].isPrimaryKey() || sm.getLoaded().get(fms[i].getIndex()))
-                    continue;
-                
-                sm.store(i, res.getFieldValue(i));                
-            }
+                    continue;                
+                sm.store(i, res.getFieldValue(i));
+            }            
         } catch (Exception e) {
+            return false;
         }
         
         return true;        
     }
-    
+
+    /**
+     * This method loads specific fields from the data store for updating them.
+     */
     @Override
-    public boolean exists(OpenJPAStateManager sm, Object context) {
-        return super.exists(sm, context);
+    public boolean load(OpenJPAStateManager sm, BitSet fields, FetchConfiguration fetch, int lockLevel, Object context) {
+        // Prepare the external entry template using the objects id
+        ClassMetaData cm = (ClassMetaData)sm.getMetaData();
+        Object[] ids = ApplicationIds.toPKValues(sm.getObjectId(), cm);        
+        ExternalEntry template = new ExternalEntry(cm.getDescribedType().getName(), new Object[cm.getFields().length]);
+        for (int i = 0; i < ids.length; i++) {
+            template.setFieldValue(cm.getPrimaryKeyFields()[i].getDeclaredIndex(), ids[i]);
+        }
+        // Read object from space
+        IJSpace space = getConfiguration().getSpace();        
+        try {        
+            ExternalEntry result = (ExternalEntry)space.read(template, _transaction, 0);
+            if (result == null)
+                return false;
+            // Process result - store only the relevant fields in the state manager
+            for (int i = 0; i < cm.getDeclaredFields().length; i++) {
+                if (fields.get(i))                
+                    sm.store(i, result.getFieldValue(i));                
+            }                                    
+            return true;            
+        } catch (Exception e) {
+            System.out.println();
+        }                    
+        return false;
     }
 
     @Override
-    public boolean load(OpenJPAStateManager sm, BitSet fields,
-            FetchConfiguration fetch, int lockLevel, Object context) {
-
-        return super.load(sm, fields, fetch, lockLevel, context);
+    public ResultObjectProvider executeExtent(ClassMetaData classmetadata, boolean flag,
+            FetchConfiguration fetchconfiguration) {
+        // TODO Auto-generated method stub
+        return null;
     }
-    
+
+    /**
+     * Flushes changes to GigaSpaces.
+     * Returns a list of exceptions that occurred.
+     */
     @Override
-    public Collection flush(Collection sms) {
-        if (_space == null)
-            getConnection();
+    protected Collection flush(Collection pNew, Collection pNewUpdated, Collection pNewFlushedDeleted,
+            Collection pDirty, Collection pDeleted) {
         
-        List<OpenJPAStateManager> unhandledStates = new ArrayList<OpenJPAStateManager>();                
+        IJSpace space = getConfiguration().getSpace();
+                
+        ArrayList<Exception> exceptions = new ArrayList<Exception>();
+        
+        handleNewObjects(pNew, exceptions, space);
+        handleUpdatedObjects(pDirty, exceptions, space);
+        handleDeletedObjects(pDeleted, exceptions, space);                     
+        
+        return exceptions;                   
+    }
+
+    /**
+     * Clears the removed objects from the space.
+     */
+    private void handleDeletedObjects(Collection<OpenJPAStateManager> sms, ArrayList<Exception> exceptions, IJSpace space) {
+        for (OpenJPAStateManager sm : sms) {
+            ClassMetaData cm = sm.getMetaData();
+            try {
+                // Remove object from space
+                Object[] ids = ApplicationIds.toPKValues(sm.getObjectId(), cm);        
+                ExternalEntry template = new ExternalEntry(cm.getDescribedType().getName(), new Object[cm.getFields().length]);
+                for (int i = 0; i < ids.length; i++) {
+                    template.setFieldValue(cm.getPrimaryKeyFields()[i].getDeclaredIndex(), ids[i]);
+                }                
+                int removed = space.clear(template, _transaction, 0); 
+                if (removed != 1)
+                    throw new Exception("Removed object not found in space.");                
+            } catch (Exception e) {
+                exceptions.add(e);
+            }
+        }
+    }
+
+    /**
+     * Partially updates dirty fields to the space.
+     */    
+    private void handleUpdatedObjects(Collection<OpenJPAStateManager> sms, ArrayList<Exception> exceptions, IJSpace space) {
+        // Generate a template for each state manager and use partial update for updating..         
+        for (OpenJPAStateManager sm : sms) {
+            ClassMetaData cm = sm.getMetaData();
+            try {
+                // Read object from space
+                Object[] ids = ApplicationIds.toPKValues(sm.getObjectId(), cm);        
+                ExternalEntry template = new ExternalEntry(cm.getDescribedType().getName(), new Object[cm.getFields().length]);
+                for (int i = 0; i < ids.length; i++) {
+                    template.setFieldValue(cm.getPrimaryKeyFields()[i].getDeclaredIndex(), ids[i]);
+                }
+                ExternalEntry result = (ExternalEntry)space.read(template, _transaction, 0);
+                if (result == null)
+                    throw new Exception("Updated object not found in space.");
+                // Calculate dirty fields count
+                int numberOfDirtyFields = 0;
+                for (int i = 0; i < cm.getDeclaredFields().length; i++) {
+                    if (sm.getDirty().get(i))
+                        numberOfDirtyFields++;
+                }
+                // Generate an external entry template using the dirty fields
+                String[] fieldNames = new String[numberOfDirtyFields];
+                Object[] fieldValues = new String[numberOfDirtyFields];
+                int dirtyIndex = 0;
+                for (int i = 0; i < cm.getDeclaredFields().length; i++) {
+                    if (sm.getDirty().get(i)) {
+                        fieldNames[dirtyIndex] = cm.getDeclaredFields()[i].getName();
+                        fieldValues[dirtyIndex] = sm.fetch(i);
+                        dirtyIndex++;
+                    }
+                }
+                template = new ExternalEntry(cm.getDescribedType().getName(), fieldValues, fieldNames);
+                template.setUID(result.getUID());
+                // Write changes to the space
+                space.write(template, _transaction, 0, 0, UpdateModifiers.PARTIAL_UPDATE);                                
+            } catch (Exception e) { 
+                exceptions.add(e);
+            }                       
+        }
+    }
+
+    /**
+     * Writes new persistent objects to the space.
+     */
+    private void handleNewObjects(Collection<OpenJPAStateManager> sms, ArrayList<Exception> exceptions, IJSpace space) {
+
         HashSet<Object> instancesInRelation = new HashSet<Object>();
-        
-        for (Iterator itr = sms.iterator(); itr.hasNext();) {
-            OpenJPAStateManager sm = (OpenJPAStateManager)itr.next();
-            if (sm.getPCState() == PCState.PNEW) {                
-                Object o = sm.getManagedInstance();
-                try {
-                    // Check if one of the fields is a relation instance and if so save the instance
-                    for (FieldMetaData fieldMetaData : sm.getMetaData().getFields()) {
-                        FieldMapping fm = (FieldMapping)fieldMetaData;
-                        if (fieldMetaData.getBackingMember() instanceof AnnotatedElement) {                            
-                            AnnotatedElement ae = (AnnotatedElement)fieldMetaData.getBackingMember();                            
-                            // Handle @OneToOne & @OneToMany annotations (its not necessary to take care
-                            // of @Embedded since these instances don't have their own state manager)
-                            if (ae.isAnnotationPresent(OneToOne.class)) {                                
-                                Object instanceInRelation = sm.fetch(fm.getIndex());
-                                if (instanceInRelation != null) {
-                                    instancesInRelation.add(instanceInRelation);
-                                }                                
-                            } else if (ae.isAnnotationPresent(OneToMany.class)) {
-                                Collection<?> oneToManyInstances = (Collection<?>)sm.fetch(fm.getIndex());
-                                if (oneToManyInstances != null) {
-                                    for (Object obj : oneToManyInstances) {
-                                        instancesInRelation.add(obj);
-                                    }
+
+        for (OpenJPAStateManager sm : sms) {
+
+            try {
+                // Check if one of the fields is a relation instance and if so save the instance
+                for (FieldMetaData fieldMetaData : sm.getMetaData().getFields()) {
+                    if (fieldMetaData.getBackingMember() instanceof AnnotatedElement) {                            
+                        AnnotatedElement ae = (AnnotatedElement)fieldMetaData.getBackingMember();                            
+                        // Handle @OneToOne & @OneToMany annotations (its not necessary to take care
+                        // of @Embedded since these instances don't have their own state manager)
+                        if (ae.isAnnotationPresent(OneToOne.class)) {                                
+                            Object instanceInRelation = sm.fetch(fieldMetaData.getIndex());
+                            if (instanceInRelation != null) {
+                                instancesInRelation.add(instanceInRelation);
+                            }                                
+                        } else if (ae.isAnnotationPresent(OneToMany.class)) {
+                            Collection<?> oneToManyInstances = (Collection<?>)sm.fetch(fieldMetaData.getIndex());
+                            if (oneToManyInstances != null) {
+                                for (Object obj : oneToManyInstances) {
+                                    instancesInRelation.add(obj);
                                 }
                             }
                         }
                     }
-                    
-                    // If the current instance is not in a relation write it to the space
-                    if (!instancesInRelation.contains(sm.getManagedInstance())) {
-                        _space.write(sm.getManagedInstance(), null, 0);
-                    }
-                                        
-                } catch (Exception e) {
                 }
-                
-                
-            } else {
-                unhandledStates.add(sm);
+
+                // If the current instance is not in a relation write it to the space
+                if (!instancesInRelation.contains(sm.getManagedInstance())) {
+                    space.write(sm.getManagedInstance(), _transaction, 0);
+                }
+
+            } catch (Exception e) {
+                exceptions.add(e);
             }
         }
-                               
-        return super.flush(unhandledStates);
     }
 
-    
-    
+    /**
+     * Initializes an ExternalEntry result as a state managed Pojo.
+     * (used by JPQL's query executor)
+     */
+    public Object loadObject(ClassMetaData classMetaData, ExternalEntry entry) {
+        // Get object id
+        Object[] primaryKeys = new Object[classMetaData.getPrimaryKeyFields().length];
+        for (int i = 0; i < primaryKeys.length; i++) {
+            primaryKeys[i] = entry.getFieldValue(classMetaData.getPrimaryKeyFields()[i].getIndex());
+        }
+        Object objectId = ApplicationIds.fromPKValues(primaryKeys, classMetaData);
+        return getContext().find(objectId, null, null, entry, 0);
+    }
+
+
 }
