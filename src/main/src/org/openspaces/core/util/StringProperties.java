d5d4c016bd64e39b9793107a3e11965869e23f97
itaif <itaif@localhost>
2010-12-02 09:38:14 +0000

    Added more utility functions to manipulate Map<String,String>
    svn path=/trunk/openspaces/; revision=78419
    
    
    Former-commit-id: 853cca48fbf20d2acd596e106f96651c43e2a269

--- src/main/src/org/openspaces/core/util/StringProperties.java
+++ src/main/src/org/openspaces/core/util/StringProperties.java
@@ -22,8 +22,18 @@ public class StringProperties {
         return this;
     }
     
-    public String get(String key) {
-        return properties.get(key);
+    
+    public String get(String key, String defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            value = defaultValue;
+        }
+        return value;
+    }
+    
+    public StringProperties putInteger(String key, int value) {
+        StringPropertiesUtils.putInteger(properties,key,value);
+        return this;
     }
     
     public int getInteger(String key, int defaultValue) throws NumberFormatException{
@@ -34,10 +44,48 @@ public class StringProperties {
         return StringPropertiesUtils.getIntegerIgnoreExceptions(properties, key, defaultValue);
     }
     
+    public StringProperties putLong(String key, long value) {
+        StringPropertiesUtils.putLong(properties,key,value);
+        return this;
+    }
+    
+    public long getLong(String key, long defaultValue) throws NumberFormatException{
+        return StringPropertiesUtils.getLong(properties, key, defaultValue);
+    }
+    
+    public long getLongIgnoreExceptions(String key, long defaultValue) {
+        return StringPropertiesUtils.getLongIgnoreExceptions(properties, key, defaultValue);
+    }
+
+    public String[] getArray(String key, String separator, String[] defaultValue) {
+        return StringPropertiesUtils.getArray(properties, key, separator, defaultValue);
+    }
+    
+    public StringProperties putArray(String key, String[] value, String separator) {
+        StringPropertiesUtils.putArray(properties, key, value, separator);
+        return this;
+    }
     public void store(OutputStream out, String comments) throws IOException {
         StringPropertiesUtils.store(properties, out, comments);
     }
 
+   public Map<String,String> getProperties() {
+       return this.properties;
+   }
+
+   public void putBoolean(String key, boolean value) {
+       StringPropertiesUtils.putBoolean(properties , key , value);
+   }
    
-    
+   public boolean getBoolean(String key, boolean defaultValue) {
+       return StringPropertiesUtils.getBoolean(properties , key , defaultValue);
+   }
+
+   public Map<String,String> getMap(String keyPrefix, Map<String,String> defaultValue) {
+       return StringPropertiesUtils.getMap(properties,keyPrefix,defaultValue);
+   }
+
+   public void putMap(String keyPrefix, Map<String,String> value) {
+       StringPropertiesUtils.putMap(properties,keyPrefix,value);
+   }
 }
--- src/main/src/org/openspaces/core/util/StringPropertiesUtils.java
+++ src/main/src/org/openspaces/core/util/StringPropertiesUtils.java
@@ -1,11 +1,15 @@
 package org.openspaces.core.util;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 public class StringPropertiesUtils {
     
@@ -25,13 +29,23 @@ public class StringPropertiesUtils {
         return properties;
     }
     
+    public static Map<String,String> load(String filename) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(new File(filename));
+        try {
+            return load(fileInputStream);
+        }
+        finally {
+            fileInputStream.close();
+        }
+    }
+    
     public static int getInteger(Map<String,String> properties, String key, int defaultValue) throws NumberFormatException {
         
         int intValue = defaultValue;
         
-        String value = properties.get(key);
+        Object value = properties.get(key);
         if (value != null ) {
-            intValue = Integer.valueOf(value);
+            intValue = Integer.valueOf(value.toString());
         }
     
         return intValue;
@@ -48,7 +62,7 @@ public class StringPropertiesUtils {
         return intValue;
     }
 
-    public static void setLong(Map<String,String> properties, String key, long value) throws NumberFormatException {
+    public static void putLong(Map<String,String> properties, String key, long value) throws NumberFormatException {
         properties.put(key, String.valueOf(value));
     }
     
@@ -56,9 +70,9 @@ public class StringPropertiesUtils {
         
         long longValue = defaultValue;
         
-        String value = properties.get(key);
+        Object value = properties.get(key);
         if (value != null ) {
-            longValue = Long.valueOf(value);
+            longValue = Long.valueOf(value.toString());
         }
     
         return longValue;
@@ -84,7 +98,7 @@ public class StringPropertiesUtils {
      * @param array
      * @param seperator
      */
-    public static void setArray(Map<String,String> properties, String key, String[] array, String separator) {
+    public static void putArray(Map<String,String> properties, String key, String[] array, String separator) {
         StringBuilder concat = new StringBuilder();
         for (int i = 0 ; i < array.length ; i++) {
             String value = array[i];
@@ -101,13 +115,78 @@ public class StringPropertiesUtils {
         properties.put(key, concat.toString());
     }
     
-    public static String[] getArray(Map<String,String> properties, String key, String sep, String[] defaultArray) {
-        String[] array = defaultArray;
+    public static String[] getArray(Map<String,String> properties, String key, String separator, String[] defaultValue) {
+        String[] array = defaultValue;
         String value = properties.get(key);
         if (value != null) {
-            array = value.split(java.util.regex.Pattern.quote(sep));
+            array = value.split(java.util.regex.Pattern.quote(separator));
         }
         return array;
     }
+
+    public static void putInteger(Map<String, String> properties, String key, int value) {
+        properties.put(key,String.valueOf(value));
+    }
+
+    public static void putBoolean(Map<String, String> properties, String key, boolean value) {
+        properties.put(key,String.valueOf(value));
+    }
     
+    public static boolean getBoolean(Map<String, String> properties, String key, boolean defaultValue) {
+        boolean booleanValue = defaultValue;
+        String value = properties.get(key);
+        
+        //don't use Boolean.valueOf() since it always defaults to false 
+        if (value != null) {
+            if (value.equalsIgnoreCase("true")) {
+                booleanValue = true;
+            }
+            else if (value.equalsIgnoreCase("false")) {
+                booleanValue = false;
+            }
+            else {
+                throw new IllegalArgumentException(key + " must be either true or false. The value " + key + " is illegal.");
+            }
+        }
+        return booleanValue;
+    }
+
+    public static Map<String, String> getMap(Map<String, String> properties, String keyPrefix, Map<String, String> defaultValue) {
+        Map<String,String> value = new HashMap<String,String>();
+        for (String key : properties.keySet()) {
+            if (key.startsWith(keyPrefix)) {
+                String newKey = key.substring(keyPrefix.length());
+                value.put(newKey, properties.get(key));
+            }
+        }
+        if (value.size() == 0) {
+            value = defaultValue;
+        }
+        return value;
+
+    }
+    
+    public static void putMap(Map<String, String> properties, String keyPrefix, Map<String, String> value) {
+        if (properties == value) {
+            throw new IllegalArgumentException("properties and value must be different objects");
+          }
+          
+          // delete old properties starting with the key prefix
+          Set<String> keysToDelete = new HashSet();
+          for (String key : properties.keySet()) {
+              if (key.toString().startsWith(keyPrefix)) {
+                  keysToDelete.add(key);
+              }
+          }
+          
+          for (String key : keysToDelete) {
+              properties.remove(key);
+          }
+      
+          // add new properties with the new key prefix
+          for (String key : value.keySet()) {
+              properties.put(keyPrefix+key, value.get(key));
+          }
+    }
 }
+
