439c83780ffca2cc0ccb397f15aa4c2d4def0f3e
barakme <barakme@localhost>
2010-12-09 12:07:14 +0000

    GS-8420
    Removed unused import
    
    svn path=/trunk/openspaces/; revision=78817
    
    
    Former-commit-id: 96ed47c97f9a59f772c533a2a9dfb8fd7e45031f

--- src/main/src/org/openspaces/wan/mirror/LogCleanupRunnable.java
+++ src/main/src/org/openspaces/wan/mirror/LogCleanupRunnable.java
@@ -8,7 +8,6 @@ import java.util.logging.Logger;
 import net.jini.core.lease.Lease;
 
 import org.openspaces.core.GigaSpace;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
