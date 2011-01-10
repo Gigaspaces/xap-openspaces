b344c528214c9be266d329a8a2dd3a6888a1c5a4
itaif <itaif@localhost>
2011-01-10 20:04:33 +0000

    ooops forgot one file
    svn path=/trunk/openspaces/; revision=80842
    
    
    Former-commit-id: 9846319de44f2544378f2cb9b8d90648af10156b

---
+++ src/main/src/org/openspaces/grid/gsm/AdvancedElasticPropertiesConfigAware.java
@@ -0,0 +1,9 @@
+package org.openspaces.grid.gsm;
+
+import org.openspaces.admin.internal.pu.elastic.AdvancedElasticPropertiesConfig;
+
+public interface AdvancedElasticPropertiesConfigAware {
+
+    void setAdvancedElasticPropertiesConfig(AdvancedElasticPropertiesConfig config);
+
+}
