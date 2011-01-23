37633cb38a29f088620b6a2e66d41b475bb64a07
moran <moran@localhost>
2011-01-23 09:16:10 +0000

    add serial version UID
    svn path=/trunk/openspaces/; revision=81895
    
    
    Former-commit-id: 26d1f0e1119de9bc6e9cddcc9f47de0b87c97e9a

--- src/main/src/org/openspaces/admin/bean/BeanConfigurationClassCastException.java
+++ src/main/src/org/openspaces/admin/bean/BeanConfigurationClassCastException.java
@@ -3,9 +3,10 @@ package org.openspaces.admin.bean;
 /**
  * Indicates the bean does not implement the required interfaces.
  * @author itaif
- *
  */
 public class BeanConfigurationClassCastException extends BeanConfigurationException {
+
+    private static final long serialVersionUID = 1L;
 
     public BeanConfigurationClassCastException(String message) {
         super(message);
