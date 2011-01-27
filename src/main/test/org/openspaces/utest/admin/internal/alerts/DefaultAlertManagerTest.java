57601976b1d2561d249758383ad9a14ac1825dde
moran <moran@localhost>
2011-01-27 08:47:07 +0000

    GS-8403: More unit tests to increase coverage of AlertManager spec
    svn path=/trunk/openspaces/; revision=82282
    
    
    Former-commit-id: c253e3705fe2d631ec940daf706fbe3a83f8c295

--- src/main/test/org/openspaces/utest/admin/internal/alerts/DefaultAlertManagerTest.java
+++ src/main/test/org/openspaces/utest/admin/internal/alerts/DefaultAlertManagerTest.java
@@ -1,7 +1,10 @@
 package org.openspaces.utest.admin.internal.alerts;
 
 
+import org.openspaces.admin.alert.AlertConfigurationException;
 import org.openspaces.admin.alert.AlertManager;
+import org.openspaces.admin.bean.BeanConfigNotFoundException;
+import org.openspaces.admin.bean.EnabledBeanConfigCannotBeChangedException;
 import org.openspaces.admin.internal.alert.DefaultAlertManager;
 import org.openspaces.utest.admin.internal.admin.NullMockAdmin;
 
@@ -113,4 +116,89 @@ public class DefaultAlertManagerTest extends TestCase {
         assertEquals(90, test.getHighThreshold().intValue());
         assertEquals(80, test.getLowThreshold().intValue());
     }
+    
+    //setConfig - An exception is raised if the alert is already enabled.
+    //removeConfig - An exception is raised if the alert is already enabled.
+    public void test_exception_is_raised_if_alert_already_enabled() {
+        AlertManager alertManager = new DefaultAlertManager(new NullMockAdmin());
+        
+        //setup
+        MockAlertConfiguration mock = new MockAlertConfiguration();
+        mock.setHighThreshold(90);
+        mock.setLowThreshold(80);
+        alertManager.setConfig(mock);
+        alertManager.enableAlert(MockAlertConfiguration.class);
+        
+        //test
+        MockAlertConfiguration test = new MockAlertConfiguration();
+        test.setHighThreshold(91);
+        test.setLowThreshold(81);
+        try {
+            alertManager.setConfig(test);
+            fail();
+        } catch(AlertConfigurationException e) {
+            assertTrue( (e.getCause() instanceof EnabledBeanConfigCannotBeChangedException));
+        }
+        
+        // test
+        try {
+            alertManager.removeConfig(MockAlertConfiguration.class);
+            fail();
+        } catch (AlertConfigurationException e) {
+            assertTrue((e.getCause() instanceof EnabledBeanConfigCannotBeChangedException));
+        }
+    }
+    
+    //enableAlert - If the alert is already enabled, the request is silently ignored
+    public void test_if_alert_is_already_enabled_request_ignored() {
+        
+        AlertManager alertManager = new DefaultAlertManager(new NullMockAdmin());
+        
+        //setup
+        MockAlertConfiguration mock = new MockAlertConfiguration();
+        mock.setHighThreshold(90);
+        mock.setLowThreshold(80);
+        alertManager.setConfig(mock);
+        alertManager.enableAlert(MockAlertConfiguration.class);
+        
+        //test
+        alertManager.enableAlert(MockAlertConfiguration.class);
+        MockAlertConfiguration test = alertManager.getConfig(MockAlertConfiguration.class);
+        assertTrue(test.isEnabled());
+        assertEquals(90, test.getHighThreshold().intValue());
+        assertEquals(80, test.getLowThreshold().intValue());
+    }
+    
+    //enableAlert - if the configuration was not previously set.
+    //disableAlert - if the configuration was not previously set.
+    //getConfig - if the configuration was not previously set.
+    public void test_if_configuration_was_not_previously_set() {
+        
+        AlertManager alertManager = new DefaultAlertManager(new NullMockAdmin());
+        
+        //test
+        try {
+            alertManager.enableAlert(MockAlertConfiguration.class);
+            fail();
+        } catch (AlertConfigurationException e) {
+            assertTrue(e.getCause() instanceof BeanConfigNotFoundException);
+        }
+        
+        //test
+        try {
+            alertManager.disableAlert(MockAlertConfiguration.class);
+            fail();
+        } catch (AlertConfigurationException e) {
+            assertTrue(e.getCause() instanceof BeanConfigNotFoundException);
+        }
+        
+        //test
+        try {
+            alertManager.getConfig(MockAlertConfiguration.class);
+            fail();
+        } catch (AlertConfigurationException e) {
+            assertTrue(e.getCause() instanceof BeanConfigNotFoundException);
+        }
+    }
+
 }
