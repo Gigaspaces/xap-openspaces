5f58b8b214348893981f716b55a6c54cf20a908e
evgeny <evgeny@localhost>
2011-02-21 11:46:56 +0000

    GS-8707 added two new classes for customers extensions: CustomServiceDetails and CustomerServiceMonitors
    svn path=/trunk/openspaces/; revision=83583
    
    
    Former-commit-id: 69b36fcdf1626cd9f961db403750a8179e562575

--- src/main/src/org/openspaces/pu/service/CustomServiceDetails.java
+++ src/main/src/org/openspaces/pu/service/CustomServiceDetails.java
@@ -2,10 +2,27 @@ package org.openspaces.pu.service;
 
 /**
  * This class should be used by customers in order to implement their ServiceDetails
- * Id that will be passed to constructor should identify that service 
  *
  * @since 8.0.1
  */
 public class CustomServiceDetails extends PlainServiceDetails {
     public static final String SERVICE_TYPE = "custom-details";
+    
+    // Just for externalizable
+    public CustomServiceDetails() {
+    }
+
+    /**
+     * 
+     * @param id should identify that service
+     * @param serviceType
+     * @param serviceSubType
+     * @param description
+     * @param longDescription
+     */
+    public CustomServiceDetails(String id, String serviceType, String serviceSubType,
+                               String description, String longDescription) {
+        
+        super(id, serviceType, serviceSubType, description, longDescription);
+    }    
 }
--- src/main/src/org/openspaces/pu/service/CustomServiceMonitors.java
+++ src/main/src/org/openspaces/pu/service/CustomServiceMonitors.java
@@ -1,10 +1,22 @@
 package org.openspaces.pu.service;
 
+
 /**
  * This class should be used by customers in order to implement their ServiceMonitors
- * Id that will be passed to constructor should identify that service 
  *
  * @since 8.0.1
  */
 public class CustomServiceMonitors extends PlainServiceMonitors {
+
+    public CustomServiceMonitors() {
+    }
+
+    /**
+     * 
+     * @param id should identify that service
+     */
+    public CustomServiceMonitors(String id) {
+        super(id);
+    }
+    
 }
