24f33e6bae57a258d1544916e142dfe7e3914eac
evgeny <evgeny@localhost>
2011-02-21 11:31:17 +0000

    GS-8707 added two new classes for customers extensions: CustomServiceDetails and CustomerServiceMonitors Reviewer: Moran
    svn path=/trunk/openspaces/; revision=83577
    
    
    Former-commit-id: 4c166e178a5d50ae57645597c451084ef5b009ed

---
+++ src/main/src/org/openspaces/pu/service/CustomServiceDetails.java
@@ -0,0 +1,11 @@
+package org.openspaces.pu.service;
+
+/**
+ * This class should be used by customers in order to implement their ServiceDetails
+ * Id that will be passed to constructor should identify that service 
+ *
+ * @since 8.0.1
+ */
+public class CustomServiceDetails extends PlainServiceDetails {
+    public static final String SERVICE_TYPE = "custom-details";
+}
---
+++ src/main/src/org/openspaces/pu/service/CustomServiceMonitors.java
@@ -0,0 +1,10 @@
+package org.openspaces.pu.service;
+
+/**
+ * This class should be used by customers in order to implement their ServiceMonitors
+ * Id that will be passed to constructor should identify that service 
+ *
+ * @since 8.0.1
+ */
+public class CustomServiceMonitors extends PlainServiceMonitors {
+}
