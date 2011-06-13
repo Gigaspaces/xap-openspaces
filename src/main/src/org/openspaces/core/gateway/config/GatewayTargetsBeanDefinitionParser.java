1bc6b3abea581587e8963c51b72d78672ff6bd36
idan <idan@localhost>
2011-06-13 07:03:30 +0000

    GS-8831 Added 'relocate-if-wrong-ports' to delegator and sink xsd schema. Set gateway lookup group as optional & assign a generated value if not set.
    svn path=/trunk/openspaces/; revision=90883
    
    
    Former-commit-id: c2dfa34026b11ab0ccfef7ec5f32eb587f910e22

--- src/main/src/org/openspaces/core/gateway/config/GatewayTargetsBeanDefinitionParser.java
+++ src/main/src/org/openspaces/core/gateway/config/GatewayTargetsBeanDefinitionParser.java
@@ -20,8 +20,6 @@ public class GatewayTargetsBeanDefinitionParser extends AbstractSimpleBeanDefini
     public static final String LOCAL_GATEWAY_NAME = "local-gateway-name";
     public static final String TARGET_NAME = "name";
     
-    //TODO WAN: add gateway async replication related params (batch size, interval...)
-    
     @Override
     protected Class<GatewayTargetsFactoryBean> getBeanClass(Element element) {
         return GatewayTargetsFactoryBean.class;
