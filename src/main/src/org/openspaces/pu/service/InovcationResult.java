1eb2f69f8adc937852d0239932ca95420a6e6216
rafi <rafi@localhost>
2011-06-21 12:51:35 +0000

    added invocation result object
    svn path=/trunk/openspaces/; revision=91492
    
    
    Former-commit-id: 22fc40abd6f7d618a18236290cc546d41766391a

---
+++ src/main/src/org/openspaces/pu/service/InovcationResult.java
@@ -0,0 +1,42 @@
+package org.openspaces.pu.service;
+
+public class InovcationResult {
+
+    private Integer instanceId;
+    private boolean executeOnce;
+    private Object executeOnceResult;
+    private Object executeOnAllResult;
+   
+    public Integer getInstanceId() {
+        return instanceId;
+    }
+    
+    public void setInstanceId(Integer instanceId) {
+        this.instanceId = instanceId;
+    }
+   
+    public boolean isExecuteOnce() {
+        return executeOnce;
+    }
+    
+    public void setExecuteOnce(boolean executeOnce) {
+        this.executeOnce = executeOnce;
+    }
+  
+    public Object getExecuteOnceResult() {
+        return executeOnceResult;
+    }
+   
+    public void setExecuteOnceResult(Object executeOnceResult) {
+        this.executeOnceResult = executeOnceResult;
+    }
+   
+    public Object getExecuteOnAllResult() {
+        return executeOnAllResult;
+    }
+   
+    public void setExecuteOnAllResult(Object executeOnAllResult) {
+        this.executeOnAllResult = executeOnAllResult;
+    }
+    
+}
