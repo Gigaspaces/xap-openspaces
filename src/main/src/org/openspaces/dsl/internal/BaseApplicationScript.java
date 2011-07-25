90566f5af0c4efeaaf89f524b3da5aa0f78a1b92
barakme <barakme@localhost>
2011-07-25 13:52:48 +0000

    CLOUDIFY-35
    Added Application Pojo and first run at Application Parser/installer.
    
    svn path=/trunk/openspaces/; revision=93706
    
    
    Former-commit-id: 14579f1f15bda7ccbcb4b3ec9953bd528d57f9a6

---
+++ src/main/src/org/openspaces/dsl/internal/BaseApplicationScript.java
@@ -0,0 +1,103 @@
+package org.openspaces.dsl.internal;
+
+import groovy.lang.Closure;
+import groovy.lang.Script;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.openspaces.dsl.Application;
+
+public abstract class BaseApplicationScript extends Script {
+
+    private static java.util.logging.Logger logger =
+            java.util.logging.Logger.getLogger(BaseApplicationScript.class.getName());
+
+    private Object activeObject = null;
+    private Application application;
+
+    @Override
+    public void setProperty(final String name, final Object value) {
+        try {
+            BeanUtils.setProperty(this.activeObject, name, value);
+        } catch (final IllegalAccessException e) {
+            throw new IllegalArgumentException("Failed to set application property " + name + " to " + name, e);
+        } catch (final InvocationTargetException e) {
+            throw new IllegalArgumentException("Failed to set application property " + name + " to " + name, e);
+        }
+    }
+
+    public Object methodMissing(final String name, final Object args) {
+        return null;
+        // final Object[] argsArray = (Object[]) args;
+        //
+        // // first check if this is an object declaration
+        // final Object obj = createDslObject(name);
+        // if (obj != null) {
+        //
+        // if (this.activeObject != null) {
+        // final Collection<Method> methods = this.activeMethods.values();
+        // for (final Method method : methods) {
+        // if (method.getName().startsWith("set") && (method.getParameterTypes().length == 1)
+        // && (method.getParameterTypes()[0].equals(obj.getClass()))) {
+        //
+        // try {
+        // method.invoke(this.activeObject, new Object[] { obj });
+        // } catch (final Exception e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        // break;
+        // }
+        // }
+        // }
+        // swapActiveObject((Closure<Object>) argsArray[0], obj);
+        // return obj;
+        // }
+        //
+        // // next check if this is a property assignment
+        //
+        // if (argsArray.length != 1) {
+        // throw new MissingMethodException(name, Service.class, argsArray);
+        // }
+        //
+        // final Object arg = argsArray[0];
+        //
+        // final String methodName = "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
+        //
+        // try {
+        // final Method m = this.activeMethods.get(methodName);
+        // if (m != null) {
+        // m.invoke(this.activeObject, arg);
+        // } else {
+        // logger.severe("Method " + methodName + " not found on object: " + this.activeObject);
+        // throw new MissingMethodException(name, this.activeObject.getClass(), new Object[0]);
+        //
+        // }
+        // } catch (final Exception e) {
+        // logger.log(Level.SEVERE, "Failed to invoke method " + methodName, e);
+        // throw new IllegalStateException("Failed to invoke method " + methodName
+        // + " on object " + this.activeObject, e);
+        // }
+        //
+        // return this.activeObject;
+    }
+
+    public Application application(final Closure<Object> closure) {
+        this.application = new Application();
+        // if (context == null) {
+        // context = new ServiceContext(service, null, null); //TODO - fix this
+        // }
+
+        this.activeObject = this.application;
+        closure.call();
+        return this.application;
+
+    }
+
+    @Override
+    public void println(final Object obj) {
+        logger.info(obj.toString());
+    }
+
+}
--- src/main/src/org/openspaces/dsl/internal/ServiceReader.java
+++ src/main/src/org/openspaces/dsl/internal/ServiceReader.java
@@ -4,26 +4,39 @@ import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.control.customizers.ImportCustomizer;
 import org.openspaces.admin.Admin;
 import org.openspaces.core.cluster.ClusterInfo;
+import org.openspaces.dsl.Application;
 import org.openspaces.dsl.Service;
 import org.openspaces.dsl.context.ServiceContext;
-import org.openspaces.dsl.ui.BarLineChart.Unit;
 
 public class ServiceReader {
 
+    /*****
+     * Private Constructor to prevent instantitaion.
+     *
+     */
+    private ServiceReader() {
+        
+    }
+    
     /****************
-     * Reads a service object from a groovy DSL file placed in the given directory.
-     * The file name must be of the format *-service.groovy, and there must be exactly
-     * one file in the directory with a name that matches this format.
-     * @param dir the directory to scan for the DSL file.
+     * Reads a service object from a groovy DSL file placed in the given directory. The file name
+     * must be of the format *-service.groovy, and there must be exactly one file in the directory
+     * with a name that matches this format.
+     * 
+     * @param dir
+     *            the directory to scan for the DSL file.
      * @return the service
      * @throws Exception
      */
@@ -56,7 +69,7 @@ public class ServiceReader {
     public static Service getServiceFromFile(final File dslFile, final File workDir, final Admin admin,
             final ClusterInfo clusterInfo) {
 
-        final GroovyShell gs = ServiceReader.createGroovyShell(null);
+        final GroovyShell gs = ServiceReader.createGroovyShellForService();
 
         Object result = null;
         try {
@@ -85,8 +98,16 @@ public class ServiceReader {
         return service;
     }
 
-    private static GroovyShell createGroovyShell(final String workDir) {
-        final CompilerConfiguration cc = ServiceReader.createCompilerConfiguration();
+    private static GroovyShell createGroovyShellForService() {
+        return createGroovyShell(BaseServiceScript.class.getName());
+    }
+    
+    private static GroovyShell createGroovyShellForApplication() {
+        return createGroovyShell(BaseApplicationScript.class.getName());
+    }
+    
+    private static GroovyShell createGroovyShell(final String baseClassName) {
+        final CompilerConfiguration cc = ServiceReader.createCompilerConfiguration(baseClassName);
         final Binding binding = new Binding();
 
         final GroovyShell gs = new GroovyShell(
@@ -95,18 +116,110 @@ public class ServiceReader {
                 cc);
         return gs;
     }
+    
+    
 
-    private static CompilerConfiguration createCompilerConfiguration() {
+    private static CompilerConfiguration createCompilerConfiguration(String baseClassName) {
         final CompilerConfiguration cc = new CompilerConfiguration();
         final ImportCustomizer ic = new ImportCustomizer();
         ic.addStarImports("org.openspaces.dsl", "org.openspaces.dsl.ui", "org.openspaces.dsl.context");
+        ic.addImports("org.openspaces.dsl.ui.BarLineChart.Unit");
         // ic.addStaticStars(USMUtils.class.getName());
-        ic.addImports(Unit.class.getName());
+        // ic.addImports(Unit.class.getName());
         cc.addCompilationCustomizers(ic);
 
-        cc.setScriptBaseClass(BaseServiceScript.class.getName());
+         
+        cc.setScriptBaseClass(baseClassName);
 
         return cc;
     }
 
+    // TODO - Support Zip files in application
+    public static Application getApplicationFromFile(File inputFile) throws IOException {
+        File dslFile = getApplicationDslFile(inputFile);
+        Application app = readApplicationFromFile(dslFile);
+        File appDir = dslFile.getParentFile();
+        List<String> serviceNames = app.getServiceNames();
+        List<Service> services = new ArrayList<Service>(serviceNames.size());
+        for (String serviceName : serviceNames) {
+            Service service = readApplicationService(app, serviceName, appDir);
+            services.add(service);
+        }
+        app.setServices(services);
+        
+        return app;
+        
+    }
+    private static File getApplicationDslFile(File inputFile) throws FileNotFoundException {
+        if(!inputFile.exists()) {
+            throw new FileNotFoundException("Could not find file: " + inputFile);
+        }
+        
+        if(inputFile.isFile()) { 
+            return inputFile;
+        }
+        
+        if(inputFile.isDirectory()) {
+            File[] files = inputFile.listFiles(new FilenameFilter() {                
+                public boolean accept(File dir, String name) {
+                    return (name.endsWith("-application.groovy"));
+                }
+            });
+            
+            if(files.length != 1) {
+                throw new IllegalArgumentException("Expected to find one application file, found " + files.length);
+            }
+            
+            return files[0];
+        }
+        
+        throw new IllegalStateException("Could not find File: " + inputFile);
+        
+        
+    }
+
+    private static Service readApplicationService(Application app, String serviceName, File appDir) throws FileNotFoundException {
+        File serviceDir = new File(appDir, serviceName);
+        if(!serviceDir.exists()) {
+            throw new FileNotFoundException("Could not find directory " + serviceDir + " for service " + serviceName);
+        }
+        
+        return getServiceFromFile(serviceDir);
+        
+    }
+
+    private static Application readApplicationFromFile(File dslFile) throws IOException {
+        
+        if(!dslFile.exists()) {
+             throw new FileNotFoundException(dslFile.getAbsolutePath());
+        }
+        final GroovyShell gs = ServiceReader.createGroovyShellForApplication();
+
+        Object result = null;
+        try {
+            result = gs.evaluate(dslFile);
+        } catch (final CompilationFailedException e) {
+            throw new IllegalArgumentException("The file " + dslFile +
+                    " could not be compiled", e);
+        } catch (final IOException e) {
+            throw new IllegalStateException("The file " + dslFile + " could not be read",
+                    e);
+        }
+
+        // final Object result = Eval.me(expr);
+        if (result == null) {
+            throw new IllegalStateException("The file: " + dslFile + " evaluates to null, not to an application object");
+        }
+        if (!(result instanceof Application)) {
+            throw new IllegalStateException("The file: " + dslFile + " did not evaluate to the required object type");
+        }
+
+        final Application application = (Application) result;
+
+//        final ServiceContext ctx = new ServiceContext(service, admin, workDir.getAbsolutePath(), clusterInfo);
+//        gs.getContext().setProperty("context", ctx);
+
+        return application;
+
+    }
 }
