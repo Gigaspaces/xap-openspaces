847a2adf1451f5e10dbe915fc89f480872f62ad2
barakme <barakme@localhost>
2011-07-25 13:52:59 +0000

    CLOUDIFY-35
    Added Application Pojo and first run at Application Parser/installer.
    
    svn path=/trunk/openspaces/; revision=93707
    
    
    Former-commit-id: c9d57034d44d83e3ae8ddc5fcd16c3272814b97a

---
+++ src/main/src/org/openspaces/dsl/Application.java
@@ -0,0 +1,37 @@
+package org.openspaces.dsl;
+
+import java.util.List;
+
+public class Application {
+
+    private String name;
+    
+    private List<String> serviceNames;
+    private List<Service> services;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getServiceNames() {
+        return serviceNames;
+    }
+
+    public void setServiceNames(List<String> serviceNames) {
+        this.serviceNames = serviceNames;
+    }
+
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+    
+    
+}
