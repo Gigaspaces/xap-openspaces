8acade024744f60533a0b47b3d89462a29390c5d
eitany <eitany@localhost>
2011-08-18 13:45:50 +0000

    GS-9245
    Deprecate BootstrapUtility
    
    svn path=/trunk/openspaces/; revision=95672
    
    
    Former-commit-id: 0555085792f62b7c40b0a01f59f5dc6553ea652e

--- src/main/src/org/openspaces/core/gateway/BootstrapUtility.java
+++ src/main/src/org/openspaces/core/gateway/BootstrapUtility.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.openspaces.admin.Admin;
+import org.openspaces.admin.gateway.GatewaySink;
+import org.openspaces.admin.gateway.GatewaySinkSource;
 import org.openspaces.admin.internal.pu.InternalProcessingUnitInstance;
 import org.openspaces.admin.pu.ProcessingUnit;
 import org.openspaces.admin.pu.ProcessingUnitInstance;
@@ -15,7 +17,9 @@ import org.openspaces.admin.pu.ProcessingUnitInstance;
  * Gateway bootstrap utilities
  * @author eitany
  * @since 8.0.3
+ * @deprecated Since 8.0.4 - use {@link GatewaySinkSource} API instead.
  */
+@Deprecated
 public class BootstrapUtility {
     
     /**
@@ -26,7 +30,9 @@ public class BootstrapUtility {
      * @param timeout timeout for operation
      * @param timeUnit
      * @throws Exception
+     * @deprecated Since 8.0.4 - use {@link GatewaySinkSource#bootstrapFromGatewayAndWait()} instead.
      */
+    @Deprecated
     public static void bootstrap(Admin admin, String gatewaySinkPuName, String sourceGatewayName, long timeout, TimeUnit timeUnit) throws Exception{
         ProcessingUnitInstance processingUnitInstance = locateSink(admin, gatewaySinkPuName, timeout, timeUnit);
         
@@ -42,7 +48,9 @@ public class BootstrapUtility {
      * @param admin admin of the service grid environment that contains the gateway sink processing unit
      * @param gatewaySinkPuName the name of the processing unit the gateway sink is deployed under
      * @throws TimeoutException 
+     * @deprecated Since 8.0.4 - use {@link GatewaySink#enableIncomingReplication()} instead.
      */
+    @Deprecated
     public static void enableIncomingReplication(Admin admin, String gatewaySinkPuName, long timeout, TimeUnit timeUnit) throws TimeoutException{
         ProcessingUnitInstance processingUnitInstance = locateSink(admin, gatewaySinkPuName, timeout, timeUnit);
         Map<String, Object> namedArgs = new HashMap<String, Object>();
