8917ce902789b20b144ea1907ed9868d5b86aee1
gal <gal@localhost>
2011-11-01 18:40:23 +0000

    Test before-authentication filter
    svn path=/xap/trunk/openspaces/; revision=102193
    
    
    Former-commit-id: 50f43d8e36d558d5d244008cd1f46b0d302d6763

---
+++ src/main/test/org/openspaces/itest/core/space/filter/security/SecurityFilter.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openspaces.itest.core.space.filter.security;
+
+import com.j_spaces.core.IJSpace;
+import com.j_spaces.core.SpaceContext;
+import com.j_spaces.core.filters.ISpaceFilter;
+import com.j_spaces.core.filters.entry.ISpaceFilterEntry;
+
+import org.junit.Assert;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.context.GigaSpaceLateContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author kimchy
+ */
+public class SecurityFilter implements ISpaceFilter {
+
+    private Map<Integer, Integer> stats = new HashMap<Integer, Integer>();
+
+    @GigaSpaceLateContext
+    GigaSpace gigaSpace;
+
+    public void init(IJSpace space, String filterId, String url, int priority) throws RuntimeException {
+
+    }
+
+    public void process(SpaceContext context, ISpaceFilterEntry entry, int operationCode) throws RuntimeException {
+        Integer counter = stats.get(operationCode);
+        if (counter == null) {
+            counter = 1;
+        } else {
+            counter = counter + 1;
+        }
+        stats.put(operationCode, counter);
+    }
+
+    public void process(SpaceContext context, ISpaceFilterEntry[] entries, int operationCode) throws RuntimeException {
+        Integer counter = stats.get(operationCode);
+        if (counter == null) {
+            counter = 1;
+        } else {
+            counter = counter + entries.length;
+        }
+        stats.put(operationCode, counter);
+    }
+
+    public void close() throws RuntimeException {
+    }
+
+    public Map<Integer, Integer> getStats() {
+        return stats;
+    }
+}
---
+++ src/main/test/org/openspaces/itest/core/space/filter/security/SecurityFilterTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openspaces.itest.core.space.filter.security;
+
+import java.rmi.RemoteException;
+import java.util.Properties;
+
+import com.gigaspaces.security.SecurityException;
+import com.j_spaces.core.LeaseContext;
+import com.j_spaces.core.SecurityContext;
+import com.j_spaces.core.filters.FilterOperationCodes;
+
+import org.junit.Assert;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.GigaSpaceConfigurer;
+import org.openspaces.core.space.SecurityConfig;
+import org.openspaces.core.space.UrlSpaceConfigurer;
+import org.openspaces.itest.core.space.filter.AllOperationsFilterUtil;
+import org.openspaces.itest.core.space.filter.SimpleFilter;
+import org.openspaces.itest.core.space.filter.adapter.Message;
+import org.openspaces.security.spring.SpringSecurityManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+
+/**
+ * @author kimchy
+ */
+public class SecurityFilterTest extends AbstractDependencyInjectionSpringContextTests {
+
+    protected SecurityFilter securityFilterCodeName;
+    protected SecurityFilter securityFilterCode;
+    protected ProviderManager authenticationManager;
+    protected GigaSpace gigaSpace;
+    protected SecurityFilter[] filters = new SecurityFilter[2];
+    
+    public SecurityFilterTest() {
+        setPopulateProtectedVariables(true);
+    }
+
+    @Override
+    protected String[] getConfigLocations() {
+        Properties props = new Properties();
+        props.setProperty(SpringSecurityManager.SPRING_SECURITY_CONFIG_LOCATION, "classpath:/org/openspaces/itest/core/space/filter/security/in-memory-security-config.xml");
+        new SpringSecurityManager().init(props);
+        try {
+            super.onSetUp();
+        } catch (Exception e) {
+           Assert.assertFalse(true);
+        }
+        return new String[]{"/org/openspaces/itest/core/space/filter/security/securityFilter.xml"};                      
+    }
+  
+    public void beforeTest(){
+        filters[0] = securityFilterCodeName;
+        filters[1] = securityFilterCode;
+        
+//        gigaSpace.takeMultiple(new Message());
+        AllOperationsFilterUtil.restartStats(filters);
+        AllOperationsFilterUtil.initialAssert(securityFilterCodeName.getStats() , "simpleFilterCodeName"); 
+        AllOperationsFilterUtil.initialAssert(securityFilterCode.getStats() , "simpleFilterCode"); 
+    }
+    
+    public void testWrite() throws SecurityException, RemoteException {
+        beforeTest();
+        Message message = new Message(1);
+        LeaseContext<Message> lease = gigaSpace.write(message);
+        assertNotNull(lease);
+        
+        AllOperationsFilterUtil.assertAfterWriteAuthentication(securityFilterCode.getStats(), "securityFilterCode");
+//        AllOperationsFilterUtil.assertAfterAuthentication(securityFilterCodeName.getStats(), "securityFilterCodeName");
+        
+    }
+//    
+//    public void testRead() {    
+//        beforeTest();
+//        Message message = new Message(1);        
+//        LeaseContext<Message> lease = gigaSpace.write(message);
+//        assertNotNull(lease);
+//        AllOperationsFilterUtil.restartStats(filters);
+//        message = gigaSpace.read(message);
+//        assertNotNull(message);
+//        
+//        AllOperationsFilterUtil.assertAfterReadAuthentication(securityFilterCode.getStats(), "securityFilterCode");
+////        AllOperationsFilterUtil.assertAfterAuthentication(securityFilterCodeName.getStats(), "securityFilterCodeName");
+//        
+//    }
+//    
+//    public void testTake() {    
+//        beforeTest();
+//        Message message = new Message(1);        
+//        LeaseContext<Message> lease = gigaSpace.write(message);
+//        assertNotNull(lease);
+//        AllOperationsFilterUtil.restartStats(filters);
+//        message = gigaSpace.take(message);
+//        assertNotNull(message);
+//        
+//        AllOperationsFilterUtil.assertAfterTakeAuthentication(securityFilterCode.getStats(), "securityFilterCode");
+////        AllOperationsFilterUtil.assertAfterAuthentication(securityFilterCodeName.getStats(), "securityFilterCodeName");
+//        
+//    }
+}
