1c4c12118c5378b89f1fc2efdafeea443c47936b
itaif <itaif@localhost>
2011-11-27 15:44:20 +0000

    GS-9696 Moved two methods from openspaces to gigaspaces in order for them to be later used by the GSM
    svn path=/xap/trunk/openspaces/; revision=103668
    
    
    Former-commit-id: 33819ea74a3be081b8bcbe0282e09abe40bbcd5e

--- src/main/src/org/openspaces/grid/esm/ESM.java
+++ src/main/src/org/openspaces/grid/esm/ESM.java
@@ -16,18 +16,4 @@ public interface ESM extends com.gigaspaces.grid.esm.ESM {
     void setProcessingUnitScaleStrategy(String puName, ScaleStrategyConfig scaleStrategyConfig) throws RemoteException;
 
     ScaleStrategyConfig getProcessingUnitScaleStrategyConfig(String processingUnitName) throws RemoteException;
-
-    /**
-     * @return true if the scale strategy of the specified processing unit is being monitored and corrective actions are enforced.
-     * In case the processing unit has been un-deployed returns true as long as the containers and machines shutdown is in progress.
-     * @since 8.0.5
-     */
-    boolean isManagingProcessingUnit(String processingUnitName) throws RemoteException;
-
-    /**
-     * @return true if the processing unit is managed by the ESM, and the scale SLA has been reached.
-     * In case the processing unit has been un-deployed returns false.
-     * @since 8.0.5
-     */
-    boolean isManagingProcessingUnitAndScaleNotInProgress(String processingUnitName)throws RemoteException;
 }
