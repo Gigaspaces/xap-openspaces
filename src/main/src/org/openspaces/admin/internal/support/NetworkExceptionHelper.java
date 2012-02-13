aee8a17a3961b0a84951e06051276b96fbf1ab0d
itaif <itaif@localhost>
2012-02-13 12:21:48 +0000

    GS-9894 Fixed NPE
    svn path=/xap/trunk/openspaces/; revision=109913
    
    
    Former-commit-id: 74886be377f3d6d6984ff1d5dea4bb273d78ef10

--- src/main/src/org/openspaces/admin/internal/support/NetworkExceptionHelper.java
+++ src/main/src/org/openspaces/admin/internal/support/NetworkExceptionHelper.java
@@ -10,21 +10,24 @@ import java.rmi.ConnectException;
 public abstract class NetworkExceptionHelper {
 
     public static boolean isConnectOrCloseException(Throwable e) {
-        if (e instanceof ConnectException) {
+        if (e instanceof ConnectException && 
+            e.getCause() != null && 
+            e.getCause() instanceof IOException) {
+            
             if (e.getCause() instanceof ClosedChannelException || e.getCause() instanceof java.net.ConnectException) {
                 return true;
             }
 
-            if (e.getCause() instanceof IOException && e.getCause().getMessage().contains("aborted")) {
+            if (e.getCause().getMessage() != null && e.getCause().getMessage().contains("aborted")) {
                 return true;
             }
 
-            if (e.getCause() instanceof IOException && e.getCause().getMessage().contains("Connection reset by peer")) {
+            if (e.getCause().getMessage() != null && e.getCause().getMessage().contains("Connection reset by peer")) {
                 return true;
             }
         }
         if (e instanceof IOException) {
-            if (e.getMessage().startsWith("Connection reset by peer")) {
+            if (e.getMessage() != null && e.getMessage().startsWith("Connection reset by peer")) {
                 return true;
             }
         }
