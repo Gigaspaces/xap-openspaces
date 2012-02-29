bb3f3d41281165c8e6d42a148af4f96ef6994111
elip <elip@localhost>
2012-02-29 13:03:26 +0000

    remove serialVersionUID for now. wait for anna to review
    svn path=/xap/trunk/openspaces/; revision=111188
    
    
    Former-commit-id: cdb0c39d1c6bd64dc45ea990d8e8002fa38a517a

--- tools/maven/maven-openspaces-plugin/src/main/resources/pu-templates/mule/common/src/main/java/${puGroupPath}/common/Data.java
+++ tools/maven/maven-openspaces-plugin/src/main/resources/pu-templates/mule/common/src/main/java/${puGroupPath}/common/Data.java
@@ -15,8 +15,6 @@ import com.gigaspaces.annotation.pojo.SpaceRouting;
  */
 @SpaceClass
 public class Data implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
 
     private String id;
 
