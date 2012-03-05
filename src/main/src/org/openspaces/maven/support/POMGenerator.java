954e9fafc1cfb8fd00c72834b15bb663c4f05585
sagib <sagib@localhost>
2012-03-05 11:35:17 +0000

    GS-9868 removed unused import
    svn path=/xap/trunk/openspaces/; revision=111571
    
    
    Former-commit-id: 17163b989f4bc64357be7fce9eaab21cffba6aca

--- src/main/src/org/openspaces/maven/support/POMGenerator.java
+++ src/main/src/org/openspaces/maven/support/POMGenerator.java
@@ -1,11 +1,19 @@
 package org.openspaces.maven.support;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 import org.springframework.core.JdkVersion;
 
 import com.j_spaces.kernel.PlatformVersion;
-import com.j_spaces.kernel.XapVersion;
-
-import java.io.*;
 
 /**
  * @author kimchy
