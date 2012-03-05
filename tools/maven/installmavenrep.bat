573e87fbcd7bfb1b85a7742999f6ce55b3b5f9a7
alexb <alexb@localhost>
2012-03-05 09:54:29 +0000

    GS-9868 XAP and Cloudify License: port to trunk from 8.0.6
    svn path=/xap/trunk/gigaspaces/; revision=111552
    
    
    Former-commit-id: cb4e560bd36b183d4f2b5194c918322501d29a79

--- tools/maven/installmavenrep.bat
+++ tools/maven/installmavenrep.bat
@@ -5,37 +5,73 @@ if "%1" == ""  (
 @call "%~dp0\..\..\bin\setenv.bat"
 )
 
-if "%1" == "version"  (
-%JAVACMD% -cp %GS_JARS% org.openspaces.maven.support.OutputVersion
+if "%1" == "edition"  (
+%JAVACMD% -cp %GS_JARS% org.openspaces.maven.support.OutputVersion edition
 goto END
 )
 
-FOR /F "usebackq tokens=*" %%i IN (`installmavenrep.bat version`) DO @set VERSION=%%i
+if "%1" == "XAP"  (
+%JAVACMD% -cp %GS_JARS% org.openspaces.maven.support.OutputVersion XAP
+goto END
+)
 
-%JAVACMD% -cp %GS_JARS% org.openspaces.maven.support.POMGenerator "%TEMP%" %VERSION% "%JSHOMEDIR%/tools/maven/maven-openspaces-plugin"
+if "%1" == "Cloudify"  (
+%JAVACMD% -cp %GS_JARS% org.openspaces.maven.support.OutputVersion Cloudify
+goto END
+)
+
+FOR /F "usebackq tokens=*" %%i IN (`installmavenrep.bat edition`) DO @set EDITION=%%i
+
+
+FOR /F "usebackq tokens=*" %%i IN (`installmavenrep.bat XAP`) DO @set XAP_VERSION=%%i
+
+
+%JAVACMD% -cp %GS_JARS% org.openspaces.maven.support.POMGenerator "%TEMP%" "%JSHOMEDIR%/tools/maven/maven-openspaces-plugin"
 
 echo ""
 echo ""
-echo "Installing Version %VERSION%"
+echo "Installing XAP %XAP_VERSION% jars"
 echo ""
 echo ""
 
 REM GigaSpaces Jars
-call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=%VERSION% -DpomFile=%TEMP%/gs-runtime-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/required/gs-runtime.jar" -Djavadoc="%JSHOMEDIR%/docs/javadoc.zip" 
-call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=%VERSION% -DpomFile=%TEMP%/gs-openspaces-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/required/gs-openspaces.jar" -Djavadoc="%JSHOMEDIR%/docs/javadoc.zip" -Dsources="%JSHOMEDIR%/lib/optional/openspaces/gs-openspaces-src.zip"  
-call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mule-os -Dversion=%VERSION% -Dpackaging=jar -DpomFile=%TEMP%/mule-os-pom.xml -Dfile="%JSHOMEDIR%/lib/optional/openspaces/mule-os.jar"
-call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=jetty-os -Dversion=%VERSION% -Dpackaging=jar -DpomFile=%TEMP%/jetty-os-pom.xml -Dfile="%JSHOMEDIR%/lib/platform/openspaces/gs-openspaces-jetty.jar"
+call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=%XAP_VERSION% -DpomFile=%TEMP%/gs-runtime-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/required/gs-runtime.jar" -Djavadoc="%JSHOMEDIR%/docs/javadoc.zip" 
+call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=%XAP_VERSION% -DpomFile=%TEMP%/gs-openspaces-pom.xml -Dpackaging=jar -Dfile="%JSHOMEDIR%/lib/required/gs-openspaces.jar" -Djavadoc="%JSHOMEDIR%/docs/javadoc.zip" -Dsources="%JSHOMEDIR%/lib/optional/openspaces/gs-openspaces-src.zip"  
+call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mule-os -Dversion=%XAP_VERSION% -Dpackaging=jar -DpomFile=%TEMP%/mule-os-pom.xml -Dfile="%JSHOMEDIR%/lib/optional/openspaces/mule-os.jar"
+call mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=jetty-os -Dversion=%XAP_VERSION% -Dpackaging=jar -DpomFile=%TEMP%/jetty-os-pom.xml -Dfile="%JSHOMEDIR%/lib/platform/openspaces/gs-openspaces-jetty.jar"
 
 REM Build and install OpenSpaces Maven Plugin 
 call mvn -f maven-openspaces-plugin/pom.xml install  -DcreateChecksum=true
 
+if "%EDITION%" == "XAP" (
 REM Copy licenese file
-call mvn os:install-license -Dfile="%JSHOMEDIR%\gslicense.xml" -Dversion=%VERSION%
+call mvn os:install-license -Dfile="%JSHOMEDIR%\gslicense.xml" -Dversion=%XAP_VERSION%
+)
+
+if "%EDITION%" == "Cloudify" FOR /F "usebackq tokens=*" %%i IN (`installmavenrep.bat Cloudify`) DO @set CLOUDIFY_VERSION=%%i
+
+if "%EDITION%" == "Cloudify" (
+
+echo ""
+echo ""
+echo "Installing Cloudify %CLOUDIFY_VERSION% jars"
+echo ""
+echo ""
+
+call mvn install:install-file -DgroupId=org.cloudifysource -DcreateChecksum=true -DartifactId=dsl -Dversion=%CLOUDIFY_VERSION% -Dpackaging=jar -DpomFile=%TEMP%/dsl-pom.xml -Dfile="%JSHOMEDIR%/lib/required/dsl.jar"
+call mvn install:install-file -DgroupId=org.cloudifysource -DcreateChecksum=true -DartifactId=usm -Dversion=%CLOUDIFY_VERSION% -Dpackaging=jar -DpomFile=%TEMP%/usm-pom.xml -Dfile="%JSHOMEDIR%/lib/platform/usm/usm.jar"
+)
+
 
 REM remove temp files
 del %TEMP%\gs-runtime-pom.xml
 del %TEMP%\gs-openspaces-pom.xml
 del %TEMP%\mule-os-pom.xml
 del %TEMP%\jetty-os-pom.xml
+
+if "%EDITION%" == "Cloudify" (
+del %TEMP%\usm-pom.xml
+del %TEMP%\dsl-pom.xml
+)
 
 :END
--- tools/maven/installmavenrep.sh
+++ tools/maven/installmavenrep.sh
@@ -9,30 +9,54 @@ if [ "${JSHOMEDIR}" = "" ] ; then
 fi
 export JSHOMEDIR
 export TMPDIR="/tmp"
-export VERSION=`${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.OutputVersion`
+export EDITION=`${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.OutputVersion edition`
 
-${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.POMGenerator $TMPDIR $VERSION "${JSHOMEDIR}/tools/maven/maven-openspaces-plugin"
+export XAP_VERSION=`${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.OutputVersion XAP`
+
+${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.POMGenerator $TMPDIR "${JSHOMEDIR}/tools/maven/maven-openspaces-plugin"
 
 echo ""
 echo ""
-echo "Installing Version $VERSION"
+echo "Installing XAP $XAP_VERSION jars"
 echo ""
 echo ""
 
 # GigaSpaces Jars
-mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=$VERSION -DpomFile=$TMPDIR/gs-runtime-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-runtime.jar -Djavadoc="${JSHOMEDIR}/docs/javadoc.zip"
-mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=$VERSION -DpomFile=$TMPDIR/gs-openspaces-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-openspaces.jar -Djavadoc="${JSHOMEDIR}/docs/javadoc.zip" -Dsources="${JSHOMEDIR}/lib/optional/openspaces/gs-openspaces-src.zip"
-mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mule-os -Dversion=$VERSION -Dpackaging=jar -DpomFile=$TMPDIR/mule-os-pom.xml -Dfile=${JSHOMEDIR}/lib/optional/openspaces/mule-os.jar
-mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=jetty-os -Dversion=$VERSION -Dpackaging=jar -DpomFile=$TMPDIR/jetty-os-pom.xml -Dfile=${JSHOMEDIR}/lib/platform/openspaces/gs-openspaces-jetty.jar
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=$XAP_VERSION -DpomFile=$TMPDIR/gs-runtime-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-runtime.jar -Djavadoc="${JSHOMEDIR}/docs/javadoc.zip"
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=$XAP_VERSION -DpomFile=$TMPDIR/gs-openspaces-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-openspaces.jar -Djavadoc="${JSHOMEDIR}/docs/javadoc.zip" -Dsources="${JSHOMEDIR}/lib/optional/openspaces/gs-openspaces-src.zip"
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mule-os -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=$TMPDIR/mule-os-pom.xml -Dfile=${JSHOMEDIR}/lib/optional/openspaces/mule-os.jar
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=jetty-os -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=$TMPDIR/jetty-os-pom.xml -Dfile=${JSHOMEDIR}/lib/platform/openspaces/gs-openspaces-jetty.jar
 
 # Build and install OpenSpaces Maven Plugin
 mvn -f maven-openspaces-plugin/pom.xml install -DcreateChecksum=true
 
-# Copy licenese file
-mvn os:install-license -Dfile=$JSHOMEDIR/gslicense.xml -Dversion=$VERSION
+if [ "${EDITION}" = "XAP" ] ; then
+	# Copy licenese file
+	mvn os:install-license -Dfile=$JSHOMEDIR/gslicense.xml -Dversion=$XAP_VERSION
+fi
+
+if [ "${EDITION}" = "Cloudify" ] ; then
+	export CLOUDIFY_VERSION=`${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.OutputVersion Cloudify`
+	
+	echo ""
+	echo ""
+	echo "Installing Cloudify $CLOUDIFY_VERSION jars"
+	echo ""
+	echo ""
+	
+	mvn install:install-file -DgroupId=org.cloudifysource -DcreateChecksum=true -DartifactId=dsl -Dversion=$CLOUDIFY_VERSION -DpomFile=$TMPDIR/dsl-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/dsl.jar 
+	mvn install:install-file -DgroupId=org.cloudifysource -DcreateChecksum=true -DartifactId=usm -Dversion=$CLOUDIFY_VERSION -DpomFile=$TMPDIR/usm-pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/platform/usm/usm.jar
+
+fi
 
 # Remove temp files
 rm $TMPDIR/gs-runtime-pom.xml
 rm $TMPDIR/gs-openspaces-pom.xml
 rm $TMPDIR/mule-os-pom.xml
 rm $TMPDIR/jetty-os-pom.xml
+
+if [ "${EDITION}" = "Cloudify" ] ; then
+	rm $TMPDIR/dsl-pom.xml
+	rm $TMPDIR/usm-pom.xml
+
+fi
