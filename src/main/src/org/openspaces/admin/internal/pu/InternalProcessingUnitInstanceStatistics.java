35b4c1e74d86568392c249ad40d77b9abbe9c6c3
itaif <itaif@localhost>
2012-03-13 10:57:13 +0000

    GS-10009 removed unused file
    svn path=/xap/trunk/openspaces/; revision=112367
    
    
    Former-commit-id: 592ba034e51f8fb63187af896141f31338ad66f6

--- src/main/src/org/openspaces/admin/internal/pu/InternalProcessingUnitInstanceStatistics.java
+++
@@ -1,8 +0,0 @@
-package org.openspaces.admin.internal.pu;
-
-import org.openspaces.admin.pu.ProcessingUnitInstanceStatistics;
-
-public interface InternalProcessingUnitInstanceStatistics extends ProcessingUnitInstanceStatistics {
-
-    //getCustomStatistics(String name);
-}
