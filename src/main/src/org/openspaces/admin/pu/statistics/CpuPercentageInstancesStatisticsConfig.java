457c9e7624b647f0e77483c4ce77a682aea594dc
gal <gal@localhost>
2012-04-02 13:27:36 +0000

    GS-9994 added config classes to get cpu usage
    svn path=/xap/trunk/openspaces/; revision=114593
    
    
    Former-commit-id: 8f7bc83ef1b14cbb091722ba4fb670f0bb70f5f4

---
+++ src/main/src/org/openspaces/admin/pu/statistics/CpuPercentageInstancesStatisticsConfig.java
@@ -0,0 +1,37 @@
+package org.openspaces.admin.pu.statistics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openspaces.admin.internal.pu.statistics.StatisticsObjectList;
+import org.openspaces.admin.internal.pu.statistics.StatisticsObjectListFunction;
+
+/**
+ * Calculates the cpu percentage of all cluster instances values.
+ * 
+ * @since 9.0.0
+ * @author itaif, gal
+ * 
+ */
+public class CpuPercentageInstancesStatisticsConfig 
+    extends AbstractTimeWindowStatisticsConfig 
+    implements StatisticsObjectListFunction, InstancesStatisticsConfig {
+
+    public CpuPercentageInstancesStatisticsConfig() {
+        this(new HashMap<String, String>());
+    }
+
+    public CpuPercentageInstancesStatisticsConfig(Map<String, String> properties) {
+        super(properties);
+    }
+
+    @Override
+    public void validate() throws IllegalStateException {
+        // ok
+    }
+
+    @Override
+    public Object calc(StatisticsObjectList values) {
+        return values.getDeltaValuePerNanoSecond();
+    }
+}
