b5cbac788665fb300a6a3d7a2a0fdc993a7b3f07
moran <moran@localhost>
2012-04-24 12:18:29 +0000

    AbstractFifoGroupingReceiveOperationHandler.java:46: warning - @param argument "useFifoGrouping-" is not a parameter name. AbstractFifoGroupsReceiveOperationHandler.java:33: warning - Tag @link: can't find FIFO_GROUPS_POLL in com.j_spaces.core.client.ReadModifiers AbstractFifoGroupsReceiveOperationHandler.java:33: warning - Tag @link: can't find FIFO_GROUPS_POLL in com.j_spaces.core.client.TakeModifiers AbstractFifoGroupsReceiveOperationHandler.java:47: warning - @param argument "fifoGroups-" is not a parameter name. AbstractFifoGroupsReceiveOperationHandler.java:47: warning - Tag @link: can't find FIFO_GROUPS_POLL in com.j_spaces.core.client.ReadModifiers AbstractFifoGroupsReceiveOperationHandler.java:47: warning - Tag @link: can't find FIFO_GROUPS_POLL in com.j_spaces.core.client.TakeModifiers
    svn path=/xap/trunk/openspaces/; revision=117086
    
    
    Former-commit-id: ddfc885606d31a11f86fad05d3d070a8befb81d8

--- src/main/src/org/openspaces/events/polling/receive/AbstractFifoGroupsReceiveOperationHandler.java
+++ src/main/src/org/openspaces/events/polling/receive/AbstractFifoGroupsReceiveOperationHandler.java
@@ -22,7 +22,7 @@ import com.j_spaces.core.client.TakeModifiers;
 
 /**
  * Support class to perform receive operations with or without Fifo Group.
- * <p>If configured to use Fifo Groups, the read/take operation will use {@link ReadModifiers#FIFO_GROUPS_POLL} / {@link TakeModifiers#FIFO_GROUPS_POLL} accordingly.
+ * <p>If configured to use Fifo Groups, the read/take operation will use {@link ReadModifiers#FIFO_GROUPING_POLL} / {@link TakeModifiers#FIFO_GROUPING_POLL} accordingly.
  * <ul><b>Note:</b> 
  * <li>All the handlers that uses the Fifo Groups capability should be used with a template that uses Fifo Groups </li>
  * <li>All the handlers that uses the Fifo Groups capability must be performed under a transaction </li>
@@ -42,7 +42,7 @@ public abstract class AbstractFifoGroupsReceiveOperationHandler extends Abstract
     /**
      * Allows to configure the take/read operation to be performed in a Fifo Groups manner.
      * 
-     * @param fifoGroups- if true, will use {@link ReadModifiers#FIFO_GROUPS_POLL} / {@link TakeModifiers#FIFO_GROUPS_POLL} as read/take modifiers.
+     * @param fifoGroups if true, will use {@link ReadModifiers#FIFO_GROUPING_POLL} / {@link TakeModifiers#FIFO_GROUPING_POLL} as read/take modifiers.
      */
     public void setFifoGroups(boolean fifoGroups) {
         this.fifoGroups = fifoGroups;
