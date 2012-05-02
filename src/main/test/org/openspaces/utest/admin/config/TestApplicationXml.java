be3cfff5b876ae473b8a700af7ee69faa89e52ea
itaif <itaif@localhost>
2012-05-02 13:15:15 +0000

    GS-10139 First version of xml parsing unit tests
    svn path=/xap/trunk/openspaces/; revision=117931
    
    
    Former-commit-id: d75fd551de06d2c3687badd8ef416d189ef2f5cc

---
+++ src/main/test/org/openspaces/utest/admin/config/TestApplicationXml.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.openspaces.utest.admin.config;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.openspaces.admin.application.ApplicationDeployment;
+import org.openspaces.admin.application.config.ApplicationConfig;
+import org.openspaces.admin.pu.ProcessingUnitDeployment;
+import org.openspaces.admin.space.SpaceDeployment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author itaif
+ * @since 9.0.1
+ */
+
+public class TestApplicationXml extends TestCase {
+
+    public void testRelocationSchema() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/org/openspaces/utest/admin/config/test-application.xml");
+        ApplicationConfig applicationConfig = context.getBean(org.openspaces.admin.application.config.ApplicationConfig.class);
+        
+        Assert.assertNotNull(applicationConfig);
+        Assert.assertEquals(expectedResult(),applicationConfig);
+    }
+
+    private ApplicationConfig expectedResult() {
+        ApplicationDeployment applicationDeployment = new ApplicationDeployment("test-application")
+        .addProcessingUnitDeployment(new SpaceDeployment("test-space"))//.addDependency("test-pu"))
+        .addProcessingUnitDeployment(new ProcessingUnitDeployment("test-pu.jar"));
+        return applicationDeployment.create();
+    }
+}
