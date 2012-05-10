be9d60505e423afd6d6df153294bcedd86abf352
eitany <eitany@localhost>
2012-05-10 12:26:01 +0000

    GS-10196
    Add parsing tests to transaction consolidation interceptor in gateway xsd
    
    svn path=/xap/trunk/openspaces/; revision=118596
    
    
    Former-commit-id: 3133ea86fc57db2cbc7069fbf58b27f2891fb1d9

---
+++ src/main/test/org/openspaces/itest/gateway/GatewaySinkWithInterceptorConfigurationTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * 
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ ******************************************************************************/
+package org.openspaces.itest.gateway;
+
+import org.openspaces.core.gateway.GatewaySinkFactoryBean;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+
+import com.gigaspaces.cluster.replication.gateway.transaction.TransactionConsolidationInterceptor;
+
+/**
+ * Test Sink component spring configuration
+ * 
+ * @author idan
+ * @since 8.0.3
+ *
+ */
+@SuppressWarnings("deprecation")
+public class GatewaySinkWithInterceptorConfigurationTest extends AbstractDependencyInjectionSpringContextTests {
+
+    public GatewaySinkWithInterceptorConfigurationTest() {
+        setPopulateProtectedVariables(true);
+    }
+    
+    protected String[] getConfigLocations() {
+        return new String[]{"/org/openspaces/itest/gateway/sinkinterceptor.xml"};
+    }
+    
+    protected GatewaySinkFactoryBean sink;
+    
+    public void testClusterConfiguration() throws SecurityException, NoSuchFieldException {
+        TransactionConsolidationInterceptor interceptor = sink.getDistributedTransactionProcessingConfiguration().getDistributedTransactionConsolidationInterceptor();
+        assertNotNull(interceptor);
+        assertTrue(interceptor instanceof MyTransactionConsolidationInterceptor);
+    }
+
+
+
+    
+}
