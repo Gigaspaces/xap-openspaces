918a695c809cb02c9f8a97df7e3d30e88913ef68
itaif <itaif@localhost>
2012-05-22 13:55:48 +0000

    GS-10285 refactoring of deploy command to base class - before adding undeploy command
    svn path=/xap/trunk/openspaces/; revision=119616
    
    
    Former-commit-id: 418a82e7e76b7a3c241ec506264859ae5d31e2c6

---
+++ src/main/src/org/openspaces/admin/application/deploy/AbstractApplicationCommand.java
@@ -0,0 +1,235 @@
+/*******************************************************************************
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.openspaces.admin.application.deploy;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openspaces.admin.Admin;
+import org.openspaces.admin.AdminFactory;
+import org.openspaces.admin.gsm.GridServiceManager;
+import org.openspaces.pu.container.support.CommandLineParser;
+
+import com.gigaspaces.security.directory.User;
+import com.gigaspaces.security.directory.UserDetails;
+
+
+/**
+ * @author itaif
+ * @since 9.0.1
+ */
+public abstract class AbstractApplicationCommand {
+
+        private final Log logger = LogFactory.getLog(this.getClass());
+
+        private String[] groups;
+
+        public Boolean getSecured() {
+            return secured;
+        }
+
+        public void setSecured(Boolean secured) {
+            this.secured = secured;
+        }
+
+        protected Log getLogger() {
+            return logger;
+        }
+
+        protected String[] getGroups() {
+            return groups;
+        }
+
+        protected String getLocators() {
+            return locators;
+        }
+
+        protected int getLookupTimeout() {
+            return lookupTimeout;
+        }
+
+        protected long getDeployTimeout() {
+            return deployTimeout;
+        }
+
+        protected boolean isSout() {
+            return sout;
+        }
+
+        protected boolean isDisableInfoLogging() {
+            return disableInfoLogging;
+        }
+
+        protected UserDetails getUserDetails() {
+            return userDetails;
+        }
+
+        protected boolean getManaged() {
+            return managed;
+        }
+
+        private String locators;
+
+        private int lookupTimeout = 5000;
+        
+        private long deployTimeout = Long.MAX_VALUE;
+
+        private boolean sout = false;
+
+        private boolean disableInfoLogging = false;
+
+        public void setDisableInfoLogging(boolean disableInfoLogging) {
+            this.disableInfoLogging = disableInfoLogging;
+        }
+
+        public void setSout(boolean sout) {
+            this.sout = sout;
+        }
+
+        public void setGroups(String[] groups) {
+            this.groups = groups;
+        }
+
+        public void setLocators(String locators) {
+            this.locators = locators;
+        }
+
+        public void setLookupTimeout(int lookupTimeout) {
+            this.lookupTimeout = lookupTimeout;
+        }
+
+        private Boolean secured;
+
+        public void setSecured(boolean secured) {
+            this.secured = secured;
+        }
+        
+        private UserDetails userDetails;
+
+        private boolean managed = false;
+
+        public void setUserDetails(UserDetails userDetails) {
+            this.userDetails = userDetails;
+        }
+
+        public void setUserDetails(String userName, String password) {
+            this.userDetails = new User(userName, password);
+        }
+        
+        public void setDeployTimeout(long deployTimeout) {
+            this.deployTimeout = deployTimeout;
+        }
+        
+        public void setManaged(boolean managed) {
+            this.managed = managed;
+        }
+        
+        protected void info(String message) {
+            if (disableInfoLogging) {
+                return;
+            }
+            if (sout) {
+                System.out.println(message);
+            }
+            if (logger.isInfoEnabled()) {
+                logger.info(message);
+            }
+        }
+                
+        
+        protected void parseArgs(String[] args) {
+            CommandLineParser.Parameter[] params = CommandLineParser.parse(args, args.length - 1);
+            String username = null;
+            String password = null;
+            for (CommandLineParser.Parameter param : params) {
+                
+                if (param.getName().equalsIgnoreCase("groups")) {
+                    setGroups(param.getArguments());
+                }
+                
+                if (param.getName().equalsIgnoreCase("locators")) {
+                    StringBuilder sb = new StringBuilder();
+                    for (String arg : param.getArguments()) {
+                        sb.append(arg).append(',');
+                    }
+                    setLocators(sb.toString());
+                }
+                
+                if (param.getName().equalsIgnoreCase("timeout")) {
+                    setLookupTimeout(Integer.valueOf(param.getArguments()[0]));
+                }
+                
+                if (param.getName().equalsIgnoreCase("deploy-timeout")) {
+                    setDeployTimeout(Long.valueOf(param.getArguments()[0]));
+                }
+                
+                if (param.getName().equals("user")) {
+                    username = param.getArguments()[0];
+                } 
+                
+                if (param.getName().equals("password")) {
+                    password = param.getArguments()[0];
+                }
+                
+                if (param.getName().equals("secured")) {
+                    if (param.getArguments().length == 0) {
+                        setSecured(true);
+                    } else {
+                        setSecured(Boolean.parseBoolean(param.getArguments()[0]));
+                    }
+                }
+            }
+            
+            if (username != null && password != null) {
+                setUserDetails(username, password);
+            }
+           
+        }
+
+
+        protected GridServiceManager waitForGridServiceManager() throws TimeoutException {
+            
+            final Admin admin = createAdmin();
+            final GridServiceManager gsm = admin.getGridServiceManagers().waitForAtLeastOne(lookupTimeout,TimeUnit.MILLISECONDS);
+            if (gsm == null) {
+                throw new TimeoutException("GSM discovery timed out.");
+            }
+            return gsm;
+        }
+
+        private Admin createAdmin() {
+            AdminFactory adminFactory = new AdminFactory().useDaemonThreads(true);
+            
+            if (locators != null) {
+                adminFactory.addLocators(locators);
+            }
+            
+            if (groups != null) {
+                for (String group : groups) {
+                    adminFactory.addGroup(group);
+                }
+            }
+            
+            if (userDetails != null) {
+                adminFactory.userDetails(userDetails);
+            }
+            
+            final Admin admin = adminFactory.create();
+            return admin;
+        }
+}
--- src/main/src/org/openspaces/admin/application/deploy/ApplicationDeployCommand.java
+++ src/main/src/org/openspaces/admin/application/deploy/ApplicationDeployCommand.java
@@ -19,10 +19,6 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.openspaces.admin.Admin;
-import org.openspaces.admin.AdminFactory;
 import org.openspaces.admin.application.Application;
 import org.openspaces.admin.application.ApplicationFileDeployment;
 import org.openspaces.admin.application.config.ApplicationConfig;
@@ -30,10 +26,6 @@ import org.openspaces.admin.gsm.GridServiceManager;
 import org.openspaces.admin.pu.ProcessingUnit;
 import org.openspaces.admin.pu.config.UserDetailsConfig;
 import org.openspaces.admin.pu.topology.ProcessingUnitConfigHolder;
-import org.openspaces.pu.container.support.CommandLineParser;
-
-import com.gigaspaces.security.directory.User;
-import com.gigaspaces.security.directory.UserDetails;
 
 
 /**
@@ -41,80 +33,8 @@ import com.gigaspaces.security.directory.UserDetails;
  * @since 9.0.1
  * A utility class with a main() entry point that allows deploying applications from ant or the cli
  */
-public class ApplicationDeployCommand {
+public class ApplicationDeployCommand extends AbstractApplicationCommand {
 
-        private static final Log logger = LogFactory.getLog(ApplicationDeployCommand.class);
-
-        private String[] groups;
-
-        private String locators;
-
-        private int lookupTimeout = 5000;
-        
-        private long deployTimeout = Long.MAX_VALUE;
-
-        private boolean sout = false;
-
-        private boolean disableInfoLogging = false;
-
-        public void setDisableInfoLogging(boolean disableInfoLogging) {
-            this.disableInfoLogging = disableInfoLogging;
-        }
-
-        public void setSout(boolean sout) {
-            this.sout = sout;
-        }
-
-        public void setGroups(String[] groups) {
-            this.groups = groups;
-        }
-
-        public void setLocators(String locators) {
-            this.locators = locators;
-        }
-
-        public void setLookupTimeout(int lookupTimeout) {
-            this.lookupTimeout = lookupTimeout;
-        }
-
-        private Boolean secured;
-
-        public void setSecured(boolean secured) {
-            this.secured = secured;
-        }
-        
-        private UserDetails userDetails;
-
-        private boolean managed = false;
-
-        public void setUserDetails(UserDetails userDetails) {
-            this.userDetails = userDetails;
-        }
-
-        public void setUserDetails(String userName, String password) {
-            this.userDetails = new User(userName, password);
-        }
-        
-        public void setDeployTimeout(long deployTimeout) {
-            this.deployTimeout = deployTimeout;
-        }
-        
-        public void setManaged(boolean managed) {
-            this.managed = managed;
-        }
-        
-        private void info(String message) {
-            if (disableInfoLogging) {
-                return;
-            }
-            if (sout) {
-                System.out.println(message);
-            }
-            if (logger.isInfoEnabled()) {
-                logger.info(message);
-            }
-        }
-        
         public static void main(String[] args) throws Exception {
             ApplicationDeployCommand deployer = new ApplicationDeployCommand();
             if (args.length < 1) {
@@ -135,20 +55,19 @@ public class ApplicationDeployCommand {
                 
                 File applicationFolder = new File(args[args.length-1]);
                 
-                long end = System.currentTimeMillis() + deployTimeout;
-                
+                long end = System.currentTimeMillis() + getDeployTimeout();
                 
                 ApplicationConfig applicationConfig = new ApplicationFileDeployment(applicationFolder).create();
                 
                 // apply security properties to each pu in the application
                 for (ProcessingUnitConfigHolder puConfig : applicationConfig.getProcessingUnits()) {
-                    if (secured != null) {
-                        puConfig.setSecured(secured);
+                    if (getSecured() != null) {
+                        puConfig.setSecured(getSecured());
                     }
-                    if (userDetails != null) {
+                    if (getUserDetails() != null) {
                         UserDetailsConfig userDetailsConfig = new UserDetailsConfig();
-                        userDetailsConfig.setUsername(userDetails.getUsername());
-                        userDetailsConfig.setPassword(userDetails.getPassword());
+                        userDetailsConfig.setUsername(getUserDetails().getUsername());
+                        userDetailsConfig.setPassword(getUserDetails().getPassword());
                         puConfig.setUserDetails(userDetailsConfig);
                     }
                 }
@@ -173,97 +92,15 @@ public class ApplicationDeployCommand {
             }
         }
 
-        private void parseArgs(String[] args) {
-            CommandLineParser.Parameter[] params = CommandLineParser.parse(args, args.length - 1);
-            String username = null;
-            String password = null;
-            for (CommandLineParser.Parameter param : params) {
-                
-                if (param.getName().equalsIgnoreCase("groups")) {
-                    setGroups(param.getArguments());
-                }
-                
-                if (param.getName().equalsIgnoreCase("locators")) {
-                    StringBuilder sb = new StringBuilder();
-                    for (String arg : param.getArguments()) {
-                        sb.append(arg).append(',');
-                    }
-                    setLocators(sb.toString());
-                }
-                
-                if (param.getName().equalsIgnoreCase("timeout")) {
-                    setLookupTimeout(Integer.valueOf(param.getArguments()[0]));
-                }
-                
-                if (param.getName().equalsIgnoreCase("deploy-timeout")) {
-                    setDeployTimeout(Long.valueOf(param.getArguments()[0]));
-                }
-                
-                if (param.getName().equals("user")) {
-                    username = param.getArguments()[0];
-                } 
-                
-                if (param.getName().equals("password")) {
-                    password = param.getArguments()[0];
-                }
-                
-                if (param.getName().equals("secured")) {
-                    if (param.getArguments().length == 0) {
-                        setSecured(true);
-                    } else {
-                        setSecured(Boolean.parseBoolean(param.getArguments()[0]));
-                    }
-                }
-            }
-            
-            if (username != null && password != null) {
-                setUserDetails(username, password);
-            }
-           
-        }
-
-
-        private GridServiceManager waitForGridServiceManager() throws TimeoutException {
-            
-            final Admin admin = createAdmin();
-            final GridServiceManager gsm = admin.getGridServiceManagers().waitForAtLeastOne(lookupTimeout,TimeUnit.MILLISECONDS);
-            if (gsm == null) {
-                throw new TimeoutException("GSM discovery timed out.");
-            }
-            return gsm;
-        }
-
-        private Admin createAdmin() {
-            AdminFactory adminFactory = new AdminFactory().useDaemonThreads(true);
-            
-            if (locators != null) {
-                adminFactory.addLocators(locators);
-            }
-            
-            if (groups != null) {
-                for (String group : groups) {
-                    adminFactory.addGroup(group);
-                }
-            }
-            
-            if (userDetails != null) {
-                adminFactory.userDetails(userDetails);
-            }
-            
-            final Admin admin = adminFactory.create();
-            return admin;
-        }
-
-        
-        public String getUsage() {
+       public String getUsage() {
             StringBuilder sb = new StringBuilder();
-            if (!managed) {
+            if (!getManaged()) {
                 sb.append("Usage: ApplicationDeployCommand [-groups groups] [-locators hots1 hots2] [-timeout timeoutValue] [-user xxx -password yyy] [-secured true/false] Application_DirOrZip");
             } else {
                 sb.append("Usage: deploy-application [-user xxx -password yyy] [-secured true/false] Application_DirOrZip");
             }
             sb.append("\n    Application_DirOrZip: The path to the application direcoty or zip file containing application.xml and the PU jars.");
-            if (!managed) {
+            if (!getManaged()) {
                 sb.append("\n    -groups [groupName] [groupName] ...      : The lookup groups used to look up the GSM");
                 sb.append("\n    -locators [host1] [host2] ...            : The lookup locators used to look up the GSM");
                 sb.append("\n    -timeout [timeout value]                 : The timeout value of GSM lookup (defaults to 5000) in milliseconds");
@@ -279,6 +116,5 @@ public class ApplicationDeployCommand {
             sb.append("\n2. Deploy data-app.zip");
             sb.append("\n    - Deploys the application in the data-app zip file.");
             return sb.toString();
-        }
-
+      }
 }
