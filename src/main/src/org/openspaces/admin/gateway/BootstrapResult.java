af4cbe8f51d646903b8a19f4bc1d94466a507c67
eitany <eitany@localhost>
2012-06-19 10:05:10 +0000

    Fix some JDoc
    svn path=/xap/trunk/openspaces/; revision=122320
    
    
    Former-commit-id: 39f5cb588d8fea03a67983f17978ced41c545032

--- src/main/src/org/openspaces/admin/gateway/BootstrapResult.java
+++ src/main/src/org/openspaces/admin/gateway/BootstrapResult.java
@@ -19,8 +19,8 @@ package org.openspaces.admin.gateway;
 
 /**
  * A result of a bootstrap
- * @see GatewaySink#bootstrapFromGatewayAndWait(String, long, java.util.concurrent.TimeUnit)
  * 
+ * @see GatewaySinkSource#bootstrapFromGatewayAndWait(long, java.util.concurrent.TimeUnit)
  * @author eitany
  * @since 8.0.4
  */
