623298a226bd627ea710f37d3b253a2a0a03de40
eliranm <eliranm@localhost>
2012-09-12 16:43:04 +0000

    GS-10613
    * created view and remote service to display recipes.
    * created abstraction for topology bottom tab split panel.
    
    code reviewer: Evgeny Fisher
    
    svn path=/xap/trunk/openspaces/; revision=128892
    
    
    Former-commit-id: d4e5f7de8a1b26167333bde567535797fe918a8e

--- src/main/java/org/openspaces/admin/internal/utils/ContextPropertyUtils.java
+++ src/main/java/org/openspaces/admin/internal/utils/ContextPropertyUtils.java
@@ -32,6 +32,7 @@ public class ContextPropertyUtils {
     private static final String CONTEXT_PROPERTY_SERVICE_TIER_TYPE = "com.gs.service.type";
     private static final String CONTEXT_PROPERTY_SERVICE_ICON = "com.gs.service.icon";
     private static final String CONTEXT_PROPERTY_CLOUD_NAME = "com.gs.cloudify.cloud-name";
+    private static final String CONTEXT_PROPERTY_SERVICE_FILE_NAME = "com.gs.cloudify.service-file-name";
     
     public static ServiceTierType getTierType(InternalProcessingUnit processingUnit) {
         String tierType = getContextPropertyValue(processingUnit, CONTEXT_PROPERTY_SERVICE_TIER_TYPE);
@@ -40,21 +41,34 @@ public class ContextPropertyUtils {
         }
         return ServiceTierType.valueOf(tierType);
     }
+
+    public static String getServiceFileCustomName(InternalProcessingUnit processingUnit) {
+        return getContextPropertyValue(processingUnit, CONTEXT_PROPERTY_SERVICE_FILE_NAME);
+    }
     
     public static String getIconPath(InternalProcessingUnit processingUnit) {
         return getContextPropertyValue(processingUnit, CONTEXT_PROPERTY_SERVICE_ICON);
     }
     
     public static String getIconName(InternalProcessingUnit processingUnit) {
-        String iconPath = getIconPath( processingUnit );
-        if( iconPath == null ){
+        if (processingUnit == null) {
             return null;
         }
-        //find icon name after last /
-        int startIndex = iconPath.lastIndexOf( "/" ) >= 0 ? 
-                            iconPath.lastIndexOf( "/" ) + 1 : 0;
-        return iconPath.substring( startIndex );
+        String iconPath = getIconPath( processingUnit );
+        return getIconName(iconPath);
     }    
+    
+    /**
+     * @see #getIconPath(InternalProcessingUnit)
+     */
+    public static String getIconName(String iconPath) {
+        if (iconPath == null) {
+            return null;
+        }
+        // find icon name after last '/'
+        int startIndex = iconPath.lastIndexOf("/") >= 0 ? iconPath.lastIndexOf("/") + 1 : 0;
+        return iconPath.substring(startIndex);
+    }
     
     public static String getCloudName(InternalProcessingUnit processingUnit) {
         String cloudName = getContextPropertyValue(processingUnit, CONTEXT_PROPERTY_CLOUD_NAME);
