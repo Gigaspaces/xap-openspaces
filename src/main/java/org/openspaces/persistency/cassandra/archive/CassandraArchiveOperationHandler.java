c9e42a439b389504fa1964aacbd65e54ff4b5f46
dank <dank@localhost>
2012-11-27 11:41:09 +0000

    GS-10849 Continue cassandra archive operation handler implemention. missing exception handling.
    svn path=/xap/trunk/openspaces/; revision=134125
    
    
    Former-commit-id: 47c612336186ad928d77f80c698e584b72b10962

--- src/main/java/org/openspaces/persistency/cassandra/archive/CassandraArchiveOperationHandler.java
+++ src/main/java/org/openspaces/persistency/cassandra/archive/CassandraArchiveOperationHandler.java
@@ -15,18 +15,27 @@
  *******************************************************************************/
 package org.openspaces.persistency.cassandra.archive;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.openspaces.archive.ArchiveOperationHandler;
 import org.openspaces.core.GigaSpace;
 import org.openspaces.persistency.cassandra.HectorCassandraClient;
+import org.openspaces.persistency.cassandra.error.CassandraTypeIntrospectionException;
+import org.openspaces.persistency.cassandra.meta.ColumnFamilyMetadata;
 import org.openspaces.persistency.cassandra.meta.conversion.ColumnFamilyNameConverter;
-import org.openspaces.persistency.cassandra.meta.conversion.DefaultColumnFamilyNameConverter;
+import org.openspaces.persistency.cassandra.meta.data.ColumnFamilyRow;
+import org.openspaces.persistency.cassandra.meta.data.ColumnFamilyRow.ColumnFamilyRowType;
 import org.openspaces.persistency.cassandra.meta.mapping.DefaultSpaceDocumentColumnFamilyMapper;
-import org.openspaces.persistency.cassandra.meta.mapping.filter.DefaultFlattenedPropertiesFilter;
 import org.openspaces.persistency.cassandra.meta.mapping.filter.FlattenedPropertiesFilter;
 import org.openspaces.persistency.cassandra.meta.types.dynamic.PropertyValueSerializer;
 import org.springframework.beans.factory.annotation.Required;
+
+import com.gigaspaces.document.SpaceDocument;
+import com.gigaspaces.metadata.SpaceTypeDescriptor;
+import com.gigaspaces.metadata.SpaceTypeDescriptorBuilder;
 
 /**
  * @author Dan Kilman
@@ -45,7 +54,6 @@ public class CassandraArchiveOperationHandler implements ArchiveOperationHandler
     private PropertyValueSerializer fixedPropertyValueSerializer;
     private FlattenedPropertiesFilter flattenedPropertiesFilter;
     private ColumnFamilyNameConverter columnFamilyNameConverter;
-    
     
     //helper object
     private DefaultSpaceDocumentColumnFamilyMapper mapper;
@@ -75,13 +83,8 @@ public class CassandraArchiveOperationHandler implements ArchiveOperationHandler
             throw new IllegalArgumentException("hectorClient cannot be null");
         }
         
-        if (columnFamilyNameConverter == null) {
-          //default value
-            columnFamilyNameConverter = new DefaultColumnFamilyNameConverter();
-        }
-        
         if (maxNestingLevel == null) {
-          //default value
+            //default value
             maxNestingLevel = DEFAULT_MAX_NESTING_LEVEL;
         }
         
@@ -89,16 +92,89 @@ public class CassandraArchiveOperationHandler implements ArchiveOperationHandler
                 null,                                           
                 flattenedPropertiesFilter, 
                 columnFamilyNameConverter,
-                maxNestingLevel);    
+                maxNestingLevel);
+        
     }
     
-    //TODO: implement
+    //TODO CAS: finish implementation
+    /**
+     * @see ArchiveOperationHandler#archive(Object...)
+     * 
+     * Known Limitation:
+     *  1. The archiver must not write two different entries with the same ID.
+     *     This would corrupt the entry in Cassandra.
+     *  2. Only Space Documents are supported
+     */
     @Override
     public void archive(Object... objects) {
+        List<ColumnFamilyRow> rows = new LinkedList<ColumnFamilyRow>();
+        
+        for (Object object : objects) {
 
+            if (!(object instanceof SpaceDocument)) {
+                //TODO CAS: throw exception
+                //errorHandler.onDataSyncOperationError(dataSyncOperation, 
+                //                                      InvalidDataSyncOperationReason.NotSupportsAsDocument,
+                //                                      null);
+            }
+            
+            SpaceDocument spaceDoc = (SpaceDocument) object;
+            String typeName = spaceDoc.getTypeName();
+            ColumnFamilyMetadata metadata = hectorClient.getColumnFamilyMetadata(typeName);
+            if (metadata == null) {
+                metadata = createColumnFamilyMetadata(typeName);
+                hectorClient.createColumnFamilyIfNecessary(metadata, false /* persist metadata */);
+            }
+
+            String keyName = metadata.getKeyName();
+            Object keyValue = spaceDoc.getProperty(keyName);
+                
+            if (keyValue == null) {
+                //TODO CAS: throw exception
+                //errorHandler.onDataSyncOperationError(dataSyncOperation, 
+                //                                       InvalidDataSyncOperationReason.MissingIDValue,
+                //                                       null);
+            }
+            ColumnFamilyRow columnFamilyRow;
+            try {
+                columnFamilyRow = 
+                        mapper.toColumnFamilyRow(metadata, 
+                                                 spaceDoc, 
+                                                 ColumnFamilyRowType.Write,
+                                                 false);
+                rows.add(columnFamilyRow);
+            }
+            catch (CassandraTypeIntrospectionException e) {
+                //TODO CAS:throw exception
+            }
+        }
+
+        hectorClient.performBatchOperation(rows);
     }
 
-    //TODO: implement
+    private ColumnFamilyMetadata createColumnFamilyMetadata(String typeName)
+    {
+        SpaceTypeDescriptor typeDesc = gigaSpace.getTypeManager().getTypeDescriptor(typeName);
+        String keyName = typeDesc.getIdPropertyName();
+        Class<?> keyType = typeDesc.getFixedProperty(keyName).getType();
+        SpaceTypeDescriptor dynamicTypeDesc = new SpaceTypeDescriptorBuilder(typeName)
+            .addFixedProperty(keyName, keyType)
+            .idProperty(keyName)
+            // TODO CAS: handle column families with no columns
+            .addFixedProperty("stub", Object.class)
+            .create();
+        return mapper.toColumnFamilyMetadata(dynamicTypeDesc);
+    }
+    
+    //TODO CAS: implement
+    /**
+     * @see ArchiveOperationHandler#supportsBatchArchiving() 
+     * @return true - 
+     *         Since write implementation is idempotent. 
+     *         Multiple archiving of the exact same objects is supported as long as
+     *         there is only one instance of {@link CassandraArchiveOperationHandler} that 
+     *         tries to archive it.
+     */
     @Override
     public boolean supportsBatchArchiving() {
         return true;
