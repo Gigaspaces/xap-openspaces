24cbab8cf54bd61bcbf21270c2e1cfe8d8545540
itaif <itaif@localhost>
2012-11-28 07:56:32 +0000

    GS-10849 Added verification that column created in cassandra when using cassandra archiver
    svn path=/xap/trunk/openspaces/; revision=134228
    
    
    Former-commit-id: 40667b286dba1cf13bc44bdc066d09613acb193b

--- src/test/java/org/openspaces/itest/persistency/cassandra/archive/TestCassandaraArchiveOperationHandler.java
+++ src/test/java/org/openspaces/itest/persistency/cassandra/archive/TestCassandaraArchiveOperationHandler.java
@@ -15,9 +15,19 @@
  *******************************************************************************/
 package org.openspaces.itest.persistency.cassandra.archive;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+
+import junit.framework.Assert;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.service.template.ColumnFamilyResult;
+import me.prettyprint.cassandra.service.template.ColumnFamilyRowMapper;
+import me.prettyprint.cassandra.service.template.ThriftColumnFamilyTemplate;
+import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.api.factory.HFactory;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -51,7 +61,15 @@ import com.j_spaces.core.IJSpace;
  */
 public class TestCassandaraArchiveOperationHandler {
 
-    private final Log logger = LogFactory.getLog(this.getClass());
+    private static final String SPACE_DOCUMENT_NAME_KEY = "Name";
+
+	private static final String SPACEDOCUMENT_NAME = "Anvil";
+
+	private static final String SPACEDOCUMENT_TYPENAME = "Product";
+
+	private static final String SPACEDOCUMENT_ID = "hw-1234";
+
+	private final Log logger = LogFactory.getLog(this.getClass());
     
     private final String TEST_NAMESPACE_XML = "/org/openspaces/itest/persistency/cassandra/archive/test-cassandra-archive-handler-namespace.xml";
     
@@ -144,25 +162,67 @@ public class TestCassandaraArchiveOperationHandler {
     }
 
     private void test(CassandraArchiveOperationHandler archiveHandler, GigaSpace gigaSpace) {
-         
-		if (!skipRegisterTypeDescriptor) {
-			SpaceTypeDescriptor typeDescriptor = 
-					new SpaceTypeDescriptorBuilder("Product")
-			.idProperty("CatalogNumber")
-			.routingProperty("Category")
-			.addPropertyIndex("Name", SpaceIndexType.BASIC)
-			.addPropertyIndex("Price", SpaceIndexType.EXTENDED)
-			.create();
-			gigaSpace.getTypeManager().registerTypeDescriptor(typeDescriptor);
+
+    	if (!skipRegisterTypeDescriptor) {
+			registerTypeDescriptor(gigaSpace);
 		}
 		
-		Map<String, Object> properties = new HashMap<String, Object>();
-		properties.put("CatalogNumber", "hw-1234");
-		properties.put("Category", "Hardware");
-		properties.put("Name", "Anvil");
-		properties.put("Price", 9.99f);
-		SpaceDocument document = new SpaceDocument("Product", properties);
-		archiveHandler.archive(document);
+		archiveHandler.archive(createSpaceDocument());
+		
+		verifyDocumentInCassandra();
     }
+
+	private SpaceDocument createSpaceDocument() {
+		final Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("CatalogNumber", SPACEDOCUMENT_ID);
+		properties.put("Category", "Hardware");
+		properties.put(SPACE_DOCUMENT_NAME_KEY, SPACEDOCUMENT_NAME);
+		properties.put("Price", 9.99f);
+		final SpaceDocument document = new SpaceDocument(SPACEDOCUMENT_TYPENAME, properties);
+		return document;
+	}
+
+	private void registerTypeDescriptor(GigaSpace gigaSpace) {
+		final SpaceTypeDescriptor typeDescriptor = 
+				new SpaceTypeDescriptorBuilder(SPACEDOCUMENT_TYPENAME)
+				.idProperty("CatalogNumber")
+				.routingProperty("Category")
+				.addPropertyIndex(SPACE_DOCUMENT_NAME_KEY, SpaceIndexType.BASIC)
+				.addPropertyIndex("Price", SpaceIndexType.EXTENDED)
+				.create();
+		gigaSpace.getTypeManager().registerTypeDescriptor(typeDescriptor);
+	}
+
+	private void verifyDocumentInCassandra() {
+		
+		Cluster cluster = HFactory.getOrCreateCluster("test-localhost:"+server.getPort(), server.getHost()+ ":" + server.getPort());
+		Keyspace keyspace = HFactory.createKeyspace(server.getKeySpaceName(), cluster);
+		
+		String columnFamilyName = SPACEDOCUMENT_TYPENAME; // as long as shorter than 40 bytes
+		ThriftColumnFamilyTemplate<String, String> template = new ThriftColumnFamilyTemplate<String, String>(
+				keyspace,
+				columnFamilyName,
+                StringSerializer.get(),
+                StringSerializer.get());
+		
+		Assert.assertTrue(SPACEDOCUMENT_TYPENAME + " does not exist",template.isColumnsExist(SPACEDOCUMENT_ID));
+
+		ColumnFamilyRowMapper<String, String, Object> mapper = new ColumnFamilyRowMapper<String, String, Object>() {
+			@Override
+		    public String mapRow(ColumnFamilyResult<String, String> rs) {
+				
+				for (String columnName : rs.getColumnNames()) {
+		            ByteBuffer bytes = rs.getColumn(columnName).getValueBytes();
+		            if (columnName.equals(SPACE_DOCUMENT_NAME_KEY)) {
+		            	return StringSerializer.get().fromByteBuffer(bytes);
+		            }
+				}
+				
+				return "Could not find column " + SPACE_DOCUMENT_NAME_KEY;
+		    }
+		};
+		Object name = template.queryColumns(SPACEDOCUMENT_ID, mapper);
+		Assert.assertEquals(SPACEDOCUMENT_NAME,name); 
+	}
 }
 
