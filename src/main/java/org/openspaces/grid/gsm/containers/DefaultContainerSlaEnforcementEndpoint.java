fa51976b41bc7ecd2b32cbf45dc09d54a010a343
itaif <itaif@localhost>
2013-10-31 13:06:35 +0000

    GS-11451 prevent ESM from killing containers not discovered by ESM, unless they were recently failed to start
    svn path=/xap/trunk/openspaces/; revision=159577
    
    
    Former-commit-id: db7872050271bb5fecaaa8fc960014b4504d2505

--- src/main/java/org/openspaces/grid/gsm/containers/DefaultContainerSlaEnforcementEndpoint.java
+++ src/main/java/org/openspaces/grid/gsm/containers/DefaultContainerSlaEnforcementEndpoint.java
@@ -424,7 +424,7 @@ class DefaultContainersSlaEnforcementEndpoint implements ContainersSlaEnforcemen
                         + " seconds ago since grid service agent no longer exists.");
                 state.removeFailedFuture(future);
             } else {
-                terminateOrphanContainersOfAgent(agent);
+                terminateOrphanContainersOfAgent(agent, future);
                 if (passedSeconds > START_CONTAINER_TIMEOUT_FAILURE_FORGET_SECONDS) {
                     logger.info("Forgetting failure to start container on machine "
                             + ContainersSlaUtils.machineToString(agent.getMachine()) + " that occured " + passedSeconds
@@ -435,8 +435,19 @@ class DefaultContainersSlaEnforcementEndpoint implements ContainersSlaEnforcemen
         }
     }
 
-    private void terminateOrphanContainersOfAgent(final GridServiceAgent agent) {
-        final Set<Integer> agentIds = new HashSet<Integer>();
+    private void terminateOrphanContainersOfAgent(final GridServiceAgent agent, final FutureGridServiceContainer suspectedFuture) {
+    	final int suspectedOrphanContainerAgentId;
+    	try {
+        	suspectedOrphanContainerAgentId = suspectedFuture.getAgentId();
+        }
+        catch (TimeoutException e) {
+        	return; // container not even started
+        }
+    	catch (ExecutionException e) {
+    		return; // container not even started
+    	}
+        
+    	final Set<Integer> agentIds = new HashSet<Integer>();
         // add all agent's containers process ids.
         for (final AgentProcessDetails processDetails : agent.getProcessesDetails()) {
             if (processDetails.getServiceType().toLowerCase().equals("gsc")) {
@@ -461,22 +472,23 @@ class DefaultContainersSlaEnforcementEndpoint implements ContainersSlaEnforcemen
                 }
             }
         }
-        
+
+        if (agentIds.contains(suspectedOrphanContainerAgentId)) {
         ((InternalAdmin) pu.getAdmin()).scheduleAdminOperation(new Runnable() {
             public void run() {
         
-                for (final int agentId : agentIds) {
-                    try {
-                        agent.killByAgentId(agentId);
-                        logger.warn("Terminated orphan container that did not register with lookup service on machine "
-                                + ContainersSlaUtils.machineToString(agent.getMachine()) + " agentId=" + agentId);
-                    } catch (final AdminException e) {
-                        logger.warn("Error terminating orphan container that did not register with lookup service on machine "
-                                + ContainersSlaUtils.machineToString(agent.getMachine()) + " agentId=" + agentId, e);
-                    }
+                try {
+                    agent.killByAgentId(suspectedOrphanContainerAgentId);
+                    logger.warn("Terminated orphan container that did not register with lookup service on machine "
+                            + ContainersSlaUtils.machineToString(agent.getMachine()) + " agentId=" + suspectedOrphanContainerAgentId);
+                } catch (final AdminException e) {
+                    logger.warn("Error terminating orphan container that did not register with lookup service on machine "
+                            + ContainersSlaUtils.machineToString(agent.getMachine()) + " agentId=" + suspectedOrphanContainerAgentId, e);
                 }
+          
             }
         });
+        }
     }
 
 } 
