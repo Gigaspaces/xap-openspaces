fbbb5aad7cf3f0fff752eaab2e57574177347a01
nivin <niv@gigaspaces.com>
2015-03-08 17:27:20 +0200

    GS-12296 - Merged AbstractNotifyEventListenerContainer into SimpleNotifyEventListenerContainer

--- src/main/java/org/openspaces/events/notify/NotifyComType.java
+++ src/main/java/org/openspaces/events/notify/NotifyComType.java
@@ -28,21 +28,21 @@ public enum NotifyComType {
      * uses TCP unicast communication which is usually best for small amount of registered clients.
      * This is the default communication type.
      */
-    UNICAST(AbstractNotifyEventListenerContainer.COM_TYPE_UNICAST),
+    UNICAST(SimpleNotifyEventListenerContainer.COM_TYPE_UNICAST),
 
     /**
      * Controls how notification are propagated from the space to the listener. Same as unicast ({@link #UNICAST})
      * in terms of communication protocol but uses a single client side multiplexer which handles
      * all the dispatching to the different notification listeners.
      */
-    MULTIPLEX(AbstractNotifyEventListenerContainer.COM_TYPE_MULTIPLEX),
+    MULTIPLEX(SimpleNotifyEventListenerContainer.COM_TYPE_MULTIPLEX),
 
     /**
      * Multicast notifications are no longer supported. This enum value will be removed in future versions.
      * @deprecated Since 9.0.0
      */
     @Deprecated
-    MULTICAST(AbstractNotifyEventListenerContainer.COM_TYPE_MULTICAST);
+    MULTICAST(SimpleNotifyEventListenerContainer.COM_TYPE_MULTICAST);
 
 
     private final int value;
