81184a70143b78ed5f4183028a75c7bb84a36b24
nivin <niv@gigaspaces.com>
2015-03-08 16:37:18 +0200

    GS-12296 - Merged AbstractSpaceListenerContainer into AbstractEventListenerContainer

--- src/main/java/org/openspaces/events/AbstractEventListenerContainer.java
+++ src/main/java/org/openspaces/events/AbstractEventListenerContainer.java
@@ -16,20 +16,45 @@
 
 package org.openspaces.events;
 
+import com.gigaspaces.admin.quiesce.QuiesceState;
+import com.gigaspaces.admin.quiesce.QuiesceStateChangedEvent;
+import com.gigaspaces.cluster.activeelection.ISpaceModeListener;
+import com.gigaspaces.cluster.activeelection.SpaceInitializationIndicator;
+import com.gigaspaces.cluster.activeelection.SpaceMode;
 import com.gigaspaces.metrics.LongCounter;
+import com.j_spaces.core.IJSpace;
+import com.j_spaces.core.admin.IInternalRemoteJSpaceAdmin;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openspaces.admin.quiesce.QuiesceStateChangedListener;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.space.mode.AfterSpaceModeChangeEvent;
+import org.openspaces.core.space.mode.BeforeSpaceModeChangeEvent;
 import org.openspaces.core.transaction.manager.JiniPlatformTransactionManager;
+import org.openspaces.core.util.SpaceUtils;
 import org.openspaces.events.adapter.EventListenerAdapter;
 import org.openspaces.events.support.AnnotationProcessorUtils;
+import org.openspaces.pu.service.ServiceDetailsProvider;
+import org.openspaces.pu.service.ServiceMonitorsProvider;
 import org.springframework.aop.support.AopUtils;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.*;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Method;
+import java.rmi.RemoteException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -40,18 +65,29 @@ import java.util.concurrent.atomic.AtomicReference;
  *
  * @author kimchy
  */
-public abstract class AbstractEventListenerContainer extends AbstractSpaceListeningContainer implements ApplicationContextAware {
+public abstract class AbstractEventListenerContainer implements ApplicationContextAware, Lifecycle, BeanNameAware, InitializingBean,
+        DisposableBean, ApplicationListener<ApplicationEvent>, ServiceDetailsProvider, ServiceMonitorsProvider, QuiesceStateChangedListener {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    private GigaSpace gigaSpace;
+    protected String beanName;
+    private boolean activeWhenPrimary = true;
+    private boolean registerSpaceModeListener = false;
+    private volatile boolean active = false;
+    private volatile boolean running = false;
+    private final List<Object> pausedTasks = new LinkedList<Object>();
+    private final Object lifecycleMonitor = new Object();
+    private PrimaryBackupListener primaryBackupListener;
+    private SpaceMode currentSpaceMode;
+    private volatile boolean autoStart = true;
+    private volatile boolean quiesced = false;
 
     private SpaceDataEventListener eventListener;
-
     private String eventListenerRef;
-
     private ApplicationContext applicationContext;
-
     protected EventExceptionHandler exceptionHandler;
-
     private final LongCounter processedEvents = new LongCounter();
-
     private final LongCounter failedEvents = new LongCounter();
 
     private Object template;
@@ -63,6 +99,82 @@ public abstract class AbstractEventListenerContainer extends AbstractSpaceListen
     private PlatformTransactionManager transactionManager;
     private DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
     protected boolean disableTransactionValidation = false;
+
+    /**
+     * Sets the GigaSpace instance to be used for space event listening operations.
+     */
+    public void setGigaSpace(GigaSpace gigaSpace) {
+        this.gigaSpace = gigaSpace;
+    }
+
+    /**
+     * Returns the GigaSpace instance to be used for Space operations.
+     */
+    protected final GigaSpace getGigaSpace() {
+        return this.gigaSpace;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    /**
+     * Return the bean name that this listener container has been assigned in its containing bean
+     * factory, if any.
+     */
+    protected final String getBeanName() {
+        return this.beanName;
+    }
+
+    /**
+     * Set whether this container will start only when it is primary (space mode).
+     *
+     * <p>Default is <code>true</code>. Set to <code>false</code> in order for this container to
+     * always start regardless of the space mode.
+     */
+    public void setActiveWhenPrimary(boolean activeWhenPrimary) {
+        this.activeWhenPrimary = activeWhenPrimary;
+    }
+
+    /**
+     */
+    public void setRegisterSpaceModeListener(boolean registerSpaceModeListener) {
+        this.registerSpaceModeListener = registerSpaceModeListener;
+    }
+
+    /**
+     * Set whether this container will start once instantiated.
+     *
+     * <p>Default is <code>true</code>. Set to <code>false</code> in order for this container to
+     * be started using {@link #start()}.
+     */
+    public void setAutoStart(boolean initOnStartup) {
+        this.autoStart = initOnStartup;
+    }
+
+    /**
+     * Return whether this container is currently running, that is, whether it has been started and
+     * not stopped yet.
+     */
+    public final boolean isRunning() {
+        return this.running && !this.quiesced;
+    }
+
+    /**
+     * Return whether this container is currently active, that is, whether it has been set up but
+     * not shut down yet.
+     */
+    public final boolean isActive() {
+        return this.active;
+    }
+
+    protected String getStatus() {
+        if (running)
+            return "started";
+        if (quiesced)
+            return "quiesced";
+        return "stopped";
+    }
 
     protected EventExceptionHandler getExceptionHandler() {
         return exceptionHandler;
@@ -130,13 +242,66 @@ public abstract class AbstractEventListenerContainer extends AbstractSpaceListen
         return applicationContext.getType(eventListenerRef);
     }
 
-    @Override
+    /**
+     * Delegates to {@link #validateConfiguration()} and {@link #initialize()}.
+     */
+    public void afterPropertiesSet() {
+        validateConfiguration();
+        initialize();
+    }
+
+    /**
+     * Initialize this container. If this container is not configured with "activeWhenPrimary" flag
+     * set to <code>true</code> will call {@link #doStart()} (if it is set to <code>true</code>,
+     * lifecycle of the container will be controlled by the current space mode).
+     * {@link #doInitialize()} will be called for additional initialization after the possible
+     * {@link #doStart()} call.
+     *
+     * @see #onApplicationEvent(org.springframework.context.ApplicationEvent)
+     */
     public void initialize() throws DataAccessException {
         initializeTransactionManager();
         initializeTemplate();
         initializeExceptionHandler();
-        super.initialize();
+        synchronized (this.lifecycleMonitor) {
+            this.active = true;
+            this.lifecycleMonitor.notifyAll();
+        }
+
+        doInitialize();
+
+        if (!activeWhenPrimary) {
+            doStart();
+        }
+
+        if (registerSpaceModeListener) {
+            SpaceMode currentMode = SpaceMode.PRIMARY;
+            if (!SpaceUtils.isRemoteProtocol(gigaSpace.getSpace())) {
+                primaryBackupListener = new PrimaryBackupListener();
+                try {
+                    IJSpace clusterMemberSpace = SpaceUtils.getClusterMemberSpace(gigaSpace.getSpace());
+                    ISpaceModeListener remoteListener = (ISpaceModeListener) clusterMemberSpace.getDirectProxy().getStubHandler()
+                            .exportObject(primaryBackupListener);
+                    currentMode = ((IInternalRemoteJSpaceAdmin) clusterMemberSpace.getAdmin()).addSpaceModeListener(remoteListener);
+                } catch (RemoteException e) {
+                    throw new InvalidDataAccessResourceUsageException("Failed to register space mode listener with space [" + gigaSpace.getSpace()
+                            + "]", e);
+                }
+            }
+            SpaceInitializationIndicator.setInitializer();
+            try {
+                onApplicationEvent(new BeforeSpaceModeChangeEvent(gigaSpace.getSpace(), currentMode));
+                onApplicationEvent(new AfterSpaceModeChangeEvent(gigaSpace.getSpace(), currentMode));
+            } finally {
+                SpaceInitializationIndicator.unsetInitializer();
+            }
+        }
     }
+
+    /**
+     * A callback to perform custom initialization steps.
+     */
+    protected abstract void doInitialize() throws DataAccessException;
 
     private void initializeTransactionManager() {
         // Use bean name as default transaction name.
@@ -145,9 +310,11 @@ public abstract class AbstractEventListenerContainer extends AbstractSpaceListen
         }
     }
 
-    @Override
+    /**
+     * Validate the configuration of this container.
+     */
     protected void validateConfiguration() {
-        super.validateConfiguration();
+        Assert.notNull(gigaSpace, "gigaSpace property is required");
         if (transactionManager != null && !disableTransactionValidation) {
             if (!getGigaSpace().getTxProvider().isEnabled()) {
                 throw new IllegalStateException(message("event container is configured to run under transactions (transaction manager is provided) " +
@@ -222,14 +389,222 @@ public abstract class AbstractEventListenerContainer extends AbstractSpaceListen
     }
 
     /**
+     * Calls {@link #shutdown()} when the BeanFactory destroys the container instance.
+     *
+     * @see #shutdown()
+     */
+    public void destroy() {
+        shutdown();
+    }
+
+    /**
+     * Stop container, call {@link #doShutdown()}, and close this container.
+     */
+    public void shutdown() throws DataAccessException {
+        if (logger.isDebugEnabled()) {
+            logger.debug(message("Shutting down Space Event listener container"));
+        }
+        synchronized (this.lifecycleMonitor) {
+            this.active = false;
+            this.running = false;
+            this.lifecycleMonitor.notifyAll();
+        }
+
+        if (registerSpaceModeListener) {
+            if (!SpaceUtils.isRemoteProtocol(gigaSpace.getSpace())) {
+                IJSpace clusterMemberSpace = SpaceUtils.getClusterMemberSpace(gigaSpace.getSpace());
+                try {
+                    ISpaceModeListener remoteListener = (ISpaceModeListener) clusterMemberSpace.getDirectProxy().getStubHandler()
+                            .exportObject(primaryBackupListener);
+                    ((IInternalRemoteJSpaceAdmin) clusterMemberSpace.getAdmin()).removeSpaceModeListener(remoteListener);
+                } catch (RemoteException e) {
+                    logger.warn("Failed to unregister space mode listener with space [" + gigaSpace.getSpace() + "]", e);
+                }
+            }
+        }
+
+        // Shut down the invokers.
+        doShutdown();
+    }
+
+    /**
+     * Perform any custom shutdown operations.
+     *
+     * @see #shutdown()
+     */
+    protected abstract void doShutdown() throws DataAccessException;
+
+    // -------------------------------------------------------------------------
+    // Lifecycle methods for dynamically starting and stopping the container
+    // -------------------------------------------------------------------------
+
+    /**
+     * Start this container.
+     *
+     * @see #doStart
+     */
+    public void start() throws DataAccessException {
+        if (!autoStart) {
+            autoStart = true;
+        }
+        if (!activeWhenPrimary) {
+            doStart();
+        } else {
+            if (currentSpaceMode != null && currentSpaceMode == SpaceMode.PRIMARY) {
+                doStart();
+            }
+        }
+    }
+
+    /**
      * Only start if we have a listener registered. If we don't, then
      * explicit start should be called.
      */
-    @Override
     protected void doStart() throws DataAccessException {
-        if (getEventListener() != null) {
-            super.doStart();
+        if (!autoStart || running || quiesced || getEventListener() == null)
+            return;
+
+        synchronized (this.lifecycleMonitor) {
+            if (running || quiesced)
+                return;
+
+            this.running = true;
+            this.lifecycleMonitor.notifyAll();
+            for (Iterator<Object> it = this.pausedTasks.iterator(); it.hasNext();) {
+                doRescheduleTask(it.next());
+                it.remove();
+            }
         }
+        doAfterStart();
+    }
+
+    protected void doAfterStart() throws DataAccessException {
+
+    }
+
+
+    /**
+     * Stop this container.
+     *
+     * @see #doStop
+     */
+    public void stop() throws DataAccessException {
+        doStop();
+    }
+
+    /**
+     * Notify all invoker tasks to stop
+     */
+    protected void doStop() throws DataAccessException {
+        if (!running) {
+            return;
+        }
+        doBeforeStop();
+        synchronized (this.lifecycleMonitor) {
+            this.running = false;
+            this.lifecycleMonitor.notifyAll();
+        }
+    }
+
+    protected void doBeforeStop() throws DataAccessException {
+
+    }
+
+    /**
+     * If the {@link #setActiveWhenPrimary(boolean)} is set to <code>true</code> (the default),
+     * the container lifecycle will be controlled by the space mode. The container will start when
+     * the space is in <code>PRIMARY</code> mode, and will stop when the space is in
+     * <code>BACKUP</code> mode.
+     *
+     * <p>Note, this might cause {@link #doStart()} or {@link #doStop()} to be called several times in
+     * a row, and sub classes should take this into account.
+     */
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        if (activeWhenPrimary) {
+            if (applicationEvent instanceof AfterSpaceModeChangeEvent) {
+                AfterSpaceModeChangeEvent spEvent = (AfterSpaceModeChangeEvent) applicationEvent;
+                if (spEvent.isPrimary() && SpaceUtils.isSameSpace(spEvent.getSpace(), gigaSpace.getSpace())) {
+                    if (logger.isTraceEnabled()) {
+                        logger.trace(message("Space [" + getGigaSpace() + "] became primary, starting the container"));
+                    }
+                    doStart();
+                }
+            } else if (applicationEvent instanceof BeforeSpaceModeChangeEvent) {
+                BeforeSpaceModeChangeEvent spEvent = (BeforeSpaceModeChangeEvent) applicationEvent;
+                if (!spEvent.isPrimary() && SpaceUtils.isSameSpace(spEvent.getSpace(), gigaSpace.getSpace())) {
+                    if (logger.isTraceEnabled()) {
+                        logger.trace(message("Space [" + getGigaSpace() + "] became backup, stopping the container"));
+                    }
+                    doStop();
+                }
+                currentSpaceMode = spEvent.getSpaceMode();
+            }
+        }
+    }
+
+    /**
+     * Wait while this container is not running.
+     *
+     * <p>To be called by asynchronous tasks that want to block while the container is in stopped
+     * state.
+     */
+    protected final void waitWhileNotRunning() {
+        while (this.active && !this.running) {
+            synchronized (this.lifecycleMonitor) {
+                if (this.active && !this.running) {
+                    try {
+                        this.lifecycleMonitor.wait();
+                    } catch (InterruptedException ex) {
+                        // Re-interrupt current thread, to allow other threads to react.
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Take the given task object and reschedule it, either immediately if this container is
+     * currently running, or later once this container has been restarted.
+     *
+     * <p>If this container has already been shut down, the task will not get rescheduled at all.
+     *
+     * @param task the task object to reschedule
+     * @return whether the task has been rescheduled (either immediately or for a restart of this
+     *         container)
+     * @see #doRescheduleTask
+     */
+    protected final boolean rescheduleTaskIfNecessary(Object task) {
+        Assert.notNull(task, "Task object must not be null");
+        if (this.running) {
+            doRescheduleTask(task);
+            return true;
+        } else if (this.active) {
+            synchronized (lifecycleMonitor) {
+                if (this.running) {
+                    doRescheduleTask(task);
+                    return true;
+                }
+                this.pausedTasks.add(task);
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Reschedule the given task object immediately.
+     *
+     * <p>To be implemented by subclasses if they ever call <code>rescheduleTaskIfNecessary</code>.
+     * This implementation throws an UnsupportedOperationException.
+     *
+     * @param task the task object to reschedule
+     * @see #rescheduleTaskIfNecessary
+     */
+    protected void doRescheduleTask(Object task) {
+        throw new UnsupportedOperationException(ClassUtils.getShortName(getClass())
+                + " does not support rescheduling of tasks");
     }
 
     // -------------------------------------------------------------------------
@@ -331,7 +706,7 @@ public abstract class AbstractEventListenerContainer extends AbstractSpaceListen
     /**
      * Called before each take and read polling operation to change the template
      * Overrides any template defined with {@link #setTemplate(Object)}
-     * @param templateProvider -
+     * @param dynamicTemplate -
      *      An object that implements {@link DynamicEventTemplateProvider}
      *      or has a method annotated with {@link DynamicEventTemplateProvider}
      */
@@ -457,4 +832,28 @@ public abstract class AbstractEventListenerContainer extends AbstractSpaceListen
         return transactionManager != null;
     }
 
+    private class PrimaryBackupListener implements ISpaceModeListener {
+
+        public void beforeSpaceModeChange(SpaceMode spaceMode) throws RemoteException {
+            onApplicationEvent(new BeforeSpaceModeChangeEvent(gigaSpace.getSpace(), spaceMode));
+        }
+
+        public void afterSpaceModeChange(SpaceMode spaceMode) throws RemoteException {
+            onApplicationEvent(new AfterSpaceModeChangeEvent(gigaSpace.getSpace(), spaceMode));
+        }
+    }
+
+    protected String message(String message) {
+        return "[" + getBeanName() + "] " + message;
+    }
+
+    public void quiesceStateChanged(QuiesceStateChangedEvent event){
+        quiesced = event.getQuiesceState().equals(QuiesceState.QUIESCED);
+        if (quiesced){
+            stop();
+        }
+        else {
+            start();
+        }
+    }
 }
