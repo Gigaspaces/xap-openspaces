d865f3e7da9b324c87275ee3d568e8f9c24bddb1
boris <boris@gigaspaces.com>
2015-04-13 13:04:21 +0300

    GS-12291 installmavenrep should work offline as well (partially)

--- tools/maven/installmavenrep.sh
+++ tools/maven/installmavenrep.sh
@@ -13,10 +13,6 @@ export EDITION=`${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.OutputVer
 
 export XAP_VERSION=`${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.OutputVersion XAP`
 
-# Dependencies that will be installed into the local maven repository
-DEPENDENCY_LIST="gs-openspaces,gs-openspaces-jetty,mongo-datasource,mule-os"
-${JAVACMD} -cp ${GS_JARS} org.openspaces.maven.support.POMGenerator ${TMPDIR} ${DEPENDENCY_LIST}
-
 echo ""
 echo ""
 echo "Installing XAP $XAP_VERSION jars"
@@ -24,10 +20,11 @@ echo ""
 echo ""
 
 # GigaSpaces Jars
-mvn -f ${TMPDIR}/gs-dependencies-pom.xml install
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=$XAP_VERSION -DpomFile=${JSHOMEDIR}/tools/maven/poms/gs-runtime/pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-runtime.jar -Djavadoc="${JSHOMEDIR}/docs/xap-javadoc.zip"
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=$XAP_VERSION -DpomFile=${JSHOMEDIR}/tools/maven/poms/gs-openspaces/pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-openspaces.jar -Djavadoc="${JSHOMEDIR}/docs/xap-javadoc.zip" -Dsources="${JSHOMEDIR}/lib/optional/openspaces/gs-openspaces-sources.jar"
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mule-os -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=${JSHOMEDIR}/tools/maven/poms/mule-os/pom.xml -Dfile=${JSHOMEDIR}/lib/optional/openspaces/mule-os.jar
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=jetty-os -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=${JSHOMEDIR}/tools/maven/poms/jetty-os/pom.xml -Dfile=${JSHOMEDIR}/lib/platform/openspaces/gs-openspaces-jetty.jar
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mongo-datasource -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=${JSHOMEDIR}/tools/maven/poms/mongo-datasource/pom.xml -Dfile=${JSHOMEDIR}/lib/optional/datasource/mongo/mongo-datasource.jar
 
 # Build and install OpenSpaces Maven Plugin
 mvn -f maven-openspaces-plugin/pom.xml install -DcreateChecksum=true
-
-# Remove temp files
-rm ${TMPDIR}/gs-dependencies-pom.xml
