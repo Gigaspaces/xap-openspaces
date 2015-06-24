f2f99d47078b9cc946632999ce148a6226975398
Barak Bar Orion <barak.bar@gmail.com>
2015-06-24 11:30:48 +0300

    GS-12295 -  Distributed task - improve class loading mechanism. GS-12351 Running Distributed Task can throw ClassNotFoundException, if this task was loaded by a client that already shutdown and the class has more dependencies to load. GS-12352 Distributed Task class is not unloaded after the task finish
    GS-12295 - Distributed task - improve class loading mechanism.
    Use class from TypeInfo only in case the classloader is one that load OneTime task.
    
    Code Review.
    Change the name of the annotation.

---
+++ src/main/java/org/openspaces/core/executor/SupportCodeChange.java
@@ -0,0 +1,16 @@
+package org.openspaces.core.executor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Barak Bar Orion
+ * 6/24/15.
+ * Mark a DistributedTask to be garbage after usage.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SupportCodeChange {
+}
