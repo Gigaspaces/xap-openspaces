cffc596e18327f367dda7975b67acb82e9930927
evgenyf <evgenyf@gigaspaces.com>
2015-10-11 17:07:01 +0300

    temporary don't use xap-javadoc.zip in scripts installmavenrep

--- tools/maven/installmavenrep.sh
+++ tools/maven/installmavenrep.sh
@@ -20,8 +20,8 @@ echo ""
 echo ""
 
 # GigaSpaces Jars
-mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=$XAP_VERSION -DpomFile=${JSHOMEDIR}/tools/maven/poms/gs-runtime/pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-runtime.jar -Djavadoc="${JSHOMEDIR}/docs/xap-javadoc.zip"
-mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=$XAP_VERSION -DpomFile=${JSHOMEDIR}/tools/maven/poms/gs-openspaces/pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-openspaces.jar -Djavadoc="${JSHOMEDIR}/docs/xap-javadoc.zip" -Dsources="${JSHOMEDIR}/lib/optional/openspaces/gs-openspaces-sources.jar"
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-runtime -Dversion=$XAP_VERSION -DpomFile=${JSHOMEDIR}/tools/maven/poms/gs-runtime/pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-runtime.jar
+mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=gs-openspaces -Dversion=$XAP_VERSION -DpomFile=${JSHOMEDIR}/tools/maven/poms/gs-openspaces/pom.xml -Dpackaging=jar -Dfile=${JSHOMEDIR}/lib/required/gs-openspaces.jar -Dsources="${JSHOMEDIR}/lib/optional/openspaces/gs-openspaces-sources.jar"
 mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mule-os -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=${JSHOMEDIR}/tools/maven/poms/mule-os/pom.xml -Dfile=${JSHOMEDIR}/lib/optional/openspaces/mule-os.jar
 mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=jetty-os -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=${JSHOMEDIR}/tools/maven/poms/jetty-os/pom.xml -Dfile=${JSHOMEDIR}/lib/platform/openspaces/gs-openspaces-jetty.jar
 mvn install:install-file -DgroupId=com.gigaspaces -DcreateChecksum=true -DartifactId=mongo-datasource -Dversion=$XAP_VERSION -Dpackaging=jar -DpomFile=${JSHOMEDIR}/tools/maven/poms/mongo-datasource/pom.xml -Dfile=${JSHOMEDIR}/lib/optional/datasource/mongo/mongo-datasource.jar
