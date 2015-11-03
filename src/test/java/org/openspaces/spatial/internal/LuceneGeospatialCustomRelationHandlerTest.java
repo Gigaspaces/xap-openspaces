b43ff7b4fd53e39a03042e19b2b7eda49c6aa4a4
yohanakh <yohaha@yohanakhoury.com>
2015-11-03 23:34:23 +0200

    GS-12593 fixed LuceneGeospatialCustomRelationHandlerTest: call initialize

--- src/test/java/org/openspaces/spatial/internal/LuceneGeospatialCustomRelationHandlerTest.java
+++ src/test/java/org/openspaces/spatial/internal/LuceneGeospatialCustomRelationHandlerTest.java
@@ -1,9 +1,11 @@
 package org.openspaces.spatial.internal;
 
+import com.gigaspaces.internal.server.space.SpaceConfigReader;
 import com.gigaspaces.spatial.shapes.Point;
 import com.gigaspaces.spatial.shapes.Polygon;
 import com.spatial4j.core.shape.Shape;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -12,10 +14,15 @@ import org.junit.Test;
  * @since 11.0
  */
 public class LuceneGeospatialCustomRelationHandlerTest {
+    private LuceneGeospatialCustomRelationHandler _handler;
+    @Before
+    public void setup() throws Exception {
+        _handler = new LuceneGeospatialCustomRelationHandler();
+        _handler.initialize("geospatial", "dummy", new SpaceConfigReader("dummy"));
+    }
 
     @Test
     public void testClosedPolygon() throws Exception {
-        LuceneGeospatialCustomRelationHandler handler = new LuceneGeospatialCustomRelationHandler();
 
         Polygon polygonWithCloseRing = new Polygon(new Point(75.05722045898438, 41.14039880964587),
                 new Point(73.30490112304686, 41.15797827873605),
@@ -23,31 +30,29 @@ public class LuceneGeospatialCustomRelationHandlerTest {
                 new Point(74.87319946289062, 40.50544628405211),
                 new Point(75.05722045898438, 41.14039880964587));
 
-        Shape spatial4jPolygon = handler.convertPolygon(polygonWithCloseRing);
+        Shape spatial4jPolygon = _handler.convertPolygon(polygonWithCloseRing);
         Assert.assertNotNull(spatial4jPolygon);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testIllegalClosedPolygon() throws Exception {
-        LuceneGeospatialCustomRelationHandler handler = new LuceneGeospatialCustomRelationHandler();
 
         Polygon polygonWithCloseRing = new Polygon(new Point(75.05722045898438, 41.14039880964587),
                 new Point(73.30490112304686, 41.15797827873605),
                 new Point(75.05722045898438, 41.14039880964587));
 
-        Shape spatial4jPolygon = handler.convertPolygon(polygonWithCloseRing);
+        Shape spatial4jPolygon = _handler.convertPolygon(polygonWithCloseRing);
         Assert.assertNotNull(spatial4jPolygon);
     }
 
     @Test
     public void testLegalClosedPolygon() throws Exception {
-        LuceneGeospatialCustomRelationHandler handler = new LuceneGeospatialCustomRelationHandler();
 
         Polygon polygonWithCloseRing = new Polygon(new Point(75.05722045898438, 41.14039880964587),
                 new Point(73.30490112304686, 41.15797827873605),
                 new Point(73.64822387695311, 40.447992135544304));
 
-        Shape spatial4jPolygon = handler.convertPolygon(polygonWithCloseRing);
+        Shape spatial4jPolygon = _handler.convertPolygon(polygonWithCloseRing);
         Assert.assertNotNull(spatial4jPolygon);
     }
 }
