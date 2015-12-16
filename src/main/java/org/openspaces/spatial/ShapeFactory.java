3f59400c083ba2ad61df3a63d34a0a882974ea15
Niv Ingberg <niv@gigaspaces.com>
2015-12-16 15:05:34 +0200

    GS-12502 - Added initial LineString support

--- src/main/java/org/openspaces/spatial/ShapeFactory.java
+++ src/main/java/org/openspaces/spatial/ShapeFactory.java
@@ -18,17 +18,20 @@
 package org.openspaces.spatial;
 
 import com.gigaspaces.internal.utils.Assert;
+import com.spatial4j.core.shape.jts.JtsGeometry;
+import org.openspaces.spatial.internal.shapes.*;
 import org.openspaces.spatial.shapes.*;
 import com.spatial4j.core.context.jts.JtsSpatialContext;
-import org.openspaces.spatial.internal.shapes.CircleImpl;
-import org.openspaces.spatial.internal.shapes.PointImpl;
-import org.openspaces.spatial.internal.shapes.PolygonImpl;
-import org.openspaces.spatial.internal.shapes.RectangleImpl;
 import com.spatial4j.core.context.SpatialContext;
+import org.openspaces.spatial.shapes.Circle;
+import org.openspaces.spatial.shapes.Point;
+import org.openspaces.spatial.shapes.Rectangle;
+import org.openspaces.spatial.shapes.Shape;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Factory class for creating spatial shapes.
@@ -76,6 +79,35 @@ public class ShapeFactory {
     }
 
     /**
+     * Creates a LineString instance from the specified points
+     * @param first The first point
+     * @param second The second point
+     * @param morePoints The rest of the points
+     * @return A new LineString instance
+     */
+    public static LineString lineString(Point first, Point second, Point... morePoints) {
+        Point[] points = new Point[2 + morePoints.length];
+        points[0] = Assert.argumentNotNull(first, "first");
+        points[1] = Assert.argumentNotNull(second, "second");
+        for (int i=0 ; i < morePoints.length ; i++)
+            points[i+2] = morePoints[i];
+        return lineString(points);
+    }
+
+    /**
+     * Creates a LineString instance from the specified points
+     * @param points The LineString points
+     * @return A new LineString instance
+     */
+    public static LineString lineString(Collection<Point> points) {
+        return lineString(points.toArray(new Point[points.size()]));
+    }
+
+    private static LineString lineString(Point[] points) {
+        return new LineStringImpl(points);
+    }
+
+    /**
      * Creates a Polygon instance from the specified points
      * @param first The first point
      * @param second The second point
@@ -103,6 +135,10 @@ public class ShapeFactory {
         return polygon(points.toArray(new Point[points.size()]));
     }
 
+    private static Polygon polygon(Point[] points) {
+        return new PolygonImpl(points);
+    }
+
     /**
      * Parses the specified string using the specified shape format
      * @param s String to parse
@@ -110,35 +146,30 @@ public class ShapeFactory {
      * @return The created shape instance
      */
     public static Shape parse(String s, ShapeFormat shapeFormat) {
+        try {
+            return fromSpatial4JShape(getReader(shapeFormat).read(s));
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to parse shape using " + shapeFormat, e);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Failed to parse shape using " + shapeFormat, e);
+        }
+    }
+
+    private static com.spatial4j.core.io.ShapeReader getReader(ShapeFormat shapeFormat) {
+        com.spatial4j.core.io.ShapeReader result;
         switch (shapeFormat) {
-            case WKT: return fromSpatial4JShape(fromWkt(s));
-            case GEOJSON: return fromSpatial4JShape(fromGeoJson(s));
-            default: throw new IllegalArgumentException("Unsupported shape format - " + shapeFormat);
+            case WKT:
+                result = getDefaultSpatialContext().getFormats().getWktReader();
+                break;
+            case GEOJSON:
+                result = getDefaultSpatialContext().getFormats().getGeoJsonReader();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported Shape Format: " + shapeFormat);
         }
-    }
-
-    private static com.spatial4j.core.shape.Shape fromWkt(String wkt) {
-        try {
-            return getDefaultSpatialContext().getFormats().getWktReader().read(wkt);
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to parse WKT shape", e);
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Failed to parse WKT shape", e);
-        }
-    }
-
-    private static com.spatial4j.core.shape.Shape fromGeoJson(String geoJson) {
-        try {
-            return getDefaultSpatialContext().getFormats().getGeoJsonReader().read(geoJson);
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to parse GeoJson shape", e);
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Failed to parse GeoJson shape", e);
-        }
-    }
-
-    private static Polygon polygon(Point[] points) {
-        return new PolygonImpl(points);
+        if (result == null)
+            throw new IllegalStateException("No Shape reader for format " + shapeFormat);
+        return result;
     }
 
     private static SpatialContext getDefaultSpatialContext() {
@@ -158,14 +189,29 @@ public class ShapeFactory {
             com.spatial4j.core.shape.Rectangle rectangle = (com.spatial4j.core.shape.Rectangle) shape;
             return rectangle(rectangle.getMinX(), rectangle.getMaxX(), rectangle.getMinY(), rectangle.getMaxY());
         }
-        if (shape instanceof com.spatial4j.core.shape.jts.JtsGeometry) {
-            com.vividsolutions.jts.geom.Coordinate[] coordinates = ((com.spatial4j.core.shape.jts.JtsGeometry) shape).getGeom().getCoordinates();
-            Point[] points = new Point[coordinates.length];
-            for (int i=0 ; i < coordinates.length ; i++)
-                points[i] = point(coordinates[i].getOrdinate(0), coordinates[i].getOrdinate(1));
-            return polygon(points);
-
+        if (shape instanceof com.spatial4j.core.shape.impl.BufferedLineString) {
+            com.spatial4j.core.shape.impl.BufferedLineString spatialLineString = (com.spatial4j.core.shape.impl.BufferedLineString) shape;
+            List<com.spatial4j.core.shape.Point> spatialPoints = spatialLineString.getPoints();
+            Point[] points = new Point[spatialPoints.size()];
+            for (int i=0 ; i < points.length ; i++)
+                points[i] = point(spatialPoints.get(i).getX(), spatialPoints.get(i).getY());
+            return lineString(points);
         }
+        if (shape instanceof com.spatial4j.core.shape.jts.JtsGeometry)
+            return fromJtsGeometry((JtsGeometry) shape);
         throw new IllegalArgumentException("Unsupported shape type: " + shape.getClass().getName());
+    }
+
+    private static Shape fromJtsGeometry(com.spatial4j.core.shape.jts.JtsGeometry shape) {
+        com.vividsolutions.jts.geom.Coordinate[] coordinates = shape.getGeom().getCoordinates();
+        Point[] points = new Point[coordinates.length];
+        for (int i=0 ; i < coordinates.length ; i++)
+            points[i] = point(coordinates[i].getOrdinate(0), coordinates[i].getOrdinate(1));
+        String shapeType = shape.getGeom().getGeometryType();
+        if (shapeType.equals("LineString"))
+            return lineString(points);
+        if (shapeType.equals("Polygon"))
+            return polygon(points);
+        throw new IllegalArgumentException("Unsupported shape type: " + shapeType);
     }
 }
