2cf835c1c75250b3b48dc8c3b6e8200e9214b7f7
Niv Ingberg <niv@gigaspaces.com>
2016-02-01 10:47:07 +0200

    GS-12733 - Replacing GS_LOGGING_CONFIG_FILE_PROP with XAP_OPTIONS

--- bin/puInstance.bat
+++ bin/puInstance.bat
@@ -4,7 +4,7 @@
 @call "%~dp0\setenv.bat"
 
 @echo Starting a Processing Unit Instance
-set COMMAND_LINE=%JAVACMD% %JAVA_OPTIONS% %XAP_OPTIONS% %GS_LOGGING_CONFIG_FILE_PROP% %RMI_OPTIONS% "-Dcom.gs.home=%XAP_HOME%" -Djava.security.policy=%POLICY% -classpath %PRE_CLASSPATH%;%GS_JARS%;%SPRING_JARS%;%EXT_JARS%;%JDBC_JARS%;%POST_CLASSPATH% org.openspaces.pu.container.standalone.StandaloneProcessingUnitContainer %*
+set COMMAND_LINE=%JAVACMD% %JAVA_OPTIONS% %XAP_OPTIONS% %RMI_OPTIONS% "-Dcom.gs.home=%XAP_HOME%" -Djava.security.policy=%POLICY% -classpath %PRE_CLASSPATH%;%GS_JARS%;%SPRING_JARS%;%EXT_JARS%;%JDBC_JARS%;%POST_CLASSPATH% org.openspaces.pu.container.standalone.StandaloneProcessingUnitContainer %*
 
 set TITLE="Processing Unit Instance ["%*"] started on [%computername%]"
 @title %TITLE%
